9f308031244e9c4b552916b9ff8f49c85225cbf3
HopeBaron <truehope9000@gmail.com>
2019-05-29 18:36:30 +0300

    Used right naming conventions.

--- gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/OpCode.kt
+++ gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/OpCode.kt
@@ -4,14 +4,14 @@ enum class OpCode(val code: Int) {
     Dispatch(0),
     Heartbeat(1),
     Identify(2),
-    Status_Update(3),
-    Voice_State_Update(4),
+    StatusUpdate(3),
+    VoiceStateUpdate(4),
     Resume(6),
     Reconnect(7),
-    Request_Guild_Memebers(8),
-    Invalid_Session(9),
+    RequestGuildMembers(8),
+    InvalidSession(9),
     Hello(10),
-    Hello_ACK(11)
+    HelloACK(11)
 
 
 }
