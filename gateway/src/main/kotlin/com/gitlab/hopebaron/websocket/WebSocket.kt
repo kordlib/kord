33c919a6b08544a16c196c5a9cfa1dc760b4f946
HopeBaron <truehope9000@gmail.com>
2019-06-01 14:33:13 +0300

    Commented troublesome code.

--- gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/WebSocket.kt
+++ gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/WebSocket.kt
@@ -4,11 +4,7 @@ import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.WebSocketSession
 import io.ktor.http.cio.websocket.readText
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.channels.first
 import kotlinx.coroutines.channels.map
-import kotlinx.coroutines.channels.ticker
-import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import kotlin.coroutines.CoroutineContext
@@ -29,28 +25,29 @@ class DiscordWebSocket(private val ws: WebSocketSession) : CoroutineScope {
     private val job = Job() + Dispatchers.IO
     override val coroutineContext: CoroutineContext = job
     val incoming = ws.incoming.map { it.payload() }
-    suspend fun send(payload: Payload) = ws.send(Frame.Text(payload.stringify()))
+    /*suspend fun send(payload: SendPayload) = ws.send(Frame.Text(payload.stringify()))
     private suspend fun getInterval(): Long {
         val hello = incoming.first { it.opCode == OpCode.Hello }
-        return hello.data!!.primitive.long
-    }
+         return hello.data!!.primitive.long
+    }*/
 
     private fun heartBeat() = launch {
-        ticker(getInterval()).consumeEach { send(Payload(OpCode.Heartbeat, sequence)) }
+        // ticker(getInterval()).consumeEach { send(SendPayload(OpCode.Heartbeat, TODO("Need a "))) }
     }
 
 
 }
 
 @UnstableDefault
-fun Frame.payload(): Payload {
+fun Frame.payload(): ReceivePayload {
     val element = Json.plain.parseJson((this as Frame.Text).readText())
-    return Json.plain.fromJson(Payload.serializer(), element)
+    return Json.plain.fromJson(ReceivePayload.serializer(), element)
 }
 
 @UnstableDefault
-private fun Payload.stringify() = Json.stringify(Payload.serializer(), this)
+private fun ReceivePayload.stringify() = Json.stringify(ReceivePayload.serializer(), this)
 
+fun SendPayload.stringify() = Unit
 
 
 
