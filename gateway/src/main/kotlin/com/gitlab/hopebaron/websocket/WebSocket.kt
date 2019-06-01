5c8273d957b8d020d70442be48a8d67df5e33034
HopeBaron <truehope9000@gmail.com>
2019-06-01 15:37:40 +0300

    Remove comments from code.

--- gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/WebSocket.kt
+++ gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/WebSocket.kt
@@ -4,12 +4,15 @@ import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.WebSocketSession
 import io.ktor.http.cio.websocket.readText
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.first
 import kotlinx.coroutines.channels.map
+import kotlinx.coroutines.channels.ticker
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import kotlin.coroutines.CoroutineContext
 
-
+// TODO("Current Class is broken.")
 @UnstableDefault
 @ExperimentalCoroutinesApi
 @ObsoleteCoroutinesApi
@@ -25,14 +28,14 @@ class DiscordWebSocket(private val ws: WebSocketSession) : CoroutineScope {
     private val job = Job() + Dispatchers.IO
     override val coroutineContext: CoroutineContext = job
     val incoming = ws.incoming.map { it.payload() }
-    /*suspend fun send(payload: SendPayload) = ws.send(Frame.Text(payload.stringify()))
+    suspend fun send(payload: SendPayload) = ws.send(Frame.Text(payload.stringify()))
     private suspend fun getInterval(): Long {
         val hello = incoming.first { it.opCode == OpCode.Hello }
          return hello.data!!.primitive.long
-    }*/
+    }
 
     private fun heartBeat() = launch {
-        // ticker(getInterval()).consumeEach { send(SendPayload(OpCode.Heartbeat, TODO("Need a "))) }
+        ticker(getInterval()).consumeEach { send(SendPayload(OpCode.Heartbeat, TODO("Need a "))) }
     }
 
 
