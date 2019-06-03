6dbe750f5c22a2f7ad18bd8ae43fd52903fe95b4
Bart Arys <mysterybtd@gmail.com>
2019-06-03 09:03:44 +0200

    Add more Commands

--- gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/Command.kt
+++ gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/Command.kt
@@ -1,31 +1,163 @@
 package com.gitlab.hopebaron.websocket
 
 import com.gitlab.hopebaron.websocket.entity.Activity
+import com.gitlab.hopebaron.websocket.entity.Shard
 import com.gitlab.hopebaron.websocket.entity.Snowflake
 import kotlinx.serialization.*
 import kotlinx.serialization.internal.SerialClassDescImpl
 import kotlinx.serialization.internal.StringDescriptor
 
-sealed class Command
+sealed class Command {
 
-data class Resume(val token: String, val sessionId: String, val sequenceNumber: Long) {
+    companion object : SerializationStrategy<Command> {
+
+        override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Command") {
+            init {
+                addElement("op")
+                addElement("d")
+            }
+        }
+
+        override fun serialize(encoder: Encoder, obj: Command) {
+            val composite = encoder.beginStructure(descriptor)
+            when (obj) {
+                is RequestGuildMembers -> {
+                    composite.encodeSerializableElement(descriptor, 0, OpCode.OpCodeSerializer, OpCode.RequestGuildMembers)
+                    composite.encodeSerializableElement(descriptor, 1, RequestGuildMembers.serializer(), obj)
+                }
+                is UpdateVoiceStatus -> {
+                    composite.encodeSerializableElement(descriptor, 0, OpCode.OpCodeSerializer, OpCode.VoiceStateUpdate)
+                    composite.encodeSerializableElement(descriptor, 1, UpdateVoiceStatus.serializer(), obj)
+                }
+                is UpdateStatus -> {
+                    composite.encodeSerializableElement(descriptor, 0, OpCode.OpCodeSerializer, OpCode.StatusUpdate)
+                    composite.encodeSerializableElement(descriptor, 1, UpdateStatus.serializer(), obj)
+                }
+            }
+
+            composite.endStructure(descriptor)
+        }
+
+    }
+
+}
+
+
+data class Identify(
+        internal val token: String,
+        val properties: IdentifyProperties,
+        val compress: Boolean? = null,
+        @SerialName("large_threshold")
+        val largeThreshold: Int = 50,
+        val shard: Shard? = null,
+        val presence: Presence? = null
+) {
+    override fun toString(): String = "Identify(token=hunter2,properties=$properties,compress=$compress,largeThreshold=$largeThreshold," +
+            "shard=$shard,presence=$presence"
+
+    companion object : SerializationStrategy<Identify> {
+
+        override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Command") {
+            init {
+                addElement("op")
+                addElement("d")
+            }
+        }
+
+        override fun serialize(encoder: Encoder, obj: Identify) {
+            val composite = encoder.beginStructure(descriptor)
+            composite.encodeSerializableElement(descriptor, 0, OpCode.OpCodeSerializer, OpCode.Identify)
+            composite.encodeSerializableElement(descriptor, 1, Inner, obj)
+            composite.endStructure(descriptor)
+        }
+
+        private object Inner : SerializationStrategy<Identify> {
+            override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Resume") {
+                init {
+                    addElement("token")
+                    addElement("properties")
+                    addElement("compress")
+                    addElement("large_threshold")
+                    addElement("shard")
+                    addElement("presence")
+                }
+            }
+
+            override fun serialize(encoder: Encoder, obj: Identify) {
+                val composite = encoder.beginStructure(descriptor)
+                composite.encodeStringElement(descriptor, 0, obj.token)
+                composite.encodeSerializableElement(descriptor, 1, IdentifyProperties.serializer(), obj.properties)
+                composite.encodeNullableSerializableElement(descriptor, 2, Boolean.serializer(), obj.compress)
+                composite.encodeIntElement(descriptor, 3, obj.largeThreshold)
+                composite.encodeNullableSerializableElement(descriptor, 4, Shard.serializer(), obj.shard)
+                composite.encodeNullableSerializableElement(descriptor, 5, Presence.serializer(), obj.presence)
+                composite.endStructure(descriptor)
+            }
+        }
+
+    }
+
+}
+
+@Serializable
+data class IdentifyProperties(
+        @SerialName("\$os")
+        val os: String,
+        @SerialName("\$browser")
+        val browser: String,
+        @SerialName("\$device")
+        val device: String
+)
+
+@Serializable
+data class Presence(
+        val status: String,
+        val afk: Boolean,
+        val since: Int? = null,
+        val game: Activity? = null
+)
+
+
+@Serializable
+data class Resume(
+        val token: String,
+        val sessionId: String,
+        val sequenceNumber: Long
+) {
     override fun toString(): String = "Resume(token=hunter2,sessionId=$sessionId,sequenceNumber:$sequenceNumber)"
 
     companion object : SerializationStrategy<Resume> {
-        override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Resume") {
+
+        override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Command") {
             init {
-                addElement("token")
-                addElement("session_id")
-                addElement("seq")
+                addElement("op")
+                addElement("d")
             }
         }
 
         override fun serialize(encoder: Encoder, obj: Resume) {
             val composite = encoder.beginStructure(descriptor)
-            composite.encodeStringElement(descriptor, 0, obj.token)
-            composite.encodeStringElement(descriptor, 1, obj.sessionId)
-            composite.encodeLongElement(descriptor, 0, obj.sequenceNumber)
+            composite.encodeSerializableElement(descriptor, 0, OpCode.OpCodeSerializer, OpCode.Resume)
+            composite.encodeSerializableElement(descriptor, 1, Inner, obj)
             composite.endStructure(descriptor)
+        }
+
+        private object Inner : SerializationStrategy<Resume> {
+            override val descriptor: SerialDescriptor = object : SerialClassDescImpl("Resume") {
+                init {
+                    addElement("token")
+                    addElement("session_id")
+                    addElement("seq")
+                }
+            }
+
+            override fun serialize(encoder: Encoder, obj: Resume) {
+                val composite = encoder.beginStructure(descriptor)
+                composite.encodeStringElement(descriptor, 0, obj.token)
+                composite.encodeStringElement(descriptor, 1, obj.sessionId)
+                composite.encodeLongElement(descriptor, 2, obj.sequenceNumber)
+                composite.endStructure(descriptor)
+            }
         }
 
     }
@@ -46,9 +178,7 @@ data class Heartbeat(val sequenceNumber: Long? = null) {
         override fun serialize(encoder: Encoder, obj: Heartbeat) {
             val composite = encoder.beginStructure(descriptor)
             composite.encodeIntElement(descriptor, 0, OpCode.Heartbeat.code)
-            obj.sequenceNumber?.let {
-                composite.encodeLongElement(descriptor, 1, it)
-            }
+            composite.encodeNullableSerializableElement(descriptor, 1, Long.serializer(), obj.sequenceNumber)
             composite.endStructure(descriptor)
         }
     }
@@ -77,16 +207,17 @@ data class UpdateVoiceStatus(
 
 @Serializable
 data class UpdateStatus(
-        val since: Int? = null,
+        val since: Long? = null,
         val game: Activity? = null,
         val status: Status,
         val afk: Boolean
 ) : Command()
 
-@Serializable
+@Serializable(with = Status.StatusSerializer::class)
 enum class Status {
     Online, DnD, Idle, Invisible, Offline;
 
+    @Serializer(forClass = Status::class)
     companion object StatusSerializer : KSerializer<Status> {
         override val descriptor: SerialDescriptor = StringDescriptor.withName("Status")
 
