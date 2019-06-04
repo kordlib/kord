192b6de2bd45cbb5b4696a0dc6d7c3b148e5e798
Bart Arys <mysterybtd@gmail.com>
2019-06-04 15:32:21 +0200

    Add checks

--- gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/retry/LinearRetry.kt
+++ gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/retry/LinearRetry.kt
@@ -2,13 +2,21 @@ package com.gitlab.hopebaron.websocket.retry
 
 import kotlinx.atomicfu.atomic
 import kotlinx.atomicfu.update
-import kotlinx.coroutines.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LinearRetry(
         private val firstBackoffMillis: Long,
         private val maxBackoffMillis: Long,
         private val maxTries: Int
 ) : Retry {
+
+    init {
+        require(firstBackoffMillis > 0) { "backoff needs to be positive but was $firstBackoffMillis" }
+        require(maxBackoffMillis > firstBackoffMillis) { "maxBackoff $maxBackoffMillis needs to be bigger than firstBackoff $firstBackoffMillis" }
+        require(maxTries > 0) { "maxTries needs to be positive but was $maxTries" }
+    }
 
     private var tries = atomic(0)
 
@@ -20,6 +28,8 @@ class LinearRetry(
     }
 
     override suspend fun retry() {
+        if (!hasNext) error("max retries exceeded")
+
         var diff = (firstBackoffMillis - maxBackoffMillis) / maxTries
         diff *= tries.value
         delay(diff)
