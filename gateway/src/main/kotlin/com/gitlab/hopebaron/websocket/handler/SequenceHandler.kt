282207135ae3f322c2b1a6f1bdbf4ffab0d73ea8
Bart Arys <mysterybtd@gmail.com>
2019-06-12 12:54:33 +0200

    Reset session on session close

--- gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/handler/SequenceHandler.kt
+++ gateway/src/main/kotlin/com/gitlab/hopebaron/websocket/handler/SequenceHandler.kt
@@ -3,6 +3,7 @@ package com.gitlab.hopebaron.websocket.handler
 import com.gitlab.hopebaron.websocket.DispatchEvent
 import com.gitlab.hopebaron.websocket.Event
 import com.gitlab.hopebaron.websocket.Sequence
+import com.gitlab.hopebaron.websocket.SessionClose
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.ObsoleteCoroutinesApi
@@ -18,6 +19,10 @@ internal class SequenceHandler(
     init {
         on<DispatchEvent> { event ->
             sequence.value = event.sequence ?: sequence.value
+        }
+
+        on<SessionClose> {
+            sequence.value = null
         }
     }
 
