046865ee1a93405cec337f03d4b9110ea89ecd49
Bart Arys <mysterybtd@gmail.com>
2019-06-25 16:23:54 +0200

    Add multipart requests

--- rest/src/main/kotlin/com/gitlab/hopebaron/rest/request/Request.kt
+++ rest/src/main/kotlin/com/gitlab/hopebaron/rest/request/Request.kt
@@ -1,14 +1,138 @@
 package com.gitlab.hopebaron.rest.request
 
+import com.gitlab.hopebaron.rest.route.Route
 import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.forms.MultiPartFormDataContent
+import io.ktor.client.request.forms.append
+import io.ktor.client.request.forms.formData
 import io.ktor.client.response.HttpResponse
+import io.ktor.client.response.readText
+import io.ktor.content.TextContent
+import io.ktor.http.ContentType
+import io.ktor.util.StringValues
+import kotlinx.io.InputStream
+import kotlinx.io.core.writeFully
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.json.Json
 
-interface Request<T> {
-    fun HttpRequestBuilder.apply()
+sealed class Request<T> {
+    internal abstract val route: Route<T>
+    internal abstract val routeParams: Map<String, Any>
+    abstract val identifier: RequestIdentifier
 
-    val identifier: RequestIdentifier
+    abstract fun HttpRequestBuilder.apply()
 
-    suspend fun parse(response: HttpResponse): T
+    open suspend fun parse(response: HttpResponse): T {
+        val json = response.readText()
+        println(json)
+        return Json.nonstrict.parse(route.strategy, json)
+    }
+
+    internal tailrec fun generatePath(builder: StringBuilder = StringBuilder(), start: Int = 0): String {
+        val indexOfNextParam = route.path.indexOf('{', start)
+
+        return when {
+            indexOfNextParam > start -> {
+                builder.append(route.path.subSequence(start, indexOfNextParam))
+                val indexOfNextParamEnd = route.path.indexOf('}', indexOfNextParam)
+                val param = route.path.subSequence(indexOfNextParam, indexOfNextParamEnd + 1)
+                builder.append(routeParams[param])
+                val nextStart = (indexOfNextParamEnd + 1).coerceAtMost(route.path.length)
+                generatePath(builder, nextStart)
+            }
+            start == 0 -> route.path
+            else -> {
+                builder.append(route.path.substring(start))
+                builder.toString()
+            }
+        }
+    }
 }
 
 interface RequestIdentifier
+
+internal data class MajorIdentifier(val path: String, val param: String? = null) : RequestIdentifier {
+
+    companion object {
+        operator fun invoke(route: Route<*>, routeParams: Map<String, Any>) : RequestIdentifier {
+            val indexOfNextParam = route.path.indexOf('{')
+            if (indexOfNextParam < 0) return MajorIdentifier(route.path)
+
+            val indexOfNextParamEnd = route.path.indexOf('}')
+            if (indexOfNextParamEnd < 0) return MajorIdentifier(route.path)
+
+            val param = route.path.subSequence(indexOfNextParam, indexOfNextParamEnd + 1)
+            val paramValue = routeParams[param].toString()
+
+            return MajorIdentifier(route.path, paramValue).also {
+                println(it)
+            }
+        }
+    }
+
+}
+
+data class RequestBody<T>(val strategy: SerializationStrategy<T>, val body: T) where T : Any
+
+internal class JsonRequest<T>(
+        override val route: Route<T>,
+        override val routeParams: Map<String, Any>,
+        private val parameters: StringValues,
+        private val body: RequestBody<*>?
+) : Request<T>() {
+
+    override fun HttpRequestBuilder.apply() {
+        method = route.method
+
+        url {
+            encodedPath += generatePath()
+            parameters.appendAll(this@JsonRequest.parameters)
+        }
+
+        this@JsonRequest.body?.let {
+            val json = Json.nonstrict.stringify(it.strategy as SerializationStrategy<Any>, it.body)
+            body = TextContent(json, ContentType.Application.Json)
+        }
+    }
+
+    override val identifier: RequestIdentifier = MajorIdentifier(route, routeParams)
+}
+
+internal class MutlipartRequest<T>(
+        override val route: Route<T>,
+        override val routeParams: Map<String, Any>,
+        private val parameters: StringValues,
+        private val body: RequestBody<*>?,
+        private val files: List<Pair<String, InputStream>> = emptyList()
+) : Request<T>() {
+    override val identifier: RequestIdentifier
+        get() = object : RequestIdentifier {}
+
+    override fun HttpRequestBuilder.apply() {
+        method = route.method
+
+        url {
+            encodedPath += generatePath()
+            parameters.appendAll(this@MutlipartRequest.parameters)
+        }
+
+
+        val data = formData {
+
+            this@MutlipartRequest.body?.let {
+                append("payload_json", Json.nonstrict.stringify(it.strategy as SerializationStrategy<Any>, it.body))
+            }
+
+            if (files.size == 1) append("file", filename = files[0].first) {
+                writeFully(files[0].second.readAllBytes())
+            }
+            else files.forEachIndexed { index, (file, content) ->
+                val bytes = content.readAllBytes()
+
+                append("file$index", file) { writeFully(bytes) }
+            }
+        }
+
+        body = MultiPartFormDataContent(data)
+    }
+}
