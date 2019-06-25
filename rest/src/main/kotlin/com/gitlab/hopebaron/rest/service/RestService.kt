9ad35d974304aa5f5aed0c2e86fa5ecc2a32c58c
Bart Arys <mysterybtd@gmail.com>
2019-06-25 20:06:17 +0200

    Fix missing import

--- rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/RestService.kt
+++ rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/RestService.kt
@@ -1,6 +1,7 @@
 package com.gitlab.hopebaron.rest.service
 
 import com.gitlab.hopebaron.rest.ratelimit.RequestHandler
+import com.gitlab.hopebaron.rest.request.RequestBuilder
 import com.gitlab.hopebaron.rest.route.Route
 
 abstract class RestService(protected val requestHandler: RequestHandler) {
