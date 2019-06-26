b7f415dce89218cad0795b8e8c964fb12c477f90
Bart Arys <mysterybtd@gmail.com>
2019-06-26 22:01:12 +0200

    Expanded RequestException message

--- rest/src/main/kotlin/com/gitlab/hopebaron/rest/request/RequestException.kt
+++ rest/src/main/kotlin/com/gitlab/hopebaron/rest/request/RequestException.kt
@@ -2,6 +2,6 @@ package com.gitlab.hopebaron.rest.request
 
 import io.ktor.client.response.HttpResponse
 
-class RequestException internal constructor(response: HttpResponse) : Exception(
-        "Request(method=${response.call.request.method.value}, url=${response.call.request.url}, body=${response.call.request})"
+class RequestException internal constructor(val response: HttpResponse) : Exception(
+        "Request(method=${response.call.request.method.value}, url=${response.call.request.url}, body=${response.call.request}) returned ${response.status}"
 )
