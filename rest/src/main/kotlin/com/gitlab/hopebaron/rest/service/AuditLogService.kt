e6bce5e863afce40d92b4dfcb7cb5a92cb912a8d
HopeBaron <truehope9000@gmail.com>
2019-06-30 18:48:29 +0300

    Change ActionType to AuditLogEventResponse

--- rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/AuditLogService.kt
+++ rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/AuditLogService.kt
@@ -1,47 +1,18 @@
 package com.gitlab.hopebaron.rest.service
 
+import com.gitlab.hopebaron.rest.json.response.AuditLogEventResponse
 import com.gitlab.hopebaron.rest.ratelimit.RequestHandler
 import com.gitlab.hopebaron.rest.route.Route
 import io.ktor.http.Parameters
 
 class AuditLogService(requestHandler: RequestHandler) : RestService(requestHandler) {
-    // TODO add action type
-    suspend fun getAuditLogs(guildId: String, userId: String, action: ActionType, before: String, limit: Int = 50) = call(Route.AuditLogGet) {
+    suspend fun getAuditLogs(guildId: String, userId: String, action: AuditLogEventResponse, before: String, limit: Int = 50) = call(Route.AuditLogGet) {
         keys[Route.GuildId] = guildId
         parameters = Parameters.build {
             append("user_id", userId)
-            append("action_type", "${action.type}")
+            append("action_type", "${action.code}")
             append("before", before)
             append("limit", "$limit")
         }
     }
-}
-
-enum class ActionType(val type: Int) {
-    GuildUpdate(1),
-    ChannelCreate(10),
-    ChannelUpdate(11),
-    ChannelDelete(12),
-    ChannelOverwriteCreate(13),
-    ChannelOverwriteUpdate(14),
-    ChannelOverwriteDelete(15),
-    MemberKick(20),
-    MemberPrune(21),
-    MemberBanAdd(22),
-    MemberBanDelete(23),
-    MemberUpdate(24),
-    MemberRoleUpdate(25),
-    RoleCreate(30),
-    RoleUpdate(31),
-    RoleDelete(32),
-    InviteCreate(40),
-    InviteUpdate(41),
-    InviteDelete(42),
-    WebhookCreate(50),
-    WebhookUpdate(51),
-    WebhookDelete(52),
-    EmojiCreate(60),
-    EmojiUpdate(61),
-    EmojiDelete(62),
-    MessageDelete(72)
 }
