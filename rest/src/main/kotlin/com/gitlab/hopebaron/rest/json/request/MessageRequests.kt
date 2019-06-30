60cc6ca2ebcca912ac54daf353922d6bb604cd9d
HopeBaron <truehope9000@gmail.com>
2019-06-30 18:52:07 +0300

    Add ChannelRequests

--- rest/src/main/kotlin/com/gitlab/hopebaron/rest/json/request/MessageRequests.kt
+++ rest/src/main/kotlin/com/gitlab/hopebaron/rest/json/request/MessageRequests.kt
@@ -1,8 +1,6 @@
 package com.gitlab.hopebaron.rest.json.request
 
 import com.gitlab.hopebaron.common.entity.Embed
-import com.gitlab.hopebaron.common.entity.Overwrite
-import com.gitlab.hopebaron.common.entity.Permission
 import kotlinx.io.InputStream
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -68,39 +66,6 @@ data class EmbedFieldRequest(
 @Serializable
 data class MessageEditRequest(val content: String? = null,
                               val embed: Embed? = null)
-
-@Serializable
-data class PutModifyChannelRequest(val name: String,
-                                   val position: Int,
-                                   val topic: String,
-                                   val nsfw: Boolean,
-                                   @SerialName("rate_limit_per_user")
-                                   val rateLimitPerUser: Int,
-                                   val bitrate: Int,
-                                   @SerialName("user_limit")
-                                   val userLimit: Int,
-                                   @SerialName("permission_overwrites")
-                                   val permissionOverwrites: List<Overwrite>,
-                                   @SerialName("parent_id")
-                                   val parentId: String)
-
-@Serializable
-data class PatchModifyChannelRequest(val name: String? = null,
-                                     val position: Int? = null,
-                                     val topic: String? = null,
-                                     val nsfw: Boolean? = null,
-                                     @SerialName("rate_limit_per_user")
-                                     val rateLimitPerUser: Int? = null,
-                                     val bitrate: Int? = null,
-                                     @SerialName("user_limit")
-                                     val userLimit: Int? = null,
-                                     @SerialName("permission_overwrites")
-                                     val permissionOverwrites: List<Overwrite>? = null,
-                                     @SerialName("parent_id")
-                                     val parentId: String? = null)
-
-@Serializable
-data class EditChannelPermissionRequest(val allow: Permission, val deny: Permission, val type: String)
 
 @Serializable
 data class BulkDeleteRequest(val messages: List<String>)
