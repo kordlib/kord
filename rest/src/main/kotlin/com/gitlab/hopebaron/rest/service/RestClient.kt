c7c443ed2ad24a73584845e8b57388cec8cd99d9
Bart Arys <mysterybtd@gmail.com>
2019-07-02 15:06:47 +0200

    Add VoiceService to RestClient

--- rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/RestClient.kt
+++ rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/RestClient.kt
@@ -12,6 +12,7 @@ class RestClient constructor(requestHandler: RequestHandler) : RestService(reque
     val guild: GuildService = GuildService(requestHandler)
     val invite: InviteService = InviteService(requestHandler)
     val user: UserService = UserService(requestHandler)
+    val voice: VoiceService = VoiceService(requestHandler)
 
     /**
      * Sends a request to the given [route].
