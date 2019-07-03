97503fc5d6951bb2b8b75a60671d75968a216f49
Bart Arys <mysterybtd@gmail.com>
2019-07-03 15:03:13 +0200

    Make getAuditLogs query optional

--- rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/AuditLogService.kt
+++ rest/src/main/kotlin/com/gitlab/hopebaron/rest/service/AuditLogService.kt
@@ -6,13 +6,21 @@ import com.gitlab.hopebaron.rest.route.Route
 import io.ktor.http.Parameters
 
 class AuditLogService(requestHandler: RequestHandler) : RestService(requestHandler) {
-    suspend fun getAuditLogs(guildId: String, userId: String, action: AuditLogEventResponse, before: String, limit: Int = 50) = call(Route.AuditLogGet) {
+
+    suspend fun getAuditLogs(
+            guildId: String,
+            userId: String? = null,
+            action: AuditLogEventResponse? = null,
+            before: String? = null,
+            limit: Int = 50
+    ) = call(Route.AuditLogGet) {
         keys[Route.GuildId] = guildId
         parameters = Parameters.build {
-            append("user_id", userId)
-            append("action_type", "${action.code}")
-            append("before", before)
+            userId?.let { append("user_id", it) }
+            action?.let { append("action_type", "${it.code}") }
+            before?.let { append("before", it) }
             append("limit", "$limit")
         }
     }
+
 }
