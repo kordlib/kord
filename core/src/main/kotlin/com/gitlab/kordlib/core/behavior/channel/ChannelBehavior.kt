0dfd0449bb417f2ed35f907e1eba3c8d7dc9cd75
Bart Arys <mysterybtd@gmail.com>
2019-08-01 13:33:47 +0200

    Repurposed ChannelBehavior to be a common Channel

--- core/src/main/kotlin/com/gitlab/kordlib/core/behavior/channel/ChannelBehavior.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/behavior/channel/ChannelBehavior.kt
@@ -1,13 +1,20 @@
 package com.gitlab.kordlib.core.behavior.channel
 
-import com.gitlab.kordlib.core.builder.message.NewMessageBuilder
+import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.entity.Entity
+import com.gitlab.kordlib.core.entity.Snowflake
 
 interface ChannelBehavior : Entity {
+    val mention get() = "<#${id.value}>"
 
-    suspend fun createMessage(builder: NewMessageBuilder): Nothing = TODO()
-    suspend fun createMessage(content: String): Nothing = TODO()
+    suspend fun delete() {
+        kord.rest.channel.deleteChannel(id.toString())
+    }
 
+    companion object {
+        internal operator fun invoke(id: Snowflake, kord: Kord) = object : ChannelBehavior {
+            override val id: Snowflake = id
+            override val kord: Kord = kord
+        }
+    }
 }
-
-suspend inline fun ChannelBehavior.createMessage(block: NewMessageBuilder.() -> Unit): Nothing = createMessage(NewMessageBuilder().apply(block))
