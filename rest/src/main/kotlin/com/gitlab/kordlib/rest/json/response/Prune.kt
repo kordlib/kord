7789ff21b41cc74ef6b3e23d8892a5c8daf1f260
Bart Arys <mysterybtd@gmail.com>
2019-08-03 13:46:01 +0200

    Differentiate between prune requests

--- rest/src/main/kotlin/com/gitlab/kordlib/rest/json/response/Prune.kt
+++ rest/src/main/kotlin/com/gitlab/kordlib/rest/json/response/Prune.kt
@@ -3,4 +3,7 @@ package com.gitlab.kordlib.rest.json.response
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PruneResponse(val pruned: Int)
+data class GetPruneResponse(val pruned: Int)
+
+@Serializable
+data class PruneResponse(val pruned: Int?)
