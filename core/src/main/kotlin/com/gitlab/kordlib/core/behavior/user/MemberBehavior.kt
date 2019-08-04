1a840444e85ed1762a3b6175d2954cfc2d02f321
Bart Arys <mysterybtd@gmail.com>
2019-08-04 13:07:50 +0200

    Add Member Behavior

---
+++ core/src/main/kotlin/com/gitlab/kordlib/core/behavior/user/MemberBehavior.kt
@@ -0,0 +1,31 @@
+package com.gitlab.kordlib.core.behavior.user
+
+import com.gitlab.kordlib.core.`object`.builder.ban.NewBanBuilder
+import com.gitlab.kordlib.core.`object`.builder.member.UpdateMemberBuilder
+import com.gitlab.kordlib.core.behavior.guild.GuildBehavior
+import com.gitlab.kordlib.core.entity.Entity
+import com.gitlab.kordlib.core.entity.Snowflake
+
+interface MemberBehavior : Entity, UserBehavior {
+    val guildId: Snowflake
+    val guild: GuildBehavior get() = GuildBehavior(guildId, kord)
+
+    suspend fun edit(builder: UpdateMemberBuilder): Nothing = TODO()
+
+    suspend fun ban(builder: NewBanBuilder = NewBanBuilder()) = guild.ban(id, builder)
+    suspend fun unban() = guild.unBan(id)
+
+    suspend fun kick() = guild.kick(id)
+
+    suspend fun addRole(roleId: Snowflake) {
+        kord.rest.guild.addRoleToGuildMember(guildId = guildId.value, userId = id.value, roleId = roleId.value)
+    }
+
+    suspend fun removeRole(roleId: Snowflake) {
+        kord.rest.guild.deleteRoleFromGuildMember(guildId = guildId.value, userId = id.value, roleId = roleId.value)
+    }
+
+}
+
+suspend inline fun MemberBehavior.ban(builder: NewBanBuilder.() -> Unit) = ban(NewBanBuilder().apply(builder))
+suspend inline fun MemberBehavior.edit(builder: UpdateMemberBuilder.() -> Unit): Nothing = edit(UpdateMemberBuilder().apply(builder))
