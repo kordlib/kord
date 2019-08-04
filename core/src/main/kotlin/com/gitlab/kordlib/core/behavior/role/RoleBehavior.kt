0509b41c19b148eb672e6ebf4bb6ec15a18a62cd
Bart Arys <mysterybtd@gmail.com>
2019-08-04 17:02:12 +0200

    Add RoleBehavior

---
+++ core/src/main/kotlin/com/gitlab/kordlib/core/behavior/role/RoleBehavior.kt
@@ -0,0 +1,31 @@
+package com.gitlab.kordlib.core.behavior.role
+
+import com.gitlab.kordlib.core.Kord
+import com.gitlab.kordlib.core.`object`.builder.role.UpdateRoleBuilder
+import com.gitlab.kordlib.core.behavior.guild.GuildBehavior
+import com.gitlab.kordlib.core.entity.Entity
+import com.gitlab.kordlib.core.entity.Snowflake
+import kotlinx.coroutines.flow.Flow
+
+interface RoleBehavior : Entity {
+    val guildId: Snowflake
+    val guild: GuildBehavior get() = GuildBehavior(guildId, kord)
+
+    suspend fun changePosition(position: Int) : Flow<Nothing /*Role*/> = TODO()
+
+    suspend fun edit(builder: UpdateRoleBuilder) : Nothing /*Role*/ = TODO()
+
+    suspend fun delete() {
+        kord.rest.guild.deleteGuildRole(guildId = guildId.value, roleId = id.value)
+    }
+
+    companion object {
+        internal operator fun invoke(guildId: Snowflake, id: Snowflake, kord: Kord): RoleBehavior = object: RoleBehavior {
+            override val guildId: Snowflake = guildId
+            override val id: Snowflake = id
+            override val kord: Kord = kord
+        }
+    }
+}
+
+suspend inline fun RoleBehavior.edit(builder: UpdateRoleBuilder.() -> Unit): Nothing /*Role*/  = edit(UpdateRoleBuilder().apply(builder))
---
+++ core/src/main/kotlin/com/gitlab/kordlib/core/object/builder/role/UpdateRoleBuilder.kt
@@ -0,0 +1,21 @@
+package com.gitlab.kordlib.core.`object`.builder.role
+
+import com.gitlab.kordlib.common.entity.Permissions
+import com.gitlab.kordlib.rest.json.request.ModifyGuildRoleRequest
+import java.awt.Color
+
+class UpdateRoleBuilder(
+        var name: String? = null,
+        var color: Color? = null,
+        var hoist: Boolean? = null,
+        var mentionable: Boolean? = null,
+        var permissions: Permissions? = null
+) {
+    internal fun toRequest(): ModifyGuildRoleRequest = ModifyGuildRoleRequest(
+            name = name,
+            color = color?.rgb,
+            separate = hoist,
+            mentionable = mentionable,
+            permissions = permissions
+    )
+}
