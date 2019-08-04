899a77026c9cc5b9d3ec8d600bbea760c123d6f2
Bart Arys <mysterybtd@gmail.com>
2019-08-04 15:56:58 +0200

    Add GuildEmojiBehavior companion constructor

--- core/src/main/kotlin/com/gitlab/kordlib/core/behavior/guild/GuildEmojiBehavior.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/behavior/guild/GuildEmojiBehavior.kt
@@ -1,5 +1,6 @@
 package com.gitlab.kordlib.core.behavior.guild
 
+import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.`object`.builder.guild.UpdateGuildEmojiBuilder
 import com.gitlab.kordlib.core.entity.Entity
 import com.gitlab.kordlib.core.entity.Snowflake
@@ -13,7 +14,15 @@ interface GuildEmojiBehavior : Entity {
         kord.rest.emoji.deleteEmoji(guildId = guildId.value, emojiId = id.value)
     }
 
-    suspend fun edit(builder: UpdateGuildEmojiBuilder) : Nothing /*GuildEmoji*/
+    suspend fun edit(builder: UpdateGuildEmojiBuilder) : Nothing /*GuildEmoji*/ = TODO()
+
+    companion object {
+        internal operator fun invoke(id: Snowflake, guildId: Snowflake, kord: Kord): GuildEmojiBehavior = object : GuildEmojiBehavior {
+            override val guildId: Snowflake = guildId
+            override val id: Snowflake = id
+            override val kord: Kord = kord
+        }
+    }
 }
 
 suspend inline fun GuildEmojiBehavior.edit(builder: UpdateGuildEmojiBuilder.() -> Unit): Nothing = edit(UpdateGuildEmojiBuilder().apply(builder))
