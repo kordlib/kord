6e3f2fe657ed2408d7e45ef91af29d884e3a538b
Bart Arys <mysterybtd@gmail.com>
2019-08-05 12:56:55 +0200

    Make Snowflake Comparable

--- core/src/main/kotlin/com/gitlab/kordlib/core/entity/Snowflake.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/entity/Snowflake.kt
@@ -1,7 +1,9 @@
 package com.gitlab.kordlib.core.entity
 
-inline class Snowflake(val value: String) {
+inline class Snowflake(val value: String) : Comparable<Snowflake> {
     constructor(value: Long) : this(value.toString())
 
     val longValue get() = value.toLong()
+
+    override fun compareTo(other: Snowflake): Int = longValue.compareTo(other.longValue )
 }
