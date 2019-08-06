90df5196d29c1324ce3e39f0b88ba5a376f0a951
Bart Arys <mysterybtd@gmail.com>
2019-08-06 11:11:42 +0200

    Remove ChannelId from WebhookBehavior

--- core/src/main/kotlin/com/gitlab/kordlib/core/behavior/webhook/WebhookBehavior.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/behavior/webhook/WebhookBehavior.kt
@@ -1,5 +1,6 @@
 package com.gitlab.kordlib.core.behavior.webhook
 
+import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.`object`.builder.webhook.ExecuteWebhookBuilder
 import com.gitlab.kordlib.core.`object`.builder.webhook.UpdateWebhookBuilder
 import com.gitlab.kordlib.core.behavior.channel.ChannelBehavior
@@ -10,11 +11,20 @@ import com.gitlab.kordlib.core.entity.Snowflake
  * The behavior of a [Discord Webhook](https://discordapp.com/developers/docs/resources/webhook).
  */
 interface WebhookBehavior : Entity {
-    val channelId: Snowflake
-    val channel: ChannelBehavior get() = ChannelBehavior(channelId, kord)
 
     suspend fun delete() {
         kord.rest.webhook.deleteWebhook(id.value)
+    }
+
+    suspend fun delete(token: String) {
+        kord.rest.webhook.deleteWebhookWithToken(id.value, token)
+    }
+
+    companion object {
+        internal operator fun invoke(id: Snowflake, kord: Kord) : WebhookBehavior = object : WebhookBehavior {
+            override val id: Snowflake = id
+            override val kord: Kord = kord
+        }
     }
 
 }
