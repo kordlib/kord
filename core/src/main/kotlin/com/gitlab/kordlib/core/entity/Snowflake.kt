8258f7c1a7e265e2338384338e6f23c081f28d0d
BartArys <mysterybtdn@gmail.com>
2019-09-15 16:43:17 +0200

    Add time functionality to Snowflake

--- core/src/main/kotlin/com/gitlab/kordlib/core/entity/Snowflake.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/entity/Snowflake.kt
@@ -1,5 +1,11 @@
 package com.gitlab.kordlib.core.entity
 
+import java.time.Instant
+import kotlin.time.ClockMark
+import kotlin.time.Duration
+import kotlin.time.toKotlinDuration
+
+
 /**
  * A unique identifier for entities [used by discord](https://discordapp.com/developers/docs/reference#snowflakes).
  */
@@ -8,5 +14,20 @@ inline class Snowflake(val value: String) : Comparable<Snowflake> {
 
     val longValue get() = value.toLong()
 
+    val timeStamp: Instant get() = Instant.ofEpochMilli(discordEpoch + (longValue shr 22))
+
+    val timeMark: ClockMark get() = SnowflakeMark(longValue shr 22)
+
     override fun compareTo(other: Snowflake): Int = longValue.shr(22).compareTo(other.longValue.shr(22))
+
+    companion object {
+        val discordEpoch = 1420070400000L
+    }
+}
+
+private class SnowflakeMark(val epochMilliseconds: Long) : ClockMark() {
+
+    override fun elapsedNow(): Duration =
+            java.time.Duration.between(Instant.ofEpochMilli(epochMilliseconds), Instant.now()).toKotlinDuration()
+
 }
