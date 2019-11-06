3980c69139af860740b2918a9c1eeff02d9ca61d
BartArys <mysterybtdn@gmail.com>
2019-11-06 16:30:43 +0100

    Disable Rest testing when not merging branch

--- rest/src/test/kotlin/com/gitlab/kordlib/rest/services/RestServiceTest.kt
+++ rest/src/test/kotlin/com/gitlab/kordlib/rest/services/RestServiceTest.kt
@@ -43,6 +43,11 @@ class RestServiceTest {
 
     private lateinit var userId: String
 
+    @BeforeEach
+    fun enableOnlyOnMerge() {
+        Assumptions.assumeTrue { System.getProperty("CI_MERGE_REQUEST_ID") != null }
+    }
+
     @BeforeAll
     fun setup() = runBlocking {
         client = HttpClient(CIO) {
