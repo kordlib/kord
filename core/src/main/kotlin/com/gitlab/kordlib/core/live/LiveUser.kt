1b204f59919f9f546425a12f414db2ad42d4ec7e
Hope <truehope9000@gmail.com>
2019-11-18 21:33:11 +0200

    Update LiveUser

--- core/src/main/kotlin/com/gitlab/kordlib/core/live/LiveUser.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/live/LiveUser.kt
@@ -1,36 +1,30 @@
 package com.gitlab.kordlib.core.live
 
+import com.gitlab.kordlib.common.annotation.KordPreview
 import com.gitlab.kordlib.core.entity.Entity
 import com.gitlab.kordlib.core.entity.User
-import com.gitlab.kordlib.core.event.*
-import com.gitlab.kordlib.core.event.channel.ChannelCreateEvent
-import com.gitlab.kordlib.core.event.channel.ChannelDeleteEvent
-import com.gitlab.kordlib.core.event.channel.ChannelUpdateEvent
-import com.gitlab.kordlib.core.event.guild.*
-import com.gitlab.kordlib.core.event.message.*
-import com.gitlab.kordlib.core.event.role.RoleCreateEvent
-import com.gitlab.kordlib.core.event.role.RoleDeleteEvent
-import com.gitlab.kordlib.core.event.role.RoleUpdateEvent
+import com.gitlab.kordlib.core.event.Event
+import com.gitlab.kordlib.core.event.PresenceUpdateEvent
+import com.gitlab.kordlib.core.event.UserUpdateEvent
 
-//TODO
+
+@KordPreview
+fun User.live() = LiveUser(this)
+
+@KordPreview
 class LiveUser(user: User) : AbstractLiveEntity(), Entity by user {
 
     var user: User = user
         private set
 
-    override fun filter(event: Event): Boolean = when (event) {
-        is UserUpdateEvent -> event.user.id == user.id
-        is UnbanEvent -> event.user.id == user.id
+    override fun filter(event: Event) = when (event) {
+        is UserUpdateEvent -> user.id == event.user.id
+        else -> false
+    }
 
-        is PresenceUpdateEvent -> event.user.id == user.id.value
-
-        is VoiceStateUpdateEvent -> event.state.userId == user.id
-
-        is MessageCreateEvent -> event.message.author?.id == user.id
-        is MessageUpdateEvent -> event.new.author?.id == user.id.value
-        is MessageDeleteEvent -> event.message?.author?.id == user.id
-
-        else -> true
+    override fun update(event: Event) = when (event) {
+        is UserUpdateEvent -> user = event.user
+        else -> Unit
     }
 
 }
