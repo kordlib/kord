e9dadbacc2c469f8add700340d257d7d1aedeb0c
HopeBaron <truehope9000@gmail.com>
2020-01-31 19:57:14 +0200

    Change avatar in WebhookCreateBuilder type to Image

--- core/src/main/kotlin/com/gitlab/kordlib/core/builder/webhook/WebhookCreateBuilder.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/builder/webhook/WebhookCreateBuilder.kt
@@ -2,13 +2,15 @@ package com.gitlab.kordlib.core.builder.webhook
 
 import com.gitlab.kordlib.core.builder.AuditRequestBuilder
 import com.gitlab.kordlib.core.builder.KordDsl
+import com.gitlab.kordlib.core.entity.Image
 import com.gitlab.kordlib.rest.json.request.WebhookCreateRequest
 
 @KordDsl
 class WebhookCreateBuilder: AuditRequestBuilder<WebhookCreateRequest> {
     override var reason: String? = null
     lateinit var name: String
-    var avatar: String? = null
+    var avatar: Image? = null
 
-    override fun toRequest() = WebhookCreateRequest(name, avatar)
+    override fun toRequest()  = WebhookCreateRequest(name, avatar?.dataUri)
+
 }
