7ee7e2ca8d7cd5968b05c7d77321e24496b4ad9d
BartArys <mysterybtdn@gmail.com>
2020-02-10 16:43:47 +0100

    More documentation
    [skip ci]

--- core/src/main/kotlin/com/gitlab/kordlib/core/entity/GuildEmoji.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/entity/GuildEmoji.kt
@@ -3,6 +3,7 @@ package com.gitlab.kordlib.core.entity
 import com.gitlab.kordlib.common.entity.Snowflake
 import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.behavior.MemberBehavior
+import com.gitlab.kordlib.core.behavior.RoleBehavior
 import com.gitlab.kordlib.core.behavior.UserBehavior
 import com.gitlab.kordlib.core.cache.data.EmojiData
 import com.gitlab.kordlib.core.toSnowflakeOrNull
@@ -11,30 +12,73 @@ import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 
+/**
+ * An instace of a [Discord emoji](https://discordapp.com/developers/docs/resources/emoji#emoji-object) belonging to a specific guild.
+ */
 class GuildEmoji(val data: EmojiData, val guildId: Snowflake, override val kord: Kord) : Entity {
     override val id: Snowflake
         get() = Snowflake(data.id)
 
+    /**
+     * Whether is emoji is animated.
+     */
     val isAnimated: Boolean get() = data.animated
 
+    /**
+     * Whether is emote is managed by Discord instead of the guild members.
+     */
     val isManaged: Boolean get() = data.managed
 
+    /**
+     * The name of this emoji.
+     *
+     * This property can be null when trying to get the name of an emoji that was deleted.
+     */
     val name: String? get() = data.name
 
+    /**
+     * Whether this emoji needs to be wrapped in colons.
+     */
     val requiresColons: Boolean get() = data.requireColons
 
+    /**
+     * The ids of the [roles][Role] for which this emoji was whitelisted.
+     */
     val roleIds: Set<Snowflake> get() = data.roles.asSequence().map { Snowflake(it) }.toSet()
 
+    /**
+     * The behaviors of the [roles][Role] for which this emoji was whitelisted.
+     */
+    val roleBehaviors: Set<RoleBehavior> get() = data.roles.asSequence().map { RoleBehavior(guildId = guildId, id = id, kord = kord) }.toSet()
+
+    /**
+     * The [roles][Role] for which this emoji was whitelisted.
+     */
     val roles: Flow<Role> get() = roleIds.asFlow().map { kord.getRole(guildId, id) }.filterNotNull()
 
+    /**
+     * The behavior of the [Member] who created the emote, if present.
+     */
     val member: MemberBehavior? get() = userId?.let { MemberBehavior(guildId, it, kord) }
 
+    /**
+     * The id of the [User] who created the emote, if present.
+     */
     val userId: Snowflake? get() = data.user?.id.toSnowflakeOrNull()
 
+    /**
+     * The [User] who created the emote, if present.
+     */
     val user: UserBehavior? get() = userId?.let { UserBehavior(it, kord) }
 
+    /**
+     * Requests to get the [Member] who created the emote, if present.
+     */
     suspend fun getMember(): Member? = userId?.let { kord.getMember(guildId = guildId, userId = it) }
 
+    /**
+     * Requests to get the [User] who created the emote, if present.
+     */
     suspend fun getUser(): User? = userId?.let { kord.getUser(it) }
 
 }
--- rest/src/main/kotlin/com/gitlab/kordlib/rest/builder/message/EmbedBuilder.kt
+++ rest/src/main/kotlin/com/gitlab/kordlib/rest/builder/message/EmbedBuilder.kt
@@ -7,32 +7,87 @@ import java.awt.Color
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 
+/**
+ * A builder for discord embeds.
+ *
+ * Inline Markdown links are supported in in all description-like fields.
+ */
 @KordDsl
 class EmbedBuilder : RequestBuilder<EmbedRequest> {
+    /**
+     * The title of the embed. Limited to the length of [Limits.title].
+     */
     var title: String? = null
+
+    /**
+     * The description of the embed. Limited to the length of [Limits.description].
+     */
     var description: String? = null
+
+    /**
+     * The url of the embed's [title].
+     */
     var url: String? = null
+
+    /**
+     * The timestamp displayed at the bottom of the embed.
+     */
     var timestamp: Instant? = null
+
+    /**
+     * The color of the embed.
+     */
     var color: Color? = null
 
+    /**
+     * The image url of the embed.
+     */
     var image: String? = null
+
+    /**
+     * The footer of the embed.
+     */
     var footer: Footer? = null
+
+    /**
+     * The thumbnail of the embed.
+     */
     var thumbnail: Thumbnail? = null
+
+    /**
+     * The author of the embed.
+     */
     var author: Author? = null
+
+    /**
+     * The embed fields.
+     */
     val fields: MutableList<Field> = mutableListOf()
 
+    /**
+     * Adds or updates the [footer] as configured by the [builder].
+     */
     inline fun footer(builder: Footer.() -> Unit) {
         footer = (footer ?: Footer()).apply(builder)
     }
 
+    /**
+     * Adds or updates the [thumbnail] as configured by the [builder].
+     */
     inline fun thumbnail(builder: Thumbnail.() -> Unit) {
         thumbnail = (thumbnail ?: Thumbnail()).apply(builder)
     }
 
+    /**
+     * Adds or updates the [author] as configured by the [builder].
+     */
     inline fun author(builder: Author.() -> Unit) {
         author = (author ?: Author()).apply(builder)
     }
 
+    /**
+     * Adds a new [Field] configured by the [builder].
+     */
     inline fun field(builder: Field.() -> Unit) {
         fields += Field().apply(builder)
     }
@@ -53,6 +108,10 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
 
     @KordDsl
     class Thumbnail : RequestBuilder<EmbedThumbnailRequest> {
+
+        /**
+         * The image url of the thumbnail. This field is required.
+         */
         lateinit var url: String
 
         override fun toRequest() = EmbedThumbnailRequest(url)
@@ -60,27 +119,100 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
 
     @KordDsl
     class Footer : RequestBuilder<EmbedFooterRequest> {
+
+        /**
+         * The text of the footer. This field is required and limited to the length of [Limits.text].
+         */
         lateinit var text: String
+
+        /**
+         * The icon url to displqy.
+         */
         var icon: String? = null
 
         override fun toRequest() = EmbedFooterRequest(text, icon)
+
+        object Limits {
+            const val text = 2048
+        }
     }
 
     @KordDsl
     class Author : RequestBuilder<EmbedAuthorRequest> {
+
+        /**
+         * The name of the author. This field is required if [url] is not null.
+         */
         var name: String? = null
+
+        /**
+         * The link that will be applied to the author's [name]. [name] is a mandatory field if [url] is not null.
+         */
         var url: String? = null
+
+        /**
+         * The image url that will be displayed next to the author's name.
+         */
         var icon: String? = null
 
         override fun toRequest() = EmbedAuthorRequest(name, url, icon)
+
+        object Limits {
+            /**
+             * The maximum length of the [Author.name] field.
+             */
+            const val name = 256
+        }
     }
 
     @KordDsl
-    class Field: RequestBuilder<EmbedFieldRequest> {
+    class Field : RequestBuilder<EmbedFieldRequest> {
+
+        /**
+         *  The value or 'description' of the [Field]. Limited to the length of [Limits.value].
+         */
         lateinit var value: String
-        lateinit  var name: String
+
+        /**
+         * The name or 'title' of the [Field]. Limited in to the length of [Limits.name].
+         */
+        lateinit var name: String
         var inline: Boolean = false
 
         override fun toRequest() = EmbedFieldRequest(name, value, inline)
+
+        object Limits {
+            /**
+             * The maximum length of the [Field.name] field.
+             */
+            const val name = 256
+
+            /**
+             * The maximum length of the [Field.value] field.
+             */
+            const val value = 2048
+        }
+    }
+
+    object Limits {
+        /**
+         * The maximum length of the [EmbedBuilder.title] field.
+         */
+        const val title = 256
+
+        /**
+         * The maximum length of the [EmbedBuilder.description] field.
+         */
+        const val description = 2048
+
+        /**
+         * The maximum amount of [EmbedBuilder.Field] in an [EmbedBuilder].
+         */
+        const val fieldCount = 25
+
+        /**
+         * The maximum length of all text inside the [EmbedBuilder].
+         */
+        const val total = 6000
     }
 }
