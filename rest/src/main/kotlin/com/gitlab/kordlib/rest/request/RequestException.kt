424ed021e9b438fb35414a73f5a0123de6a4213d
HopeBaron <truehope9000@gmail.com>
2020-04-04 12:50:26 +0300

    Add Json error codes (#76)

--- rest/src/main/kotlin/com/gitlab/kordlib/rest/request/RequestException.kt
+++ rest/src/main/kotlin/com/gitlab/kordlib/rest/request/RequestException.kt
@@ -1,7 +1,8 @@
 package com.gitlab.kordlib.rest.request
 
+import com.gitlab.kordlib.rest.json.response.DiscordErrorResponse
 import io.ktor.client.statement.HttpResponse
 
 abstract class RequestException(val code: Int, message: String) : Exception(message)
 
-class KtorRequestException(val response: HttpResponse, message: String) : RequestException(response.status.value, message)
+class KtorRequestException(val response: HttpResponse, message: String, error: DiscordErrorResponse) : RequestException(response.status.value, message)
