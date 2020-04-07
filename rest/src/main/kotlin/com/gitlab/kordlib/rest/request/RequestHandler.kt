59a27cfc94085145098e3a05ca5972dbec81796c
HopeBaron <truehope9000@gmail.com>
2020-04-07 13:15:48 +0300

    Fix incorrect serialization of discord error request

--- rest/src/main/kotlin/com/gitlab/kordlib/rest/request/RequestHandler.kt
+++ rest/src/main/kotlin/com/gitlab/kordlib/rest/request/RequestHandler.kt
@@ -1,7 +1,5 @@
 package com.gitlab.kordlib.rest.request
 
-import com.gitlab.kordlib.rest.json.response.DiscordRequestException
-
 /**
  * Handles Discord API requests.
  */
@@ -11,7 +9,7 @@ interface RequestHandler {
      * Executes the [request], abiding by the active rate limits and returning the response [R].
      * Throws an [RequestException] when a non-rate limit error response is returned.
      */
-    @Throws(DiscordRequestException::class)
+    @Throws(RequestException::class)
     suspend fun <B : Any,R>  handle(request: Request<B,R>): R
 
 }
