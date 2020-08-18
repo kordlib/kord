d360d0aab2f8543582b28015fe4e65594fd47816
BartArys <mysterybtdn@gmail.com>
2020-08-18 12:16:21 +0200

    Document Strategizable

--- core/src/main/kotlin/com/gitlab/kordlib/core/entity/Strategizable.kt
+++ core/src/main/kotlin/com/gitlab/kordlib/core/entity/Strategizable.kt
@@ -3,10 +3,23 @@ package com.gitlab.kordlib.core.entity
 import com.gitlab.kordlib.core.supplier.EntitySupplier
 import com.gitlab.kordlib.core.supplier.EntitySupplyStrategy
 
+/**
+ * A class that will defer the requesting of [Entities][Entity] to a [supplier].
+ * Copies of this class with a different [supplier] can be made through [withStrategy].
+ *
+ * Unless stated otherwise, all members that fetch [Entities][Entity] will delegate to the [supplier].
+ */
 interface Strategizable {
 
+    /**
+     * The supplier used to request entities.
+     */
     val supplier: EntitySupplier
 
+
+    /**
+     * Returns a copy of this class with a new [supplier] provided by the [strategy].
+     */
     fun withStrategy(strategy: EntitySupplyStrategy<*>) : Strategizable
 
 }
