bbae1084ed1024e138fdf8308a14a78872645c1e
BartArys <mysterybtdn@gmail.com>
2020-09-27 16:54:38 +0200

    Have Members consider user equality

--- core/src/test/kotlin/com/gitlab/kordlib/core/entity/MemberTest.kt
+++ core/src/test/kotlin/com/gitlab/kordlib/core/entity/MemberTest.kt
@@ -8,6 +8,8 @@ import equality.GuildEntityEqualityTest
 import io.mockk.every
 import io.mockk.mockk
 import mockKord
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class MemberTest : GuildEntityEqualityTest<Member> by GuildEntityEqualityTest({ id, guildId ->
     val kord = mockKord()
@@ -21,4 +23,22 @@ internal class MemberTest : GuildEntityEqualityTest<Member> by GuildEntityEquali
     Member(memberData, userData, kord)
 }), BehaviorEqualityTest<Member> {
     override fun Member.behavior(): Entity = MemberBehavior(guildId = guildId, id = id, kord = kord)
+
+    @Test
+    fun `members equal users with the same ID`() {
+        val kord = mockKord()
+        val memberData = mockk<MemberData>()
+        every { memberData.userId } returns 0L
+        every { memberData.guildId } returns 1L
+
+        val userData = mockk<UserData>()
+        every { userData.id } returns 0L
+        val member = Member(memberData, userData, kord)
+        val user = User(userData, kord)
+
+        assertEquals(member, user)
+        assertEquals(user, member)
+    }
+
+
 }
