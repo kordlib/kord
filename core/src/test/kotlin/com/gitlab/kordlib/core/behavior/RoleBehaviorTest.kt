9ac435c96b8b183524d747877eb6bf3707d35047
BartArys <mysterybtdn@gmail.com>
2020-10-26 08:04:18 +0100

    Add test for role mention fix

--- core/src/test/kotlin/com/gitlab/kordlib/core/behavior/RoleBehaviorTest.kt
+++ core/src/test/kotlin/com/gitlab/kordlib/core/behavior/RoleBehaviorTest.kt
@@ -1,9 +1,25 @@
 package com.gitlab.kordlib.core.behavior
 
+import com.gitlab.kordlib.common.entity.Snowflake
 import equality.GuildEntityEqualityTest
+import io.mockk.mockk
 import mockKord
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class RoleBehaviorTest : GuildEntityEqualityTest<RoleBehavior> by GuildEntityEqualityTest({ id, guildId ->
     val kord = mockKord()
     RoleBehavior(guildId = guildId, id = id, kord = kord)
-})
+}) {
+
+    @Test
+    fun `everyone role mention is properly formatted`(){
+        val kord = mockKord()
+
+        val id = Snowflake(1337)
+        val behavior = RoleBehavior(id, id, kord)
+
+        assertEquals("@everyone", behavior.mention)
+    }
+
+}
