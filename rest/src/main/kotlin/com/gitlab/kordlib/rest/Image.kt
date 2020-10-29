3d7a7ddadfcf5e406603520dfbf9dcaf999db1d1
Animeshz <animeshsahu19@yahoo,com>
2020-10-29 17:08:00 +0530

    Code Cleanup, that was left over (without any usage).

--- rest/src/main/kotlin/com/gitlab/kordlib/rest/Image.kt
+++ rest/src/main/kotlin/com/gitlab/kordlib/rest/Image.kt
@@ -2,19 +2,11 @@ package com.gitlab.kordlib.rest
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.request
-import io.ktor.client.statement.*
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpMethod
 import io.ktor.util.toByteArray
 import kotlinx.coroutines.Dispatchers
-import mu.KotlinLogging
-import java.awt.Dimension
-import java.io.File
-import java.io.IOException
 import java.util.*
-import javax.imageio.ImageIO
-import javax.imageio.stream.ImageInputStream
-
-private val logger = KotlinLogging.logger { }
 
 class Image private constructor(val data: ByteArray, val format: Format) {
 
@@ -32,13 +24,12 @@ class Image private constructor(val data: ByteArray, val format: Format) {
         suspend fun fromUrl(client: HttpClient, url: String): Image = with(Dispatchers.IO) {
             val call = client.request<HttpResponse>(url) { method = HttpMethod.Get }
             val contentType = call.headers["Content-Type"]
-                ?: error("expected 'Content-Type' header in image request")
+                    ?: error("expected 'Content-Type' header in image request")
 
             @Suppress("EXPERIMENTAL_API_USAGE")
             val bytes = call.content.toByteArray()
-            val format = Format.fromContentType(contentType)
 
-            Image(bytes, format)
+            Image(bytes, Format.fromContentType(contentType))
         }
     }
 
@@ -61,7 +52,7 @@ class Image private constructor(val data: ByteArray, val format: Format) {
 
     /**
      * Represents size of the [Image], for requesting different sizes from the discord.
-     * [Image.Resolution] (both height and width) will always be smaller than or equal to [maxRes] of the [Size].
+     * Both height and width will always be smaller than or equal to [maxRes] of the [Size].
      */
     enum class Size(val maxRes: Int) {
         Size16(16),
