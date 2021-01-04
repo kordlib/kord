040b0fcc2c652d233c8c12622bf7123b15ba24f2
HopeBaron <truehope9000@gmail.com>
2021-01-04 20:14:32 +0200

    expose the list of channels

--- rest/src/main/kotlin/builder/guild/WelcomeScreenModifyBuilder.kt
+++ rest/src/main/kotlin/builder/guild/WelcomeScreenModifyBuilder.kt
@@ -5,8 +5,12 @@ import dev.kord.common.entity.Snowflake
 import dev.kord.common.entity.optional.Optional
 import dev.kord.common.entity.optional.OptionalBoolean
 import dev.kord.common.entity.optional.delegate.delegate
+import dev.kord.common.entity.optional.map
+import dev.kord.common.entity.optional.mapList
 import dev.kord.rest.builder.RequestBuilder
 import dev.kord.rest.json.request.GuildWelcomeScreenModifyRequest
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 class WelcomeScreenModifyBuilder : RequestBuilder<GuildWelcomeScreenModifyRequest> {
 
@@ -16,17 +20,31 @@ class WelcomeScreenModifyBuilder : RequestBuilder<GuildWelcomeScreenModifyReques
     private var _description: Optional<String> = Optional.Missing()
     var description: String? by ::_description.delegate()
 
-    private var _welcomeScreenChannels: Optional<MutableList<DiscordWelcomeScreenChannel>> = Optional.Missing()
-    private var welcomeScreenChannels: MutableList<DiscordWelcomeScreenChannel>? by ::_welcomeScreenChannels.delegate()
+    private var _welcomeScreenChannels: Optional<MutableList<WelcomeScreenChannelBuilder>> = Optional.Missing()
+     var welcomeScreenChannels: MutableList<WelcomeScreenChannelBuilder>? by ::_welcomeScreenChannels.delegate()
 
-    fun welcomeChannel(id: Snowflake, description: String, builder: DiscordWelcomeScreenChannel.() -> Unit) {
+    fun welcomeChannel(id: Snowflake, description: String, builder: WelcomeScreenChannelBuilder.() -> Unit) {
 
         if (welcomeScreenChannels == null) welcomeScreenChannels = mutableListOf()
-        welcomeScreenChannels!!.add(DiscordWelcomeScreenChannel(id, description, null, null).apply(builder))
+        welcomeScreenChannels!!.add(WelcomeScreenChannelBuilder(id, description, null, null).apply(builder))
     }
 
     override fun toRequest(): GuildWelcomeScreenModifyRequest {
-        return GuildWelcomeScreenModifyRequest(_enabled, _welcomeScreenChannels, _description)
+        return GuildWelcomeScreenModifyRequest(_enabled, _welcomeScreenChannels.mapList { it.toRequest() }, _description)
+    }
+
+}
+
+
+
+class WelcomeScreenChannelBuilder(
+    var channelId: Snowflake,
+    var description: String,
+    var emojiId: Snowflake?,
+    var emojiName: String?
+): RequestBuilder<DiscordWelcomeScreenChannel> {
+    override fun toRequest(): DiscordWelcomeScreenChannel {
+        return DiscordWelcomeScreenChannel(channelId, description, emojiId, emojiName)
     }
 
 }
