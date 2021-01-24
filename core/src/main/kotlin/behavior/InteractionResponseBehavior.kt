41bafec6f2b39c8c1a95fef6f8f3c1d98fc2ef9b
Hope <34831095+HopeBaron@users.noreply.github.com>
2021-01-24 21:04:16 +0200

    Allow using the message behaviors in a followup (#175)
    * Allow using the message behaviors in a followup
    
    The message is now associated with the follow-up object allowing the use of its behaviors.
    
    * Apply suggestions
    
    * improve interaction followup structuring
    
    * Make ids computed
    
    * Don't add empty lines after docs

--- core/src/main/kotlin/behavior/InteractionResponseBehavior.kt
+++ core/src/main/kotlin/behavior/InteractionResponseBehavior.kt
@@ -7,7 +7,7 @@ import dev.kord.core.KordObject
 import dev.kord.core.cache.data.MessageData
 import dev.kord.core.cache.data.toData
 import dev.kord.core.entity.Message
-import dev.kord.core.entity.interaction.FollowupMessage
+import dev.kord.core.entity.interaction.InteractionFollowup
 import dev.kord.rest.builder.interaction.FollowupMessageCreateBuilder
 import dev.kord.rest.builder.interaction.InteractionResponseModifyBuilder
 import kotlin.contracts.ExperimentalContracts
@@ -69,10 +69,10 @@ suspend inline fun EditableInteractionResponseBehavior.edit(builder: Interaction
 
 @KordPreview
 @OptIn(ExperimentalContracts::class)
-suspend inline fun InteractionResponseBehavior.followUp(builder: FollowupMessageCreateBuilder.() -> Unit): FollowupMessage {
+suspend inline fun InteractionResponseBehavior.followUp(builder: FollowupMessageCreateBuilder.() -> Unit): InteractionFollowup {
     contract { callsInPlace(builder, InvocationKind.EXACTLY_ONCE) }
     val request = FollowupMessageCreateBuilder().apply(builder).toRequest()
     val response = kord.rest.interaction.createFollowupMessage(applicationId, token, request)
     val data = MessageData.from(response)
-    return FollowupMessage(data, token, applicationId, kord)
+    return InteractionFollowup(Message(data, kord), token, applicationId, kord)
 }
---
+++ core/src/main/kotlin/entity/interaction/InteractionFollowup.kt
@@ -0,0 +1,39 @@
+package dev.kord.core.entity.interaction
+
+import dev.kord.common.annotation.KordPreview
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.behavior.InteractionFollowupBehavior
+import dev.kord.core.cache.data.MessageData
+import dev.kord.core.entity.*
+import dev.kord.core.entity.channel.MessageChannel
+
+/**
+ * Holds the follow-up [Message] resulting from an interaction follow-up
+ * and behaves on it through [InteractionFollowupBehavior]
+ *
+ * @param message The message created by this follow-up.
+ * To use the message behavior your application must be authorized as a bot.
+ * @param token The unique token used to follow-up the Interaction Response.
+ * @param applicationId the application id of bot invoking the follow-up
+ * @param kord The kord instance responsible for the follow-up
+ */
+@KordPreview
+class InteractionFollowup(
+     val message: Message,
+    override val token: String,
+    override val applicationId: Snowflake,
+    override val kord: Kord,
+) : InteractionFollowupBehavior {
+
+    /**
+     * The id of the follow-up message.
+     */
+    override val id: Snowflake get() = message.id
+
+    /**
+     * The id of the [MessageChannel] the follow-up message was send in.
+     */
+    override val channelId: Snowflake get() = message.channelId
+
+}
