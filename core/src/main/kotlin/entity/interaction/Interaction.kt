b96dfd4f4bd27103b78ccaeebf8aebb5d6925de1
HopeBaron <truehope9000@gmail.com>
2021-02-14 17:09:53 +0200

    Move documents to declaration and rename Command

--- core/src/main/kotlin/entity/interaction/Interaction.kt
+++ core/src/main/kotlin/entity/interaction/Interaction.kt
@@ -7,29 +7,16 @@ import dev.kord.common.entity.Permissions
 import dev.kord.common.entity.Snowflake
 import dev.kord.common.entity.optional.*
 import dev.kord.core.Kord
-import dev.kord.core.KordObject
 import dev.kord.core.behavior.GuildBehavior
 import dev.kord.core.behavior.InteractionBehavior
 import dev.kord.core.behavior.MemberBehavior
 import dev.kord.core.behavior.channel.TextChannelBehavior
 import dev.kord.core.cache.data.ApplicationCommandInteractionData
 import dev.kord.core.cache.data.InteractionData
-import dev.kord.core.cache.data.OptionData
-import dev.kord.core.entity.Entity
 import dev.kord.core.supplier.EntitySupplier
 
 /**
  * Interaction that can respond to interactions and follow them up.
- *
- * @property id interaction's id.
- * @property channelId the channel id where the interaction took place.
- * @property token a continuation token for responding to the interaction
- * @property guildId the id of the guild where the interaction took place.
- * @property permissions the permissions of the member with the overwrites.
- * @property type the type of the interaction.
- * @property member the invoker of the command as [MemberBehavior].
- * @property command [Command] object that contains the data related to the interaction's command.
- * @property version read-only property, always 1
  */
 @KordPreview
 class Interaction(
@@ -41,37 +28,67 @@ class Interaction(
 
     override val id: Snowflake get() = data.id
 
+    /**
+     * The channel id where the interaction took place.
+     */
     override val channelId: Snowflake get() = data.channelId
 
+    /**
+     * a continuation token for responding to the interaction
+     */
     override val token: String get() = data.token
 
+    /**
+     * The id of the guild where the interaction took place.
+     */
     override val guildId: Snowflake get() = data.guildId
 
+    /**
+     * The type of the interaction.
+     */
     val type: InteractionType get() = data.type
 
+    /**
+     * The permissions of the member with the overwrites.
+     */
     val permissions: Permissions get() = data.permissions
 
+    /**
+     * The [TextChannelBehavior] of the channel the command was executed in.
+     */
     val channel: TextChannelBehavior get() = TextChannelBehavior(id = channelId, guildId = guildId, kord = kord)
 
+    /**
+     * The [GuildBehavior] for the guild the command was executed in.
+     */
     val guild get() = GuildBehavior(guildId, kord)
 
+    /**
+     * The invoker of the command as [MemberBehavior].
+     */
     val member: MemberBehavior get() = MemberBehavior(data.guildId, data.member.userId, kord)
 
-    val command: Command
-        get() = Command(data.data)
+    /**
+     * [InteractionCommand] object that contains the data related to the interaction's command.
+     */
+    val command: InteractionCommand
+        get() = InteractionCommand(data.data)
 
+    /**
+     * read-only property, always 1
+     */
     val version: Int get() = data.version
 }
 
 @KordPreview
-sealed class Command {
+sealed class InteractionCommand {
 
     abstract val rootId: Snowflake
     abstract val rootName: String
     abstract val options: Map<String, OptionValue<*>>
 
     companion object {
-        operator fun invoke(data: ApplicationCommandInteractionData): Command {
+        operator fun invoke(data: ApplicationCommandInteractionData): InteractionCommand {
             val firstLevelOptions = data.options.orEmpty()
             val rootPredicate = firstLevelOptions.isEmpty() || firstLevelOptions.any { it.value.value != null }
             val groupPredicate = firstLevelOptions.any { it.subCommands.orEmpty().isNotEmpty() }
@@ -88,9 +105,9 @@ sealed class Command {
 }
 
 @KordPreview
-class RootCommand(val data: ApplicationCommandInteractionData) : Command() {
+class RootCommand(val data: ApplicationCommandInteractionData) : InteractionCommand() {
 
-     override val rootId: Snowflake
+    override val rootId: Snowflake
         get() = data.id
 
     override val rootName get() = data.name
@@ -102,7 +119,7 @@ class RootCommand(val data: ApplicationCommandInteractionData) : Command() {
 }
 
 @KordPreview
-class SubCommand(val data: ApplicationCommandInteractionData) : Command() {
+class SubCommand(val data: ApplicationCommandInteractionData) : InteractionCommand() {
 
     private val subCommandData = data.options.orEmpty().first()
 
@@ -111,7 +128,7 @@ class SubCommand(val data: ApplicationCommandInteractionData) : Command() {
     override val rootId: Snowflake
         get() = data.id
 
-     val name get() = subCommandData.name
+    val name get() = subCommandData.name
 
     override val options: Map<String, OptionValue<*>>
         get() = subCommandData.values.orEmpty()
@@ -120,7 +137,7 @@ class SubCommand(val data: ApplicationCommandInteractionData) : Command() {
 }
 
 @KordPreview
-class GroupCommand(val data: ApplicationCommandInteractionData) : Command() {
+class GroupCommand(val data: ApplicationCommandInteractionData) : InteractionCommand() {
 
     private val groupData get() = data.options.orEmpty().first()
     private val subCommandData get() = groupData.subCommands.orEmpty().first()
