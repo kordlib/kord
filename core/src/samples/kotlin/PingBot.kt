94ec068862df0ed8ced7d9f9609add588a02d74c
Michael Rittmeister <michael@rittmeister.in>
2021-02-28 17:23:01 +0100

    Update example - Now I need to wait 2 hours

--- core/src/samples/kotlin/PingBot.kt
+++ core/src/samples/kotlin/PingBot.kt
@@ -1,13 +1,34 @@
+import dev.kord.common.annotation.KordPreview
 import dev.kord.core.Kord
+import dev.kord.core.behavior.respond
+import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.core.event.interaction.InteractionCreateEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 
+@OptIn(KordPreview::class)
 suspend fun main(args: Array<String>) {
     val kord = Kord(args.firstOrNull() ?: error("token required"))
 
     kord.on<MessageCreateEvent> {
         if (message.author?.isBot == true) return@on
         if (message.content == "!ping") message.channel.createMessage("pong")
+    }
+
+    kord.on<ReadyEvent> {
+        kord.slashCommands.createGlobalApplicationCommand(
+            "ping", "Allows you to play ping pong"
+        )
+    }
+
+    kord.on<InteractionCreateEvent> {
+        when (interaction.command.name) {
+            "ping" -> {
+                interaction.respond(true) {
+                    content = "Pong!"
+                }
+            }
+        }
     }
 
     kord.login { playing("!ping to pong") }
