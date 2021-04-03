34063702c9f6843eeee26ef2c9591fc7de9eca95
Hope <34831095+HopeBaron@users.noreply.github.com>
2021-04-03 13:30:48 +0300

    Update Projects.kt

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -15,17 +15,25 @@ val isJitPack get() = "true" == System.getenv("JITPACK")
 object Library {
     const val name = "kord"
     const val group = "dev.kord"
-    val version: String get() = if (isJitPack) System.getenv("RELEASE_TAG")
-    else {
-        val tag = System.getenv("GITHUB_TAG_NAME")
-        val branch = System.getenv("GITHUB_BRANCH_NAME")
-        if (tag != null) tag
-        else if (branch != null && branch.startsWith("refs/heads/")) branch.substringAfter("refs/heads/")
-        else throw error("Couldn't find a tag name or branch.")
-    }
+    val version: String
+        get() = if (isJitPack) System.getenv("RELEASE_TAG")
+        else {
+            val tag = System.getenv("GITHUB_TAG_NAME")
+            val branch = System.getenv("GITHUB_BRANCH_NAME")
+            when {
+                tag != null -> tag
+                branch != null && branch.startsWith("refs/heads/") ->
+                    branch.substringAfter("refs/heads/") + "-SNAPSHOT"
+                else -> "undefined"
+            }
+        }
 
     const val description = "Idiomatic Kotlin Wrapper for The Discord API"
     const val projectUrl = "https://github.com/kordlib/kord"
+
+    val isSnapshot: Boolean get() = version.endsWith("-SNAPSHOT")
+
+    val isRelease: Boolean get() = !isSnapshot
 
     /**
      * Whether the current API is considered stable, and should be compared to the 'golden' API dump.
