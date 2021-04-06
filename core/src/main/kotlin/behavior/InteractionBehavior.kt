338d3253cfbd8ee1036c913746ddf2660d541658
HopeBaron <truehope9000@gmail.com>
2021-04-06 22:14:35 +0300

    acknowledgement message flags

--- core/src/main/kotlin/behavior/InteractionBehavior.kt
+++ core/src/main/kotlin/behavior/InteractionBehavior.kt
@@ -2,17 +2,17 @@ package dev.kord.core.behavior
 
 import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.InteractionResponseType
+import dev.kord.common.entity.MessageFlags
 import dev.kord.common.entity.Snowflake
 import dev.kord.common.entity.optional.optional
 import dev.kord.core.Kord
-import dev.kord.core.cache.data.InteractionData
-import dev.kord.core.entity.Guild
 import dev.kord.core.entity.KordEntity
 import dev.kord.core.entity.Strategizable
 import dev.kord.core.entity.channel.Channel
 import dev.kord.core.supplier.EntitySupplier
 import dev.kord.core.supplier.EntitySupplyStrategy
 import dev.kord.rest.builder.interaction.InteractionApplicationCommandCallbackDataBuilder
+import dev.kord.rest.json.request.InteractionApplicationCommandCallbackData
 import dev.kord.rest.json.request.InteractionResponseCreateRequest
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
@@ -31,8 +31,15 @@ interface InteractionBehavior : KordEntity, Strategizable {
      * @param source weather to show the author's name and provided arguments of the command.
      * @return [InteractionResponseBehavior] which can be used to create follow-up message or edit the original response.
      */
-    suspend fun acknowledge(): InteractionResponseBehavior {
-        val request = InteractionResponseCreateRequest(InteractionResponseType.DeferredChannelMessageWithSource)
+    suspend fun acknowledge(flags: MessageFlags): InteractionResponseBehavior {
+
+        val builder = InteractionApplicationCommandCallbackDataBuilder()
+        builder.flags = flags
+
+        val request = InteractionResponseCreateRequest(
+            InteractionResponseType.DeferredChannelMessageWithSource,
+            builder.build().optional()
+        )
         kord.rest.interaction.createInteractionResponse(id, token, request)
         return InteractionResponseBehavior(applicationId, token, kord)
     }
@@ -49,7 +56,7 @@ interface InteractionBehavior : KordEntity, Strategizable {
 }
 
 @KordPreview
- fun InteractionBehavior(
+fun InteractionBehavior(
     id: Snowflake,
     channelId: Snowflake,
     token: String,
@@ -57,26 +64,26 @@ interface InteractionBehavior : KordEntity, Strategizable {
     kord: Kord,
     strategy: EntitySupplyStrategy<*> = kord.resources.defaultStrategy
 ) = object : InteractionBehavior {
-        override val id: Snowflake
-            get() = id
+    override val id: Snowflake
+        get() = id
 
-        override val token: String
-            get() = token
+    override val token: String
+        get() = token
 
-        override val applicationId: Snowflake
-            get() = applicationId
+    override val applicationId: Snowflake
+        get() = applicationId
 
-        override val kord: Kord
-            get() = kord
+    override val kord: Kord
+        get() = kord
 
-        override val channelId: Snowflake
-            get() = channelId
+    override val channelId: Snowflake
+        get() = channelId
 
 
-        override val supplier: EntitySupplier
-            get() = strategy.supply(kord)
+    override val supplier: EntitySupplier
+        get() = strategy.supply(kord)
 
-    }
+}
 
 /**
  * Acknowledges an interaction and responds with [InteractionResponseBehavior] built using [builder].
@@ -97,4 +104,11 @@ suspend inline fun InteractionBehavior.respond(
     kord.rest.interaction.createInteractionResponse(id, token, request)
     return InteractionResponseBehavior(applicationId, token, kord)
 
+}
+
+@OptIn(ExperimentalContracts::class)
+suspend inline fun InteractionBehavior.acknowledge(builder: MessageFlags.Builder.() -> Unit = {}) {
+    contract { callsInPlace(builder, InvocationKind.EXACTLY_ONCE) }
+    val flags = MessageFlags.Builder().apply(builder).flags()
+    acknowledge(flags)
 }
