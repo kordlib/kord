77a52ff8bbff98b90189346161e6df2d8e7aa1e8
Hope <34831095+HopeBaron@users.noreply.github.com>
2021-04-20 19:30:52 +0300

    Fix master's inability to generate docs (#266)

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,6 @@ subprojects {
 
 
     tasks.dokkaHtml.configure {
-        onlyIf { Library.isRelease }
         this.outputDirectory.set(file("${project.projectDir}/dokka/kord/"))
 
         dokkaSourceSets {
