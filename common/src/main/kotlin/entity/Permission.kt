e6c10a6e1ee3d6bf68f16dc009b7e4ebd9a207f5
BartArys <mysterybtdn@gmail.com>
2021-05-06 20:54:16 +0200

    Optimize Permission All
    The All Permission folded each DiscordBitSet of each value into eachother, resulting in n + 1 bitsets being created. This commit changes that to use the internal `add` which instead, which only mutates the single bitset created.

--- common/src/main/kotlin/entity/Permission.kt
+++ common/src/main/kotlin/entity/Permission.kt
@@ -155,7 +155,7 @@ sealed class Permission(val code: DiscordBitSet) {
     object ManageEmojis : Permission(0x40000000)
     object UseSlashCommands : Permission(0x80000000)
     object RequestToSpeak : Permission(0x100000000)
-    object All : Permission(Permission.values.fold(EmptyBitSet()) { acc, value -> acc + value.code })
+    object All : Permission(values.fold(EmptyBitSet()) { acc, value -> acc.add(value.code); acc })
 
     companion object {
         val values: Set<Permission>
