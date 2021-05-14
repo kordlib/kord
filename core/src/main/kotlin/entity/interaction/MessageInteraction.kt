9c059028ebe2e08c394e030d5e9df4205ce4b737
HopeBaron <truehope9000@gmail.com>
2021-05-14 15:03:40 +0300

    Implement Strategizable for MessageInteraction

--- core/src/main/kotlin/entity/interaction/MessageInteraction.kt
+++ core/src/main/kotlin/entity/interaction/MessageInteraction.kt
@@ -4,10 +4,16 @@ import cache.data.MessageInteractionData
 import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.InteractionType
 import dev.kord.common.entity.Snowflake
+import dev.kord.common.exception.RequestException
 import dev.kord.core.Kord
 import dev.kord.core.behavior.UserBehavior
 import dev.kord.core.entity.KordEntity
 import dev.kord.core.entity.Message
+import dev.kord.core.entity.Strategizable
+import dev.kord.core.entity.User
+import dev.kord.core.exception.EntityNotFoundException
+import dev.kord.core.supplier.EntitySupplier
+import dev.kord.core.supplier.EntitySupplyStrategy
 
 /**
  * An instance of [MessageInteraction](https://discord.com/developers/docs/interactions/slash-commands#messageinteraction)
@@ -17,7 +23,8 @@ import dev.kord.core.entity.Message
 class MessageInteraction(
     val data: MessageInteractionData,
     override val kord: Kord,
-) : KordEntity {
+    override val supplier: EntitySupplier = kord.defaultSupplier
+) : KordEntity, Strategizable {
     /**
      * [id][Interaction.id] of the [Interaction] this message is responding to.
      */
@@ -37,4 +44,24 @@ class MessageInteraction(
      * the [InteractionType] of the interaction [MessageInteraction].
      */
     val type: InteractionType get() = data.type
+
+    /**
+     * Requests the [User] of this interaction message.
+     *
+     * @throws RequestException if something went wrong while retrieving the user.
+     * @throws EntityNotFoundException if the user was null.
+     */
+    suspend fun getUser(): User = supplier.getUser(user.id)
+
+    /**
+     * Requests to get the user of this interaction message,
+     * returns null if the [User] isn't present.
+     *
+     * @throws [RequestException] if anything went wrong during the request.
+     */
+    suspend fun getUserOrNull(): User? = supplier.getUserOrNull(user.id)
+
+    override fun withStrategy(strategy: EntitySupplyStrategy<*>): Strategizable {
+        return MessageInteraction(data, kord, strategy.supply(kord))
+    }
 }
