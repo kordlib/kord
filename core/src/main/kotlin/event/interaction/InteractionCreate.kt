8841b2a3763669b2ad80120ac43aeb95c4ff0e83
HopeBaron <truehope9000@gmail.com>
2021-05-15 22:13:03 +0300

    apply suggestions and add more details

--- core/src/main/kotlin/event/interaction/InteractionCreate.kt
+++ core/src/main/kotlin/event/interaction/InteractionCreate.kt
@@ -10,19 +10,26 @@ import dev.kord.core.event.Event
  * This event fires when an interaction is created.
  *
  *
- * Discord currently have one type of interactions,
+ * Discord currently has one type of interaction,
  * [Slash Commands][dev.kord.core.entity.interaction.ApplicationCommand].
  *
- * The event should be acknowledged using one of the following methods:
+ * The event should be acknowledged  withing 3 seconds of reception using one of the following methods:
  * * [acknowledgeEphemeral][Interaction.acknowledgeEphemeral] - acknowledges an interaction ephemerally.
  * * [acknowledgePublic][Interaction.acknowledgePublic] - acknowledges an interaction in public.
  * * [respondPublic][Interaction.respondPublic] - same as public acknowledgement, but an immediate result (message) can be supplied.
  * * [respondEphemeral][Interaction.respondEphemeral] - same as ephemeral acknowledgement, but an immediate result (message) can be supplied.
  *
  * Once an interaction has been acknowledged,
- * you can use the [PublicInteractionResponseBehavior.followUp] or [EphemeralInteractionResponseBehavior.followUp].
+ * you can use [PublicInteractionResponseBehavior.followUp] or [EphemeralInteractionResponseBehavior.followUp] to display additional messages.
  *
- * The resulting followup message and its methods may defer based on which method is used.
+ * The resulting follow-up message and its methods may differ based on which method is used.
+ * * Following up an acknowledgement results in replacing "The bot is thinking prompt" with the follow-up content.
+ * * Following up a respond results in a completely new message instance.
+ *
+ * As such, due to how Discord handles ephemeral acknowledgements,
+ * a follow-up on a  ephemeral acknowledgement will result in an ephemeral message.
+ *
+ * In the current iteration, ephemeral messages (regardless of the type) doesn't support files and/or embeds.
  */
 @KordPreview
 class InteractionCreateEvent(
