da1e509123c16a488d7c402e75218d5efa026c0a
Distractic <distractic@outlook.fr>
2021-05-29 16:31:09 +0200

    chore: format

--- core/src/main/kotlin/live/LiveKordEntity.kt
+++ core/src/main/kotlin/live/LiveKordEntity.kt
@@ -34,7 +34,6 @@ abstract class AbstractLiveKordEntity(final override val kord: Kord, dispatcher:
     final override val events: SharedFlow<Event> =
         kord.events.filter { filter(it) }.onEach { update(it) }.shareIn(this, SharingStarted.Eagerly)
 
-
     protected abstract fun filter(event: Event): Boolean
     protected abstract fun update(event: Event)
 
