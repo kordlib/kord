1f4eeb434cdc6f78b3fb8b09d8fd6c6695d5560f
BartArys <mysterybtdn@gmail.com>
2021-06-02 11:49:47 +0200

    Update ktor to 1.6.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val kotlin = "1.5.10"
     const val kotlinxSerialization = "1.2.1"
-    const val ktor = "1.5.4"
+    const val ktor = "1.6.0"
     const val kotlinxCoroutines = "1.5.0"
     const val kotlinLogging = "2.0.6"
     const val dateTime = "0.2.1"
