43fd5158d92569c8d8dac80902b80eead360462a
Hope <34831095+HopeBaron@users.noreply.github.com>
2021-06-18 11:01:27 +0300

    Fix followups method signiture (#321)
    * Fix followups
    
    * rename followup functions

--- core/src/main/kotlin/behavior/interaction/EphemeralInteractionResponseBehavior.kt
+++ core/src/main/kotlin/behavior/interaction/EphemeralInteractionResponseBehavior.kt
@@ -45,9 +45,8 @@ suspend inline fun EphemeralInteractionResponseBehavior.edit(builder: EphemeralI
  */
 @OptIn(ExperimentalContracts::class)
 @KordPreview
-suspend inline fun EphemeralInteractionResponseBehavior.followUp(
-    content: String,
-    builder: EphemeralFollowupMessageCreateBuilder.() -> Unit = {}
+suspend inline fun EphemeralInteractionResponseBehavior.followUpEphemeral(
+    builder: EphemeralFollowupMessageCreateBuilder.() -> Unit
 ): EphemeralFollowupMessage {
     contract { callsInPlace(builder, InvocationKind.EXACTLY_ONCE) }
     val builder = EphemeralFollowupMessageCreateBuilder().apply(builder)
@@ -68,7 +67,7 @@ suspend inline fun EphemeralInteractionResponseBehavior.followUp(
 @OptIn(ExperimentalContracts::class)
 @KordPreview
 @KordUnsafe
-suspend inline fun EphemeralInteractionResponseBehavior.followUp(
+suspend inline fun EphemeralInteractionResponseBehavior.followUpPublic(
     builder: PublicFollowupMessageCreateBuilder.() -> Unit
 ): PublicFollowupMessage {
     contract { callsInPlace(builder, InvocationKind.EXACTLY_ONCE) }
