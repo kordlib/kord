505c405641f13c63743e4b1fc5015e83819f6efe
HopeBaron <truehope9000@gmail.com>
2021-08-02 18:56:10 +0300

    add documentation and remove deprecated functions/properties

--- core/src/main/kotlin/entity/interaction/ApplicationCommand.kt
+++ core/src/main/kotlin/entity/interaction/ApplicationCommand.kt
@@ -1,6 +1,5 @@
 package dev.kord.core.entity.interaction
 
-import dev.kord.common.annotation.DeprecatedSinceKord
 import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.ApplicationCommandOptionType
 import dev.kord.common.entity.Snowflake
@@ -17,9 +16,13 @@ abstract class ApplicationCommand(
     val data: ApplicationCommandData,
     override val service: InteractionService
 ) : ApplicationCommandBehavior {
+
     override val id: Snowflake
         get() = data.id
 
+    /**
+     * The id of the targeted application for this command
+     */
     override val applicationId: Snowflake
         get() = data.applicationId
 
@@ -29,23 +32,11 @@ abstract class ApplicationCommand(
     val description: String
         get() = data.description
 
-    val version: Snowflake get() = data.version
-
     /**
-     * Whether this command is the default command suggested in a hierarchy of commands.
-     * Only one (sub)command can be the default and will be suggested first.
-     * If no (sub)command is the default then the top command will become the implicit default.
+     * [Snowflake] of last time the command was edited
      */
-    @DeprecatedSinceKord("0.7.0-SNAPSHOT")
-    @Deprecated(
-        "For the next iteration of slash commands, moving subcommands and groups into an autocomplete type will not be supported",
-        level = DeprecationLevel.ERROR,
-        replaceWith = ReplaceWith("true")
-    )
-    val isDefault: Boolean
-        get() = groups.values.flatMap { it.subcommands.values }
-            .any { it.data.isDefault.discordBoolean }
-                || subCommands.values.any { it.data.isDefault.discordBoolean }
+
+    val version: Snowflake get() = data.version
 
     /**
      * The groups of this command, each group contains at least one [sub command][ApplicationCommandSubCommand].
@@ -111,20 +102,6 @@ class ApplicationCommandSubCommand(
      * The description of the sub command.
      */
     val description: String get() = data.description
-
-    /**
-     * Whether this command is the default command suggested in a hierarchy of commands.
-     * Only one (sub)command can be the default and will be suggested first.
-     * If no (sub)command is the default then the top command will become the implicit default.
-     */
-    @DeprecatedSinceKord("0.7.0-SNAPSHOT")
-    @Deprecated(
-        "For the next iteration of slash commands, moving subcommands and groups into an autocomplete type will not be supported",
-        level = DeprecationLevel.ERROR,
-        replaceWith = ReplaceWith("false")
-    )
-    val isDefault: Boolean
-        get() = data.isDefault.discordBoolean
 
     /**
      * The parameters of this sub command. Is empty if the command takes no parameters.
