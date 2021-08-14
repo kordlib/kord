fbf7819699ce058ccfb3fa4ae535bb38f1dd91ff
Lost <im@lostillusion.net>
2021-08-14 03:57:25 -0400

    shortcut to connect to a VoiceChannel

--- core/src/main/kotlin/entity/channel/VoiceChannel.kt
+++ core/src/main/kotlin/entity/channel/VoiceChannel.kt
@@ -7,8 +7,11 @@ import dev.kord.core.behavior.channel.GuildChannelBehavior
 import dev.kord.core.behavior.channel.TopGuildChannelBehavior
 import dev.kord.core.behavior.channel.VoiceChannelBehavior
 import dev.kord.core.cache.data.ChannelData
+import dev.kord.core.exception.GatewayNotFoundException
 import dev.kord.core.supplier.EntitySupplier
 import dev.kord.core.supplier.EntitySupplyStrategy
+import dev.kord.voice.VoiceConnection
+import dev.kord.voice.VoiceConnectionBuilder
 import java.util.*
 
 /**
@@ -53,4 +56,25 @@ class VoiceChannel(
         return "VoiceChannel(data=$data, kord=$kord, supplier=$supplier)"
     }
 
+    /**
+     * Connect to this [VoiceChannel] and create a [VoiceConnection] for this voice session.
+     *
+     * @param builder a builder for the [VoiceConnection].
+     * @throws GatewayNotFoundException when there is no associated [dev.kord.gateway.Gateway] for the [dev.kord.core.entity.Guild] this channel is in.
+     * @throws dev.kord.voice.exception.VoiceConnectionInitializationException when there was a problem retrieving voice information from Discord.
+     * @return a [VoiceConnection] representing the connection to this [VoiceConnection].
+     */
+    suspend fun connect(builder: VoiceConnectionBuilder.() -> Unit): VoiceConnection {
+        val voiceConnection = VoiceConnection(
+            getGuild().gateway ?: GatewayNotFoundException.voiceConnectionGatewayNotFound(guildId),
+            kord.selfId,
+            id,
+            guildId,
+            builder
+        )
+
+        voiceConnection.connect()
+
+        return voiceConnection
+    }
 }
