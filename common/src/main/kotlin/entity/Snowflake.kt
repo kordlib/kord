c47caa04f99e2bccc3a8054ec908d7651fe377a7
Lukellmann <lukellmann@gmail.com>
2021-08-24 13:53:50 +0200

    Deduplicate timeStamp creation logic in Snowflake.timeStamp and Snowflake.timeMark.

--- common/src/main/kotlin/entity/Snowflake.kt
+++ common/src/main/kotlin/entity/Snowflake.kt
@@ -34,7 +34,7 @@ class Snowflake(val value: Long) : Comparable<Snowflake> {
 
     val timeStamp: Instant get() = Instant.fromEpochMilliseconds(discordEpochLong + (value shr 22))
 
-    val timeMark: TimeMark get() = SnowflakeMark(discordEpochLong + (value shr 22))
+    val timeMark: TimeMark get() = SnowflakeMark(timeStamp)
 
     override fun compareTo(other: Snowflake): Int = value.shr(22).compareTo(other.value.shr(22))
 
@@ -76,7 +76,7 @@ class Snowflake(val value: Long) : Comparable<Snowflake> {
     }
 }
 
-private class SnowflakeMark(val epochMilliseconds: Long) : TimeMark() {
+private class SnowflakeMark(private val timeStamp: Instant) : TimeMark() {
 
-    override fun elapsedNow(): Duration = Clock.System.now() - Instant.fromEpochMilliseconds(epochMilliseconds)
+    override fun elapsedNow(): Duration = Clock.System.now() - timeStamp
 }
