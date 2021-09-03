1668eaf03e2fd953c042facd58d7df7479122ead
warrior <warriormayer@gmail.com>
2021-09-03 14:04:32 +0200

    Remove data class

--- common/src/main/kotlin/DiscordTimestamp.kt
+++ common/src/main/kotlin/DiscordTimestamp.kt
@@ -1,45 +1,9 @@
 package dev.kord.common
 
 import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
-import kotlinx.serialization.Serializable
 
-/**
- * The [timestamp format](https://discord.com/developers/docs/reference#message-formatting-formats) of the message formats
- */
-@Serializable
-data class DiscordTimestamp(
-
-    /**
-     * The Unix timestamp to be displayed
-     */
-    val time: Long,
-
-    /**
-     * The style of the timestamp
-     */
-    val style: DiscordTimestampStyle = DiscordTimestampStyle.ShortDateTime
-) : Comparable<DiscordTimestamp> {
-
-    val value: String
-        get() = "<t:$time:${style.style}>"
-
-    override fun equals(other: Any?): Boolean = other is DiscordTimestamp && other.time == time
-    operator fun plus(value: Long): DiscordTimestamp = DiscordTimestamp(time + value, style)
-    operator fun minus(value: Long): DiscordTimestamp = DiscordTimestamp(time - value, style)
-
-    override fun compareTo(other: DiscordTimestamp): Int = time.compareTo(other.time)
-
-    override fun toString() = value
-}
-
-fun LocalDateTime.toDiscordTimestamp(style: DiscordTimestampStyle = DiscordTimestampStyle.ShortDateTime) =
-    toInstant(TimeZone.UTC).toDiscordTimestamp(style)
-
-fun Instant.toDiscordTimestamp(style: DiscordTimestampStyle = DiscordTimestampStyle.ShortDateTime) =
-    DiscordTimestamp(toEpochMilliseconds(), style)
+fun Instant.toMessageFormat(style: DiscordTimestampStyle = DiscordTimestampStyle.ShortDateTime) =
+    "<t:${toEpochMilliseconds()}:${style.style}>"
 
 /**
  * The class representing the [style of a timestamp](https://discord.com/developers/docs/reference#message-formatting-timestamp-styles)
