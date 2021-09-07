f46a423b4b3a46407e4f04e07bb328db17ab1103
Lost <im@lostillusion.net>
2021-09-07 19:17:47 -0400

    move default audioframesender impl into separate file

--- voice/src/main/kotlin/udp/AudioFrameSender.kt
+++ voice/src/main/kotlin/udp/AudioFrameSender.kt
@@ -1,20 +1,13 @@
-@file:OptIn(KordVoice::class)
 @file:Suppress("ArrayInDataClass")
 
 package dev.kord.voice.udp
 
 import dev.kord.common.annotation.KordVoice
-import dev.kord.voice.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.transform
-import mu.KotlinLogging
-import kotlin.coroutines.CoroutineContext
-import kotlin.random.Random
-
-private val audioFrameSenderLogger = KotlinLogging.logger { }
+import dev.kord.voice.AudioProvider
+import dev.kord.voice.FrameInterceptor
+import dev.kord.voice.FrameInterceptorContext
+import dev.kord.voice.FrameInterceptorContextBuilder
+import kotlinx.coroutines.CoroutineScope
 
 @KordVoice
 data class AudioFrameSenderConfiguration(
@@ -32,57 +25,4 @@ interface AudioFrameSender : CoroutineScope {
      * send them to Discord.
      */
     suspend fun start(configuration: AudioFrameSenderConfiguration)
-}
-
-@KordVoice
-data class DefaultAudioFrameSenderData(
-    val udp: VoiceUdpConnection,
-    val dispatcher: CoroutineDispatcher
-)
-
-@KordVoice
-class DefaultAudioFrameSender(
-    val data: DefaultAudioFrameSenderData
-) : AudioFrameSender {
-    override val coroutineContext: CoroutineContext =
-        SupervisorJob() + data.dispatcher + CoroutineName("Default Audio Frame Poller")
-
-    private fun createFrameInterceptor(configuration: AudioFrameSenderConfiguration): FrameInterceptor =
-        with(configuration) {
-            val builder = baseFrameInterceptorContext
-            builder.ssrc = ssrc
-            return interceptorFactory(builder.build()) // we should assume that everything else is set before-hand in the base builder
-        }
-
-    override suspend fun start(configuration: AudioFrameSenderConfiguration) = with(configuration) {
-        val interceptor = createFrameInterceptor(configuration)
-        var sequence: UShort = Random.nextBits(UShort.SIZE_BITS).toUShort()
-
-        val frames = Channel<AudioFrame?>(Channel.RENDEZVOUS)
-        with(provider) { launch { provideFrames(frames) } }
-
-        audioFrameSenderLogger.trace { "audio poller starting." }
-
-        try {
-            frames.receiveAsFlow()
-                .transform { emit(interceptor.intercept(it)) }
-                .collect { frame ->
-                    if (frame != null) {
-                        val encryptedPacket = AudioPacket.DecryptedPacket(
-                            sequence = sequence,
-                            timestamp = sequence * 960u,
-                            ssrc = ssrc,
-                            decryptedData = frame.data
-                        ).encrypt(key)
-
-                        data.udp.send(encryptedPacket.asByteReadPacket())
-                        sequence++
-                    }
-                }
-        } catch (e: Exception) {
-            /* we're done polling, nothing to worry about */
-        }
-
-        audioFrameSenderLogger.trace { "udp connection closed, stopped polling for audio frames." }
-    }
 }
---
+++ voice/src/main/kotlin/udp/DefaultAudioFrameSender.kt
@@ -0,0 +1,75 @@
+package udp
+
+import dev.kord.common.annotation.KordVoice
+import dev.kord.voice.AudioFrame
+import dev.kord.voice.FrameInterceptor
+import dev.kord.voice.udp.AudioFrameSender
+import dev.kord.voice.udp.AudioFrameSenderConfiguration
+import dev.kord.voice.udp.AudioPacket
+import dev.kord.voice.udp.VoiceUdpConnection
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.launch
+import mu.KotlinLogging
+import kotlin.coroutines.CoroutineContext
+import kotlin.random.Random
+
+private val audioFrameSenderLogger = KotlinLogging.logger { }
+
+@KordVoice
+data class DefaultAudioFrameSenderData(
+    val udp: VoiceUdpConnection,
+    val dispatcher: CoroutineDispatcher
+)
+
+@KordVoice
+class DefaultAudioFrameSender(
+    val data: DefaultAudioFrameSenderData
+) : AudioFrameSender {
+    override val coroutineContext: CoroutineContext =
+        SupervisorJob() + data.dispatcher + CoroutineName("Default Audio Frame Poller")
+
+    private fun createFrameInterceptor(configuration: AudioFrameSenderConfiguration): FrameInterceptor =
+        with(configuration) {
+            val builder = baseFrameInterceptorContext
+            builder.ssrc = ssrc
+            return interceptorFactory(builder.build()) // we should assume that everything else is set before-hand in the base builder
+        }
+
+    override suspend fun start(configuration: AudioFrameSenderConfiguration) = with(configuration) {
+        val interceptor = createFrameInterceptor(configuration)
+        var sequence: UShort = Random.nextBits(UShort.SIZE_BITS).toUShort()
+
+        val frames = Channel<AudioFrame?>(Channel.RENDEZVOUS)
+        with(provider) { launch { provideFrames(frames) } }
+
+        audioFrameSenderLogger.trace { "audio poller starting." }
+
+        try {
+            frames.receiveAsFlow()
+                .transform { emit(interceptor.intercept(it)) }
+                .collect { frame ->
+                    if (frame != null) {
+                        val encryptedPacket = AudioPacket.DecryptedPacket(
+                            sequence = sequence,
+                            timestamp = sequence * 960u,
+                            ssrc = ssrc,
+                            decryptedData = frame.data
+                        ).encrypt(key)
+
+                        data.udp.send(encryptedPacket.asByteReadPacket())
+                        sequence++
+                    }
+                }
+        } catch (e: Exception) {
+            /* we're done polling, nothing to worry about */
+        }
+
+        audioFrameSenderLogger.trace { "udp connection closed, stopped polling for audio frames." }
+    }
+}
