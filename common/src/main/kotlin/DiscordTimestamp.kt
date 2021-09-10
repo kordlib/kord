5a6c5d6a7d2f1af8b2bf4c9529ebb79f858e94e9
Bjarne <63170816+warriorzz@users.noreply.github.com>
2021-09-10 10:53:22 +0200

    Add discord messaging timestamps (#385)
    * Add discord messaging timestamps
    
    Co-Authored-By: NyCode <nico@nycode.de>
    
    * Add toDiscordTimestamp function for Instant
    
    * Use value getter instead of a function and add toString
    
    Co-Authored-By: warrior <warriormayer@gmail.com>
    
    * Resolve requested changes
    
    Co-Authored-By: Lukellmann <47486203+Lukellmann@users.noreply.github.com>
    
    * Remove data class
    
    * Use seconds instead of milliseconds, so it will show up the right way
    
    Co-authored-by: NyCode <nico@nycode.de>
    Co-authored-by: Lukellmann <47486203+Lukellmann@users.noreply.github.com>

---
+++ common/src/main/kotlin/DiscordTimestamp.kt
@@ -0,0 +1,47 @@
+package dev.kord.common
+
+import kotlinx.datetime.Instant
+
+fun Instant.toMessageFormat(style: DiscordTimestampStyle = DiscordTimestampStyle.ShortDateTime) =
+    "<t:$epochSeconds:${style.style}>"
+
+/**
+ * The class representing the [style of a timestamp](https://discord.com/developers/docs/reference#message-formatting-timestamp-styles)
+ */
+enum class DiscordTimestampStyle(val style: String) {
+
+    /**
+     * For example 16:20
+     */
+    ShortTime("t"),
+
+    /**
+     * For example 16:20:30
+     */
+    LongTime("T"),
+
+    /**
+     * For example 20/04/2021
+     */
+    ShortDate("d"),
+
+    /**
+     * For example 20 April 2021
+     */
+    LongDate("D"),
+
+    /**
+     * For example 20 April 2021 16:20
+     */
+    ShortDateTime("f"),
+
+    /**
+     * For example Tuesday, 20 April 2021 16:20
+     */
+    LongDateTime("F"),
+
+    /**
+     * For example 2 months ago
+     */
+    RelativeTime("R"),
+}
