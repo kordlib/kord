543313a3aa81b346dc3093c6bded08373f9aeb8b
ByteAlex <bytealex@zerotwo.bot>
2021-09-16 15:24:13 +0200

    StageInstance is missing default supplier

--- core/src/main/kotlin/entity/StageInstance.kt
+++ core/src/main/kotlin/entity/StageInstance.kt
@@ -7,7 +7,7 @@ import dev.kord.core.cache.data.StageInstanceData
 import dev.kord.core.supplier.EntitySupplier
 import dev.kord.core.supplier.EntitySupplyStrategy
 
-class StageInstance(val data: StageInstanceData, override val kord: Kord, override val supplier: EntitySupplier) : StageInstanceBehavior {
+class StageInstance(val data: StageInstanceData, override val kord: Kord, override val supplier: EntitySupplier = kord.defaultSupplier) : StageInstanceBehavior {
     override val id: Snowflake get() = data.id
     val guildId: Snowflake get() = data.guildId
     override val channelId: Snowflake get() = data.channelId
