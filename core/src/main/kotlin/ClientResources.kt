b5428f49baa43067578d331dfc5c0d7cace3905f
HopeBaron <truehope9000@gmail.com>
2021-10-03 16:29:31 +0300

    Fix the conflicts caused by fixing the conflicts

--- core/src/main/kotlin/ClientResources.kt
+++ core/src/main/kotlin/ClientResources.kt
@@ -6,13 +6,12 @@ import dev.kord.gateway.Intents
 import dev.kord.gateway.builder.Shards
 import io.ktor.client.*
 
-
-class ClientResources(
-    val token: String,
-    val applicationId: Snowflake,
-    val shards: Shards,
-    val httpClient: HttpClient,
-    val defaultStrategy: EntitySupplyStrategy<*>,
+public class ClientResources(
+    public val token: String,
+    public val applicationId: Snowflake,
+    public val shards: Shards,
+    public val httpClient: HttpClient,
+    public val defaultStrategy: EntitySupplyStrategy<*>,
 ) {
     override fun toString(): String {
         return "ClientResources(shards=$shards, httpClient=$httpClient, defaultStrategy=$defaultStrategy)"
--- core/src/main/kotlin/builder/kord/KordRestOnlyBuilder.kt
+++ core/src/main/kotlin/builder/kord/KordRestOnlyBuilder.kt
@@ -9,12 +9,12 @@ import dev.kord.core.exception.KordInitializationException
 import dev.kord.core.gateway.DefaultMasterGateway
 import dev.kord.core.supplier.EntitySupplyStrategy
 import dev.kord.gateway.Gateway
-import dev.kord.gateway.Intents
+import dev.kord.gateway.builder.Shards
 import dev.kord.rest.ratelimit.ExclusionRequestRateLimiter
 import dev.kord.rest.request.KtorRequestHandler
 import dev.kord.rest.request.RequestHandler
 import dev.kord.rest.service.RestClient
-import io.ktor.client.HttpClient
+import io.ktor.client.*
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -64,7 +64,6 @@ public class KordRestOnlyBuilder(public val token: String) {
             Shards(0),
             client,
             EntitySupplyStrategy.rest,
-            Intents.none
         )
         val rest = RestClient(handlerBuilder(resources))
         val selfId = getBotIdFromToken(token)
