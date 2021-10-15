8877a4c4fa5a0db4b98d657fb5649e7929d8ec05
Lost <im@lostillusion.net>
2021-10-15 17:11:13 -0400

    external port may actually change in the global udp socket

--- voice/src/main/kotlin/udp/GlobalVoiceUdpSocket.kt
+++ voice/src/main/kotlin/udp/GlobalVoiceUdpSocket.kt
@@ -5,7 +5,10 @@ import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import io.ktor.util.network.*
 import io.ktor.utils.io.core.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.*
 import mu.KotlinLogging
 import kotlin.text.String
@@ -18,13 +21,10 @@ private val globalVoiceSocketLogger = KotlinLogging.logger { }
  */
 @KordVoice
 object GlobalVoiceUdpSocket : VoiceUdpSocket {
-    private val socketScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+    private val socketScope = CoroutineScope(Dispatchers.Default + SupervisorJob() + CoroutineName("kord-voice-global-socket"))
 
     private val _incoming: MutableSharedFlow<Datagram> = MutableSharedFlow()
     override val incoming: SharedFlow<Datagram> = _incoming
-
-    // once we discover it, it will stay the same for the rest of this socket's lifetime.
-    private var ourAddress: NetworkAddress? = null
 
     private val socket = aSocket(ActorSelectorManager(socketScope.coroutineContext)).udp().bind()
 
@@ -39,22 +39,18 @@ object GlobalVoiceUdpSocket : VoiceUdpSocket {
     override suspend fun discoverIp(address: NetworkAddress, ssrc: Int): NetworkAddress {
         globalVoiceSocketLogger.trace { "discovering ip" }
 
-        if (ourAddress != null) return ourAddress!!
-
         send(packet(address) {
             writeInt(ssrc)
             writeFully(ByteArray(66))
         })
 
-        with(receiveFrom(address).packet) {
+        return with(receiveFrom(address).packet) {
             discard(4)
             val ip = String(readBytes(64)).trimEnd(0.toChar())
             val port = readUShort().toInt()
 
-            ourAddress = NetworkAddress(ip, port)
+            NetworkAddress(ip, port)
         }
-
-        return ourAddress!!
     }
 
     override suspend fun send(packet: Datagram) {
