9de86c15ec3ccb31672ecff3c5d093bffaf40d91
HopeBaron <truehope9000@gmail.com>
2021-10-23 17:55:53 +0300

    fix ButtonBuilder#emoji function

--- core/src/main/kotlin/builder/component/ButtonBuilderExtensions.kt
+++ core/src/main/kotlin/builder/component/ButtonBuilderExtensions.kt
@@ -1,16 +1,20 @@
 package dev.kord.core.builder.components
 
 import dev.kord.common.entity.DiscordPartialEmoji
+import dev.kord.common.entity.optional.optional
 import dev.kord.core.entity.GuildEmoji
 import dev.kord.core.entity.ReactionEmoji
 import dev.kord.rest.builder.component.ButtonBuilder
 
 
-public fun ButtonBuilder.emoji(emoji: ReactionEmoji) {
+public fun ButtonBuilder.emoji(emoji: ReactionEmoji.Unicode) {
     this.emoji = DiscordPartialEmoji(name = emoji.name, id = null)
 }
 
+public fun ButtonBuilder.emoji(emoji: ReactionEmoji.Custom) {
+    this.emoji = DiscordPartialEmoji(name = emoji.name, id = emoji.id, animated = emoji.isAnimated.optional())
+}
 
 public fun ButtonBuilder.emoji(emoji: GuildEmoji) {
-    this.emoji = DiscordPartialEmoji(id = emoji.id, name = null)
+    this.emoji = DiscordPartialEmoji(id = emoji.id, name = null, animated = emoji.isAnimated.optional())
 }
