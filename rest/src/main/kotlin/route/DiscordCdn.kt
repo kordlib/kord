990f5b1e67ab640358f24968244a01be3c17e337
Lukellmann <lukellmann@gmail.com>
2021-12-31 14:05:32 +0100

    explicitApi for DiscordCdn

--- rest/src/main/kotlin/route/DiscordCdn.kt
+++ rest/src/main/kotlin/route/DiscordCdn.kt
@@ -2,18 +2,18 @@ package dev.kord.rest.route
 
 import dev.kord.common.entity.Snowflake
 
-object DiscordCdn {
+public object DiscordCdn {
 
     private const val BASE_URL = "https://cdn.discordapp.com"
 
-    fun emoji(emojiId: Snowflake): CdnUrl = CdnUrl("$BASE_URL/emojis/$emojiId")
+    public fun emoji(emojiId: Snowflake): CdnUrl = CdnUrl("$BASE_URL/emojis/$emojiId")
 
-    fun defaultAvatar(discriminator: Int): CdnUrl = CdnUrl("$BASE_URL/embed/avatars/${discriminator % 5}")
+    public fun defaultAvatar(discriminator: Int): CdnUrl = CdnUrl("$BASE_URL/embed/avatars/${discriminator % 5}")
 
-    fun userAvatar(userId: Snowflake, hash: String): CdnUrl = CdnUrl("$BASE_URL/avatars/$userId/$hash")
+    public fun userAvatar(userId: Snowflake, hash: String): CdnUrl = CdnUrl("$BASE_URL/avatars/$userId/$hash")
 
-    fun memberAvatar(guildId: Snowflake, userId: Snowflake, hash: String) =
+    public fun memberAvatar(guildId: Snowflake, userId: Snowflake, hash: String): CdnUrl =
         CdnUrl("$BASE_URL/guilds/$guildId/users/$userId/avatars/$hash")
 
-    fun roleIcon(roleId: Snowflake, hash: String) = CdnUrl("$BASE_URL/role-icons/$roleId/$hash")
+    public fun roleIcon(roleId: Snowflake, hash: String): CdnUrl = CdnUrl("$BASE_URL/role-icons/$roleId/$hash")
 }
