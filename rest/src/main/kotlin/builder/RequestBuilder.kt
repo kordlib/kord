ecb5b9b2e000433291944027fe40ed5df47380f5
Lukellmann <lukellmann@gmail.com>
2021-12-31 12:53:35 +0100

    explicitApi for RequestBuilder

--- rest/src/main/kotlin/builder/RequestBuilder.kt
+++ rest/src/main/kotlin/builder/RequestBuilder.kt
@@ -3,14 +3,14 @@ package dev.kord.rest.builder
 import dev.kord.common.annotation.KordDsl
 
 @KordDsl
-interface RequestBuilder<T> {
-    fun toRequest(): T
+public interface RequestBuilder<T> {
+    public fun toRequest(): T
 }
 
 @KordDsl
-interface AuditRequestBuilder<T> : RequestBuilder<T> {
+public interface AuditRequestBuilder<T> : RequestBuilder<T> {
     /**
      * The reason for this request, this will be displayed in the audit log.
      */
-    var reason: String?
+    public var reason: String?
 }
