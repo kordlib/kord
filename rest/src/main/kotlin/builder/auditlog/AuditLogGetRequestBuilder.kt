c749c7283bd9aaea97ecc94b338e84df281f3d05
Lukellmann <lukellmann@gmail.com>
2021-12-31 12:21:42 +0100

    explicitApi for AuditLogGetRequestBuilder

--- rest/src/main/kotlin/builder/auditlog/AuditLogGetRequestBuilder.kt
+++ rest/src/main/kotlin/builder/auditlog/AuditLogGetRequestBuilder.kt
@@ -5,22 +5,22 @@ import dev.kord.common.entity.Snowflake
 import dev.kord.rest.builder.RequestBuilder
 import dev.kord.rest.json.request.AuditLogGetRequest
 
-class AuditLogGetRequestBuilder : RequestBuilder<AuditLogGetRequest> {
+public class AuditLogGetRequestBuilder : RequestBuilder<AuditLogGetRequest> {
 
     /**
      * The id of the user whose actions should be filtered for. `null` be default.
      */
-    var userId: Snowflake? = null
+    public var userId: Snowflake? = null
 
     /**
      * The type of [AuditLogEvent] which should be filtered for. `null` be default.
      */
-    var action: AuditLogEvent? = null
+    public var action: AuditLogEvent? = null
 
     /**
      * The time, represented as a Snowflake, after which entries are no longer returned.
      */
-    var before: Snowflake? = null
+    public var before: Snowflake? = null
 
     override fun toRequest(): AuditLogGetRequest = AuditLogGetRequest(userId, action, before, 100)
 }
