a5fec1bf54dc20005e3ce247b1ceb49077dfd941
Lukellmann <lukellmann@gmail.com>
2021-12-31 13:54:15 +0100

    explicitApi for HttpUtils

--- rest/src/main/kotlin/request/HttpUtils.kt
+++ rest/src/main/kotlin/request/HttpUtils.kt
@@ -20,45 +20,47 @@ private const val auditLogReason = "X-Audit-Log-Reason"
 /**
  * Sets the reason that will show up in the [Discord Audit Log]() to [reason] for this request.
  */
-fun <T> RequestBuilder<T>.auditLogReason(reason: String?) = reason?.let { header(auditLogReason, reason) }
+public fun <T> RequestBuilder<T>.auditLogReason(reason: String?) {
+    reason?.let { header(auditLogReason, reason) }
+}
 
-val HttpResponse.channelResetPoint: Instant
+public val HttpResponse.channelResetPoint: Instant
     get() {
         val unixSeconds = headers[resetTimeHeader]?.toDouble() ?: return Clock.System.now()
         return Instant.fromEpochMilliseconds(unixSeconds.times(1000).toLong())
     }
 
-fun HttpResponse.channelResetPoint(clock: Clock): Instant {
+public fun HttpResponse.channelResetPoint(clock: Clock): Instant {
     val seconds = headers[rateLimitResetAfter]?.toDouble() ?: return clock.now()
     return clock.now().plus(seconds.seconds)
 }
 
-val HttpResponse.isRateLimit get() = status.value == 429
-val HttpResponse.isError get() = status.value in 400 until 600
-val HttpResponse.isErrorWithRateLimit get() = status.value == 403 || status.value == 401
-val HttpResponse.isGlobalRateLimit get() = headers[rateLimitGlobalHeader] != null
-val HttpResponse.rateLimitTotal get() = headers[rateLimit]?.toLongOrNull()
-val HttpResponse.rateLimitRemaining get() = headers[rateLimitRemainingHeader]?.toLongOrNull()
-val HttpResponse.isChannelRateLimit get() = headers[rateLimitRemainingHeader]?.toIntOrNull() == 0
-val HttpResponse.bucket: BucketKey? get() = headers[bucketRateLimitKey]?.let { BucketKey(it) }
+public val HttpResponse.isRateLimit: Boolean get() = status.value == 429
+public val HttpResponse.isError: Boolean get() = status.value in 400 until 600
+public val HttpResponse.isErrorWithRateLimit: Boolean get() = status.value == 403 || status.value == 401
+public val HttpResponse.isGlobalRateLimit: Boolean get() = headers[rateLimitGlobalHeader] != null
+public val HttpResponse.rateLimitTotal: Long? get() = headers[rateLimit]?.toLongOrNull()
+public val HttpResponse.rateLimitRemaining: Long? get() = headers[rateLimitRemainingHeader]?.toLongOrNull()
+public val HttpResponse.isChannelRateLimit: Boolean get() = headers[rateLimitRemainingHeader]?.toIntOrNull() == 0
+public val HttpResponse.bucket: BucketKey? get() = headers[bucketRateLimitKey]?.let { BucketKey(it) }
 
 /**
  * The unix time (in ms) when the global rate limit gets reset.
  */
-fun HttpResponse.globalSuspensionPoint(clock: Clock): Long {
+public fun HttpResponse.globalSuspensionPoint(clock: Clock): Long {
     val secondsWait = headers[retryAfterHeader]?.toLong() ?: return clock.now().toEpochMilliseconds()
     return (secondsWait * 1000) + clock.now().toEpochMilliseconds()
 }
 
-fun HttpResponse.logString(body: String) =
+public fun HttpResponse.logString(body: String): String =
     "[RESPONSE]:${status.value}:${call.request.method.value}:${call.request.url} body:$body"
 
-suspend fun HttpResponse.errorString(): String {
+public suspend fun HttpResponse.errorString(): String {
     val message = String(this.readBytes())
     return logString(message)
 }
 
-fun Request<*, *>.logString(body: String): String {
+public fun Request<*, *>.logString(body: String): String {
     val method = route.method.value
     val path = route.path
     val params = routeParams.entries
