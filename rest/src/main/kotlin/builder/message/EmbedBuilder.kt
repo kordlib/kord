485917342be5c731a4b90e1705d9db81f899bcfc
Lukellmann <lukellmann@gmail.com>
2021-12-31 14:19:23 +0100

    explicitApi for EmbedBuilder and EmbedResponse

--- rest/src/main/kotlin/builder/message/EmbedBuilder.kt
+++ rest/src/main/kotlin/builder/message/EmbedBuilder.kt
@@ -18,13 +18,13 @@ import kotlinx.datetime.Instant
  */
 @KordDsl
 @Suppress("unused")
-class EmbedBuilder : RequestBuilder<EmbedRequest> {
-    companion object {
+public class EmbedBuilder : RequestBuilder<EmbedRequest> {
+    public companion object {
         /**
          * Utility default value for fields.
          * While blank values are not allowed, this value can be used to trick Discord into rendering a Field as empty.
          */
-        const val ZERO_WIDTH_SPACE = "\u200E"
+        public const val ZERO_WIDTH_SPACE: String = "\u200E"
     }
 
     private var _title: Optional<String> = Optional.Missing()
@@ -32,14 +32,14 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
     /**
      * The title of the embed. Limited to the length of [Limits.title].
      */
-    var title: String? by ::_title.delegate()
+    public var title: String? by ::_title.delegate()
 
     private var _description: Optional<String> = Optional.Missing()
 
     /**
      * The description of the embed. Limited to the length of [Limits.description].
      */
-    var description: String? by ::_description.delegate()
+    public var description: String? by ::_description.delegate()
 
 
     private var _url: Optional<String> = Optional.Missing()
@@ -47,21 +47,21 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
     /**
      * The url of the embed's [title].
      */
-    var url: String? by ::_url.delegate()
+    public var url: String? by ::_url.delegate()
 
     private var _timestamp: Optional<Instant> = Optional.Missing()
 
     /**
      * The timestamp displayed at the bottom of the embed.
      */
-    var timestamp: Instant? by ::_timestamp.delegate()
+    public var timestamp: Instant? by ::_timestamp.delegate()
 
     private var _color: Optional<Color> = Optional.Missing()
 
     /**
      * The color of the embed.
      */
-    var color: Color? by ::_color.delegate()
+    public var color: Color? by ::_color.delegate()
 
 
     private var _image: Optional<String> = Optional.Missing()
@@ -69,59 +69,59 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
     /**
      * The image url of the embed.
      */
-    var image: String? by ::_image.delegate()
+    public var image: String? by ::_image.delegate()
 
     private var _footer: Optional<Footer> = Optional.Missing()
 
     /**
      * The footer of the embed.
      */
-    var footer: Footer? by ::_footer.delegate()
+    public var footer: Footer? by ::_footer.delegate()
 
     private var _thumbnail: Optional<Thumbnail> = Optional.Missing()
 
     /**
      * The thumbnail of the embed.
      */
-    var thumbnail: Thumbnail? by ::_thumbnail.delegate()
+    public var thumbnail: Thumbnail? by ::_thumbnail.delegate()
 
     private var _author: Optional<Author> = Optional.Missing()
 
     /**
      * The author of the embed.
      */
-    var author: Author? by ::_author.delegate()
+    public var author: Author? by ::_author.delegate()
 
     /**
      * The embed fields.
      */
-    var fields: MutableList<Field> = mutableListOf()
+    public var fields: MutableList<Field> = mutableListOf()
 
     /**
      * Adds or updates the [footer] as configured by the [builder].
      */
-    inline fun footer(builder: Footer.() -> Unit) {
+    public inline fun footer(builder: Footer.() -> Unit) {
         footer = (footer ?: Footer()).apply(builder)
     }
 
     /**
      * Adds or updates the [thumbnail] as configured by the [builder].
      */
-    inline fun thumbnail(builder: Thumbnail.() -> Unit) {
+    public inline fun thumbnail(builder: Thumbnail.() -> Unit) {
         thumbnail = (thumbnail ?: Thumbnail()).apply(builder)
     }
 
     /**
      * Adds or updates the [author] as configured by the [builder].
      */
-    inline fun author(builder: Author.() -> Unit) {
+    public inline fun author(builder: Author.() -> Unit) {
         author = (author ?: Author()).apply(builder)
     }
 
     /**
      * Adds a new [Field] configured by the [builder].
      */
-    inline fun field(builder: Field.() -> Unit) {
+    public inline fun field(builder: Field.() -> Unit) {
         fields.add(Field().apply(builder))
     }
 
@@ -137,7 +137,7 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
      * Limited in to the length of [Field.Limits.name].
      *
      */
-    inline fun field(name: String, inline: Boolean = false, value: () -> String = { ZERO_WIDTH_SPACE }) {
+    public inline fun field(name: String, inline: Boolean = false, value: () -> String = { ZERO_WIDTH_SPACE }) {
         val field = Field()
         field.name = name
         field.inline = inline
@@ -161,52 +161,52 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
     )
 
     @KordDsl
-    class Thumbnail : RequestBuilder<EmbedThumbnailRequest> {
+    public class Thumbnail : RequestBuilder<EmbedThumbnailRequest> {
 
         /**
          * The image url of the thumbnail. This field is required.
          */
-        lateinit var url: String
+        public lateinit var url: String
 
-        override fun toRequest() = EmbedThumbnailRequest(url)
+        override fun toRequest(): EmbedThumbnailRequest = EmbedThumbnailRequest(url)
     }
 
     @KordDsl
-    class Footer : RequestBuilder<EmbedFooterRequest> {
+    public class Footer : RequestBuilder<EmbedFooterRequest> {
 
         /**
          * The text of the footer. This field is required and limited to the length of [Limits.text].
          */
-        lateinit var text: String
+        public lateinit var text: String
 
         /**
          * The icon url to display.
          */
-        var icon: String? = null
+        public var icon: String? = null
 
-        override fun toRequest() = EmbedFooterRequest(text, icon)
+        override fun toRequest(): EmbedFooterRequest = EmbedFooterRequest(text, icon)
 
-        object Limits {
-            const val text = 2048
+        public object Limits {
+            public const val text: Int = 2048
         }
     }
 
     @KordDsl
-    class Author : RequestBuilder<EmbedAuthorRequest> {
+    public class Author : RequestBuilder<EmbedAuthorRequest> {
 
         private var _name: Optional<String> = Optional.Missing()
 
         /**
          * The name of the author. This field is required if [url] is not null.
          */
-        var name: String? by ::_name.delegate()
+        public var name: String? by ::_name.delegate()
 
         private var _url: Optional<String> = Optional.Missing()
 
         /**
          * The link that will be applied to the author's [name]. [name] is a mandatory field if [url] is not null.
          */
-        var url: String? by ::_url.delegate()
+        public var url: String? by ::_url.delegate()
 
 
         private var _icon: Optional<String> = Optional.Missing()
@@ -214,20 +214,20 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
         /**
          * The image url that will be displayed next to the author's name.
          */
-        var icon: String? by ::_icon.delegate()
+        public var icon: String? by ::_icon.delegate()
 
-        override fun toRequest() = EmbedAuthorRequest(_name, _url, _icon)
+        override fun toRequest(): EmbedAuthorRequest = EmbedAuthorRequest(_name, _url, _icon)
 
-        object Limits {
+        public object Limits {
             /**
              * The maximum length of the [Author.name] field.
              */
-            const val name = 256
+            public const val name: Int = 256
         }
     }
 
     @KordDsl
-    class Field : RequestBuilder<EmbedFieldRequest> {
+    public class Field : RequestBuilder<EmbedFieldRequest> {
 
         /**
          *  The value or 'description' of the [Field], [ZERO_WIDTH_SPACE] by default.
@@ -236,7 +236,7 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
          *  Blank values are not allowed, resulting an exception being thrown on the completion of the request.
          *  Use [ZERO_WIDTH_SPACE] instead to simulate an empty [value].
          */
-        var value: String = ZERO_WIDTH_SPACE
+        public var value: String = ZERO_WIDTH_SPACE
 
         /**
          * The name or 'title' of the [Field], [ZERO_WIDTH_SPACE] by default.
@@ -245,7 +245,7 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
          * Blank values are not allowed, resulting an exception being thrown on the completion of the request.
          * Use [ZERO_WIDTH_SPACE] instead to simulate an empty [value].
          */
-        var name: String = ZERO_WIDTH_SPACE
+        public var name: String = ZERO_WIDTH_SPACE
 
 
         private var _inline: OptionalBoolean = OptionalBoolean.Missing
@@ -253,49 +253,42 @@ class EmbedBuilder : RequestBuilder<EmbedRequest> {
         /**
          * Whether the field should be rendered inline, `false` by default.
          */
-        var inline: Boolean? by ::_inline.delegate()
+        public var inline: Boolean? by ::_inline.delegate()
 
-        init {
-            //these declarations and assignments are separated to maintain binary compatibility.
-            //`lateinit` fields expose the JVM fields, and this was the cleanest solution I could think of
-            // to maintain that field access.
-        }
+        override fun toRequest(): EmbedFieldRequest = EmbedFieldRequest(name, value, _inline)
 
-
-        override fun toRequest() = EmbedFieldRequest(name, value, _inline)
-
-        object Limits {
+        public object Limits {
             /**
              * The maximum length of the [Field.name] field.
              */
-            const val name = 256
+            public const val name: Int = 256
 
             /**
              * The maximum length of the [Field.value] field.
              */
-            const val value = 1024
+            public const val value: Int = 1024
         }
     }
 
-    object Limits {
+    public object Limits {
         /**
          * The maximum length of the [EmbedBuilder.title] field.
          */
-        const val title = 256
+        public const val title: Int = 256
 
         /**
          * The maximum length of the [EmbedBuilder.description] field.
          */
-        const val description = 4096
+        public const val description: Int = 4096
 
         /**
          * The maximum amount of [EmbedBuilder.Field] in an [EmbedBuilder].
          */
-        const val fieldCount = 25
+        public const val fieldCount: Int = 25
 
         /**
          * The maximum length of all text inside the [EmbedBuilder].
          */
-        const val total = 6000
+        public const val total: Int = 6000
     }
 }
