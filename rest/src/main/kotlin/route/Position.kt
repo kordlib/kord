88be02be55096840cad468c50c94e6d28f5c1b54
Lukellmann <lukellmann@gmail.com>
2022-01-01 16:52:38 +0100

    explicitApi for Position, PresenceBuilder and Prune

--- rest/src/main/kotlin/route/Position.kt
+++ rest/src/main/kotlin/route/Position.kt
@@ -2,8 +2,8 @@ package dev.kord.rest.route
 
 import dev.kord.common.entity.Snowflake
 
-sealed class Position(val key: String, val value: Snowflake) {
-    class Before(id: Snowflake) : Position("before", id)
-    class After(id: Snowflake) : Position("after", id)
-    class Around(id: Snowflake) : Position("around", id)
+public sealed class Position(public val key: String, public val value: Snowflake) {
+    public class Before(id: Snowflake) : Position("before", id)
+    public class After(id: Snowflake) : Position("after", id)
+    public class Around(id: Snowflake) : Position("around", id)
 }
