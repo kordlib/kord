5f6ce74e000818f503dc1496acacaf95ce939b98
Lukellmann <lukellmann@gmail.com>
2022-01-01 17:03:44 +0100

    explicitApi for RequestHandler and RestRequestException

--- rest/src/main/kotlin/request/RequestHandler.kt
+++ rest/src/main/kotlin/request/RequestHandler.kt
@@ -3,13 +3,13 @@ package dev.kord.rest.request
 /**
  * Handles Discord API requests.
  */
-interface RequestHandler {
+public interface RequestHandler {
 
     /**
      * Executes the [request], abiding by the active rate limits and returning the response [R].
-     * Throws an [RestRequestException] when a non-rate limit error response is returned.
+     * @throws RestRequestException when a non-rate limit error response is returned.
      */
     @Throws(RestRequestException::class)
-    suspend fun <B : Any, R> handle(request: Request<B, R>): R
+    public suspend fun <B : Any, R> handle(request: Request<B, R>): R
 
 }
