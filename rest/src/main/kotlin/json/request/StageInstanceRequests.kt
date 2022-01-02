d86d1f1541a5f2dae8e05ecd9d47d891cbafb47e
Lukellmann <lukellmann@gmail.com>
2022-01-02 06:02:20 +0100

    explicitApi for StageInstanceRequests and StageInstanceService.kt

--- rest/src/main/kotlin/json/request/StageInstanceRequests.kt
+++ rest/src/main/kotlin/json/request/StageInstanceRequests.kt
@@ -5,11 +5,11 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class StageInstanceCreateRequest(
+public data class StageInstanceCreateRequest(
     @SerialName("channel_id")
     val channelId: Snowflake,
     val topic: String
 )
 
 @Serializable
-data class StageInstanceUpdateRequest(val topic: String)
+public data class StageInstanceUpdateRequest(val topic: String)
