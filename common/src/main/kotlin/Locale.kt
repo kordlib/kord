4f296b859fa00ca8dd3f69a5b064b40cc40da2b1
Michael Rittmeister <michael@rittmeister.in>
2022-01-16 14:48:12 +0100

    Parse locale properly

--- common/src/main/kotlin/Locale.kt
+++ common/src/main/kotlin/Locale.kt
@@ -10,35 +10,220 @@ import kotlinx.serialization.encoding.Encoder
 import java.util.Locale as JLocale
 
 /**
- * Representation of a locale.
+ * Representation of a locale [supported by Discord](https://discord.com/developers/docs/reference#Locales).
  *
  * @property language an ISO 639 language code representing the language
  * @property country an ISO 3166 country code representing the country
  */
 @Serializable(with = Locale.Serializer::class)
-data class Locale(val language: String, val country: String?) {
+data class Locale(val language: String, val country: String? = null) {
     /**
      * Converts this into a [JLocale].
      */
     fun asJavaLocale() = JLocale(language, country ?: "")
 
     companion object {
+
+        /**
+         * English (United States).
+         */
+        val ENGLISH_UNITED_STATES = Locale("en", "US")
+
+        /**
+         * English (Great Britain).
+         */
+        val ENGLISH_GREAT_BRITAN = Locale("en", "GB")
+
+        /**
+         * Bulgarian.
+         */
+        val BULGARIAN = Locale("bg")
+
+        /**
+         * Chinese (China).
+         */
+        val CHINESE_CHINA = Locale("zh", "CN")
+
+        /**
+         * Chinese (Taiwan).
+         */
+        val CHINESE_TAIWAN = Locale("zh", "TW")
+
+        /**
+         * Croatian.
+         */
+        val CROATIAN = Locale("hr")
+
+        /**
+         * Czech.
+         */
+        val CZECH = Locale("cs")
+
+        /**
+         * Danish.
+         */
+        val DANISH = Locale("da")
+
+        /**
+         * Dutch.
+         */
+        val DUTCH = Locale("nl")
+
+        /**
+         * Finnish.
+         */
+        val FINNISH = Locale("fi")
+
+        /**
+         * French.
+         */
+        val FRENCH = Locale("fr")
+
+        /**
+         * German.
+         */
+        val GERMAN = Locale("de")
+
+        /**
+         * Greek.
+         */
+        val GREEK = Locale("el")
+
+        /**
+         * Hindi.
+         */
+        val HINDI = Locale("hi")
+
+        /**
+         * Hungarian.
+         */
+        val HUNGARIAN = Locale("hu")
+
+        /**
+         * Italian.
+         */
+        val ITALIAN = Locale("it")
+
+        /**
+         * Japanese.
+         */
+        val JAPENESE = Locale("ja")
+
+        /**
+         * Korean.
+         */
+        val KOREAN = Locale("ko")
+
+        /**
+         * Lithuanian.
+         */
+        val LITHUANIAN = Locale("lt")
+
+        /**
+         * Norwegian.
+         */
+        val NORWEGIAN = Locale("no")
+
+        /**
+         * Polish.
+         */
+        val POLISH = Locale("pl")
+
+        /**
+         * Portuguese (Brazil).
+         */
+        val PORTUGUESE_BRAZIL = Locale("pt", "BR")
+
+        /**
+         * Romanian.
+         */
+        val ROMANIAN = Locale("ro")
+
+        /**
+         * Russian.
+         */
+        val RUSSIAN = Locale("ru")
+
+        /**
+         * Spanish (Spain).
+         */
+        val SPANISH_SPAIN = Locale("es", "ES")
+
+        /**
+         * Swedish.
+         */
+        val SWEDISH = Locale("sv", "SE")
+
+        /**
+         * Thai.
+         */
+        val THAI = Locale("th")
+
+        /**
+         * Turkish.
+         */
+        val TURKISH = Locale("tr")
+
+        /**
+         * Ukrainian.
+         */
+        val UKRAINIAN = Locale("uk")
+
+        /**
+         * Vietnamese.
+         */
+        val VIETNAMESE = Locale("vi")
+
+
+        /**
+         * All languages [supported by Discord](https://discord.com/developers/docs/reference#Locales).
+         */
+        val ALL = listOf(
+            ENGLISH_UNITED_STATES, ENGLISH_GREAT_BRITAN,
+            BULGARIAN,
+            CHINESE_CHINA, CHINESE_TAIWAN,
+            CROATIAN,
+            CZECH,
+            DANISH,
+            DUTCH,
+            FINNISH,
+            FRENCH,
+            GERMAN,
+            GREEK,
+            HINDI,
+            HUNGARIAN,
+            ITALIAN,
+            JAPENESE,
+            KOREAN,
+            LITHUANIAN,
+            NORWEGIAN,
+            POLISH,
+            PORTUGUESE_BRAZIL,
+            ROMANIAN,
+            RUSSIAN,
+            SPANISH_SPAIN,
+            SWEDISH,
+            THAI,
+            TURKISH,
+            UKRAINIAN,
+            VIETNAMESE
+        )
+
         // We accept both "_" and "-" as a separator, because Discord doesn't really document it
         // https://regex101.com/r/fVPdR8/2
         private val languageTagFormat = "(\\w{2})(?:[_\\-](\\w{2}))?".toRegex()
 
         /**
          * Decodes the language from a `languageCode_countryCode` or `languageCode` format.
-         *
-         * This doesn't validate whether the language code is actually valid
          */
         fun fromString(string: String): Locale {
             val match = languageTagFormat.matchEntire(string) ?: error("$string is not a valid Locale")
-
             val (language) = match.destructured
             val country = match.groupValues.getOrNull(2)
 
-            return Locale(language, country)
+            return ALL.firstOrNull { (l, c) ->
+                language == l && country == c
+            } ?: error("This locale is not supported by Discord")
         }
     }
 
