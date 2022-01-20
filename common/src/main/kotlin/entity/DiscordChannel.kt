cf4fbb162cd97f6a1bff112fb8e1374386dbad93
Lukellmann <lukellmann@gmail.com>
2022-01-20 01:19:26 +0100

    createTimestamp for Threads, see https://github.com/discord/discord-api-docs/commit/53ee0109b874bde5ec2316379bf266bb332630a0

--- common/src/main/kotlin/entity/DiscordChannel.kt
+++ common/src/main/kotlin/entity/DiscordChannel.kt
@@ -4,6 +4,7 @@ import dev.kord.common.entity.optional.Optional
 import dev.kord.common.entity.optional.OptionalBoolean
 import dev.kord.common.entity.optional.OptionalInt
 import dev.kord.common.entity.optional.OptionalSnowflake
+import kotlinx.datetime.Instant
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -178,14 +179,16 @@ sealed class OverwriteType(val value: Int) {
 }
 
 @Serializable
-class DiscordThreadMetadata(
+data class DiscordThreadMetadata(
     val archived: Boolean,
     @SerialName("archive_timestamp")
     val archiveTimestamp: String,
     @SerialName("auto_archive_duration")
     val autoArchiveDuration: ArchiveDuration,
     val locked: OptionalBoolean = OptionalBoolean.Missing,
-    val invitable: OptionalBoolean = OptionalBoolean.Missing
+    val invitable: OptionalBoolean = OptionalBoolean.Missing,
+    @SerialName("create_timestamp")
+    val createTimestamp: Optional<Instant> = Optional.Missing(),
 )
 
 @Serializable(with = ArchiveDuration.Serializer::class)
