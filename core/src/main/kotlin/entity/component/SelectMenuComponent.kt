6e3813124e06640b4af38912f52548e50808ce7e
Michael Rittmeister <michael@rittmeister.in>
2022-01-20 02:55:26 +0100

    Add disabled property to SelectMenuComponent (Fix #487)

--- core/src/main/kotlin/entity/component/SelectMenuComponent.kt
+++ core/src/main/kotlin/entity/component/SelectMenuComponent.kt
@@ -40,6 +40,11 @@ public class SelectMenuComponent(override val data: ComponentData) : Component {
      */
     public val maxValues: Int get() = data.maxValues.orElse(1)
 
+    /**
+     * Disable the select, default `false`
+     */
+    public val disabled: Boolean get() = data.disabled.discordBoolean
+
     override fun equals(other: Any?): Boolean {
         if (other !is SelectMenuComponent) return false
 
