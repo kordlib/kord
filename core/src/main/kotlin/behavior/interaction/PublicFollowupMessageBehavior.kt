4a37a8ca35e6d7ec9d0548b793e1d3db932dda58
Lukellmann <lukellmann@gmail.com>
2022-01-26 22:55:00 +0100

    use builder from InteractionService in PublicFollowupMessageBehavior

--- core/src/main/kotlin/behavior/interaction/PublicFollowupMessageBehavior.kt
+++ core/src/main/kotlin/behavior/interaction/PublicFollowupMessageBehavior.kt
@@ -43,8 +43,7 @@ public interface PublicFollowupMessageBehavior : FollowupMessageBehavior {
  */
 public suspend inline fun PublicFollowupMessageBehavior.edit(builder: FollowupMessageModifyBuilder.() -> Unit): PublicFollowupMessage {
     contract { callsInPlace(builder, InvocationKind.EXACTLY_ONCE) }
-    val builder = FollowupMessageModifyBuilder().apply(builder)
-    val response = kord.rest.interaction.modifyFollowupMessage(applicationId, token, id, builder.toRequest())
+    val response = kord.rest.interaction.modifyFollowupMessage(applicationId, token, id, builder)
     return PublicFollowupMessage(Message(response.toData(), kord), applicationId, token, kord)
 }
 
