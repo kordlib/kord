87520667ac8a187d3cbd750bec3c592b7d69aa86
Lukellmann <lukellmann@gmail.com>
2022-02-18 22:51:27 +0100

    ActionRowComponent docs

--- core/src/main/kotlin/entity/component/ActionRowComponent.kt
+++ core/src/main/kotlin/entity/component/ActionRowComponent.kt
@@ -7,34 +7,34 @@ import dev.kord.core.cache.data.ComponentData
 /**
  * A non-interactive container component for other types of component.
  */
-
 public class ActionRowComponent(override val data: ComponentData) : Component {
 
     override val type: ComponentType.ActionRow
         get() = ComponentType.ActionRow
 
     /**
-     * All components nested inside this component.
+     * All [Component]s that are nested inside this component.
      */
     public val components: List<Component>
         get() = data.components.orEmpty().map { Component(it) }
 
     /**
-     * The buttons that are nested inside this component.
+     * The [ButtonComponent]s that are nested inside this component.
      * @see components
      */
     public val buttons: List<ButtonComponent>
         get() = components.filterIsInstance<ButtonComponent>()
 
     /**
-     * The buttons that are nested inside this component.
+     * The [SelectMenuComponent]s that are nested inside this component.
      * @see components
      */
     public val selectMenus: List<SelectMenuComponent>
         get() = components.filterIsInstance<SelectMenuComponent>()
 
     /**
-     * The [TextInputComponent] that are nested inside this component.
+     * The [TextInputComponent]s that are nested inside this component.
+     * @see components
      */
     public val textInputs: List<TextInputComponent>
         get() = components.filterIsInstance<TextInputComponent>()
