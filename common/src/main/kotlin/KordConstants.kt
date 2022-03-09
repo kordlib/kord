2d3ee73e88b9ff998563a64ba7c02109ca6cd8b6
Lukellmann <lukellmann@gmail.com>
2022-03-09 17:01:09 +0100

    https://github.com/kordlib/kord instead of https://kord.dev

--- common/src/main/kotlin/KordConstants.kt
+++ common/src/main/kotlin/KordConstants.kt
@@ -3,5 +3,5 @@ package dev.kord.common
 public object KordConstants {
 
     /** Kord's value for the [User Agent header](https://discord.com/developers/docs/reference#user-agent). */
-    public const val UserAgent: String = "DiscordBot (https://kord.dev/, 0.8.0-M12)"
+    public const val UserAgent: String = "DiscordBot (https://github.com/kordlib/kord, 0.8.0-M12)"
 }
