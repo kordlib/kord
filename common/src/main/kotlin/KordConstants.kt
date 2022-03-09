76d409816acbfd890b7230477c7c617c4bfd75ee
Lukellmann <lukellmann@gmail.com>
2022-03-09 16:58:22 +0100

    M12 in user agent

--- common/src/main/kotlin/KordConstants.kt
+++ common/src/main/kotlin/KordConstants.kt
@@ -3,5 +3,5 @@ package dev.kord.common
 public object KordConstants {
 
     /** Kord's value for the [User Agent header](https://discord.com/developers/docs/reference#user-agent). */
-    public const val UserAgent: String = "DiscordBot (https://kord.dev/, 0.8.0-M11)"
+    public const val UserAgent: String = "DiscordBot (https://kord.dev/, 0.8.0-M12)"
 }
