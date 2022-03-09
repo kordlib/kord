d7adb27b0a76faf6bca36c98fd6d541d3d54e4f5
Lukellmann <lukellmann@gmail.com>
2022-03-09 23:58:42 +0100

    update gateway sample

--- gateway/src/samples/kotlin/EventListener.kt
+++ gateway/src/samples/kotlin/EventListener.kt
@@ -1,5 +1,3 @@
-import dev.kord.common.entity.ActivityType
-import dev.kord.common.entity.DiscordBotActivity
 import dev.kord.common.entity.PresenceStatus
 import dev.kord.common.ratelimit.BucketRateLimiter
 import dev.kord.gateway.*
@@ -9,9 +7,7 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.websocket.*
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
@@ -40,30 +36,19 @@ suspend fun main(args: Array<String>) {
             "!close" -> gateway.stop()
             "!detach" -> gateway.detach()
             "!status" -> when (words.getOrNull(1)) {
-                "playing" -> gateway.send(
-                    UpdateStatus(
-                        status = PresenceStatus.Online,
-                        afk = false,
-                        activities = listOf(DiscordBotActivity("Kord", ActivityType.Game)),
-                        since = null
-                    )
-                )
+                "playing" -> gateway.editPresence {
+                    status = PresenceStatus.Online
+                    afk = false
+                    playing("Kord")
+                }
             }
-            "!ping" -> gateway.send(
-                UpdateStatus(
-                    status = PresenceStatus.Online,
-                    afk = false,
-                    activities = listOf(
-                        DiscordBotActivity(
-                            "Ping is ${gateway.ping.value?.inWholeMilliseconds}",
-                            ActivityType.Game
-                        )
-                    ),
-                    since = null
-                )
-            )
+            "!ping" -> gateway.editPresence {
+                status = PresenceStatus.Online
+                afk = false
+                listening("a ${gateway.ping.value?.inWholeMilliseconds} ms ping")
+            }
         }
-    }.launchIn(@OptIn(DelicateCoroutinesApi::class) GlobalScope)
+    }.launchIn(gateway)
 
     gateway.start(token) {
         @OptIn(PrivilegedIntent::class)
