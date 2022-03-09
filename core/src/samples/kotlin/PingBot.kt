f9888c8cffaf1db683ee8a91c1e29e5c248e211a
Lukellmann <lukellmann@gmail.com>
2022-03-10 00:14:49 +0100

    update core sample

--- core/src/samples/kotlin/PingBot.kt
+++ core/src/samples/kotlin/PingBot.kt
@@ -1,6 +1,8 @@
 import dev.kord.core.Kord
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
 
 suspend fun main(args: Array<String>) {
     val kord = Kord(args.firstOrNull() ?: error("token required"))
@@ -10,5 +12,10 @@ suspend fun main(args: Array<String>) {
         if (message.content == "!ping") message.channel.createMessage("pong")
     }
 
-    kord.login { presence { playing("!ping to pong") } }
+    kord.login {
+        presence { playing("!ping to pong") }
+
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
 }
