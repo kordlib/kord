7e0c73bbc1c557dc780634dce141ebee31117bc8
Lukellmann <lukellmann@gmail.com>
2022-03-27 10:32:34 +0200

    inject version at build time

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     `kord-module`
     `kord-sampled-module`
     `kord-publishing`
+
+    // see https://github.com/gmazzo/gradle-buildconfig-plugin
+    id("com.github.gmazzo.buildconfig") version "3.0.3"
 }
 
 dependencies {
@@ -10,4 +13,27 @@ dependencies {
     api(libs.bundles.common)
     testImplementation(libs.bundles.test.implementation)
     testRuntimeOnly(libs.bundles.test.runtime)
+}
+
+/*
+This will generate a file named "BuildConfigGenerated.kt" that looks like:
+
+package dev.kord.common
+
+internal const val BUILD_CONFIG_GENERATED_LIBRARY_VERSION: String = "<version>"
+*/
+buildConfig {
+    packageName("dev.kord.common")
+    className("BuildConfigGenerated")
+
+    useKotlinOutput {
+        topLevelConstants = true
+        internalVisibility = true
+    }
+
+    buildConfigField(
+        type = "String",
+        name = "BUILD_CONFIG_GENERATED_LIBRARY_VERSION",
+        value = "\"${Library.version}\"",
+    )
 }
--- common/src/main/kotlin/KordConstants.kt
+++ common/src/main/kotlin/KordConstants.kt
@@ -1,7 +1,16 @@
+// No const vals, they are inlined, so recompiling would be required when values change.
+@file:Suppress("MayBeConstant")
+
 package dev.kord.common
 
 public object KordConstants {
 
+    /** Kord's version. */
+    public val KORD_VERSION: String = BUILD_CONFIG_GENERATED_LIBRARY_VERSION
+
+    /** URL for Kord's GitHub repository. */
+    public val KORD_GITHUB_URL: String = "https://github.com/kordlib/kord"
+
     /** Kord's value for the [User Agent header](https://discord.com/developers/docs/reference#user-agent). */
-    public const val USER_AGENT: String = "DiscordBot (https://github.com/kordlib/kord, 0.8.0-M12)"
+    public val USER_AGENT: String = "DiscordBot ($KORD_GITHUB_URL, $KORD_VERSION)"
 }
