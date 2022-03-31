56acd5b5bd1071cb2a5c6095fe5c76cb76c9570a
Lukellmann <lukellmann@gmail.com>
2022-03-31 20:46:35 +0200

    StageInstanceCreateBuilder

--- rest/src/main/kotlin/json/request/StageInstanceRequests.kt
+++ rest/src/main/kotlin/json/request/StageInstanceRequests.kt
@@ -1,6 +1,9 @@
 package dev.kord.rest.json.request
 
 import dev.kord.common.entity.Snowflake
+import dev.kord.common.entity.StageInstancePrivacyLevel
+import dev.kord.common.entity.optional.Optional
+import dev.kord.common.entity.optional.OptionalBoolean
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -8,7 +11,11 @@ import kotlinx.serialization.Serializable
 public data class StageInstanceCreateRequest(
     @SerialName("channel_id")
     val channelId: Snowflake,
-    val topic: String
+    val topic: String,
+    @SerialName("privacy_level")
+    val privacyLevel: Optional<StageInstancePrivacyLevel> = Optional.Missing(),
+    @SerialName("send_start_notification")
+    val sendStartNotification: OptionalBoolean = OptionalBoolean.Missing,
 )
 
 @Serializable
