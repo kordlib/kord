971ad90c27f9f8521b8d97a3a0c5df77a6a8c72e
Lukellmann <lukellmann@gmail.com>
2022-04-12 00:40:34 +0200

    add ChannelData.flags

--- core/src/main/kotlin/cache/data/ChannelData.kt
+++ core/src/main/kotlin/cache/data/ChannelData.kt
@@ -33,7 +33,8 @@ public data class ChannelData(
     val messageCount: OptionalInt = OptionalInt.Missing,
     val memberCount: OptionalInt = OptionalInt.Missing,
     val defaultAutoArchiveDuration: Optional<ArchiveDuration> = Optional.Missing(),
-    val member: Optional<ThreadMemberData> = Optional.Missing()
+    val member: Optional<ThreadMemberData> = Optional.Missing(),
+    val flags: Optional<ChannelFlags> = Optional.Missing(),
 ) {
 
 
@@ -66,7 +67,8 @@ public data class ChannelData(
                 messageCount,
                 memberCount,
                 defaultAutoArchiveDuration,
-                member.map { ThreadMemberData.from(it, id) }
+                member.map { ThreadMemberData.from(it, id) },
+                flags,
             )
         }
     }
