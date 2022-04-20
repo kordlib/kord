42c9b45a095c3781f16a04fd5c9700c845df33be
Lukellmann <lukellmann@gmail.com>
2022-04-20 23:45:13 +0200

    Remove project-wide opt-in for ExperimentalStdlibApi

--- buildSrc/src/main/kotlin/Compiler.kt
+++ buildSrc/src/main/kotlin/Compiler.kt
@@ -1,7 +1,6 @@
 object CompilerArguments {
     const val coroutines = "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
     const val time = "-opt-in=kotlin.time.ExperimentalTime"
-    const val stdLib = "-opt-in=kotlin.ExperimentalStdlibApi"
     const val contracts = "-opt-in=kotlin.contracts.ExperimentalContracts"
     const val progressive = "-progressive"
 }
