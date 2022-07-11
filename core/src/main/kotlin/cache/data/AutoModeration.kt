f4e861c97ead016fc06bf8a20e2e7fab6d350279
Lukellmann <lukellmann@gmail.com>
2022-07-11 20:50:05 +0200

    Add cache models

---
+++ core/src/main/kotlin/cache/data/AutoModeration.kt
@@ -0,0 +1,90 @@
+package dev.kord.core.cache.data
+
+import dev.kord.common.entity.*
+import dev.kord.common.entity.optional.Optional
+import dev.kord.common.entity.optional.OptionalSnowflake
+import dev.kord.common.entity.optional.map
+import dev.kord.common.serialization.DurationInSeconds
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class AutoModerationRuleData(
+    val id: Snowflake,
+    val guildId: Snowflake,
+    val name: String,
+    val creatorId: Snowflake,
+    val eventType: AutoModerationRuleEventType,
+    val triggerType: AutoModerationRuleTriggerType,
+    val triggerMetadata: AutoModerationRuleTriggerMetadataData,
+    val actions: List<AutoModerationActionData>,
+    val enabled: Boolean,
+    val exemptRoles: List<Snowflake>,
+    val exemptChannels: List<Snowflake>,
+) {
+    public companion object {
+        public fun from(rule: DiscordAutoModerationRule): AutoModerationRuleData = with(rule) {
+            AutoModerationRuleData(
+                id = id,
+                guildId = guildId,
+                name = name,
+                creatorId = creatorId,
+                eventType = eventType,
+                triggerType = triggerType,
+                triggerMetadata = AutoModerationRuleTriggerMetadataData.from(triggerMetadata),
+                actions = actions.map { AutoModerationActionData.from(it) },
+                enabled = enabled,
+                exemptRoles = exemptRoles,
+                exemptChannels = exemptChannels,
+            )
+        }
+    }
+}
+
+@Serializable
+public data class AutoModerationRuleTriggerMetadataData(
+    val keywordFilter: Optional<List<String>> = Optional.Missing(),
+    val presets: Optional<List<AutoModerationRuleKeywordPresetType>> = Optional.Missing(),
+    val allowList: Optional<List<String>> = Optional.Missing(),
+) {
+    public companion object {
+        public fun from(metadata: DiscordAutoModerationRuleTriggerMetadata): AutoModerationRuleTriggerMetadataData =
+            with(metadata) {
+                AutoModerationRuleTriggerMetadataData(
+                    keywordFilter = keywordFilter,
+                    presets = presets,
+                    allowList = allowList,
+                )
+            }
+    }
+}
+
+@Serializable
+public data class AutoModerationActionData(
+    val type: AutoModerationActionType,
+    val metadata: Optional<AutoModerationActionMetadataData> = Optional.Missing(),
+) {
+    public companion object {
+        public fun from(action: DiscordAutoModerationAction): AutoModerationActionData = with(action) {
+            AutoModerationActionData(
+                type = type,
+                metadata = metadata.map { AutoModerationActionMetadataData.from(it) },
+            )
+        }
+    }
+}
+
+@Serializable
+public data class AutoModerationActionMetadataData(
+    public val channelId: OptionalSnowflake = OptionalSnowflake.Missing,
+    public val durationSeconds: Optional<DurationInSeconds> = Optional.Missing(),
+) {
+    public companion object {
+        public fun from(metadata: DiscordAutoModerationActionMetadata): AutoModerationActionMetadataData =
+            with(metadata) {
+                AutoModerationActionMetadataData(
+                    channelId = channelId,
+                    durationSeconds = durationSeconds,
+                )
+            }
+    }
+}
