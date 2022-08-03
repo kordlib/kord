560f6f3961c79fa9ac3c36d870f3065808affa71
Lukellmann <lukellmann@gmail.com>
2022-08-03 21:55:47 +0200

    Kotlin version in buildSrc dependencies (not plugins)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,10 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     groovy
     `kotlin-dsl`
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.10"
 }
 
 repositories {
@@ -13,14 +9,11 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin"))
-    implementation(kotlin("serialization"))
+    val kotlinVersion = "1.7.10"
+    implementation(kotlin("gradle-plugin", kotlinVersion))
+    implementation(kotlin("serialization", kotlinVersion))
     implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.7.0")
     implementation("org.jetbrains.kotlinx", "atomicfu-gradle-plugin", "0.18.2")
     implementation(gradleApi())
     implementation(localGroovy())
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.languageVersion = "1.5"
 }
