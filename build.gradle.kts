fbdd7e8e2a1863b6d540c707d6ba159f6650f197
Lukellmann <47486203+Lukellmann@users.noreply.github.com>
2022-08-09 09:59:38 +0200

    Upgrade to Gradle 7.5.1 (#662)
    * Verify downloaded Gradle distributions
    
    * Document steps for upgrading Gradle
    
    * Upgrade to Gradle 7.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,18 @@ version = Library.version
 
 tasks {
     wrapper {
-        gradleVersion = "7.5"
+        // Steps for upgrading Gradle:
+        // 1. update `gradleVersion` and `distributionSha256Sum`
+        //    (use 'Complete (-all) ZIP Checksum' found here: https://gradle.org/release-checksums/)
+        // 2. run `./gradlew wrapper`
+        //    (will update 'gradle/wrapper/gradle-wrapper.properties')
+        // 3. run `./gradlew wrapper` again
+        //    (might update 'gradle/wrapper/gradle-wrapper.jar', 'gradlew' and 'gradlew.bat')
+        // 4. commit all changes
+
+        gradleVersion = "7.5.1"
         distributionType = ALL
+        distributionSha256Sum = "db9c8211ed63f61f60292c69e80d89196f9eb36665e369e7f00ac4cc841c2219"
     }
 
     val dokkaOutputDir = "${rootProject.projectDir}/dokka"
