94de408d6f60dcdd0d06d4a77458fa4dfba28f08
Lukellmann <47486203+Lukellmann@users.noreply.github.com>
2022-08-21 09:12:02 +0200

    Use GitHub Pages Deploy Action (#673)
    * Use https://github.com/marketplace/actions/deploy-to-github-pages instead of https://github.com/ajoberstar/gradle-git-publish for publishing docs
    
    * Show actions-user in commits
    
    * Provide tag and branch name in env
    
    * Go back to 0.8.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.ajoberstar.gradle.git.publish.GitPublishExtension
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
 
 plugins {
@@ -6,8 +5,6 @@ plugins {
     kotlin("plugin.serialization")
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.11.0"
     id("org.jetbrains.dokka")
-
-    id("org.ajoberstar.git-publish") version "3.0.1"
 
     signing
     `maven-publish`
@@ -48,19 +45,4 @@ tasks {
         failOnWarning.set(true)
         outputDirectory.set(dokkaOutputDir)
     }
-
-    gitPublishReset {
-        dependsOn(dokkaHtmlMultiModule)
-    }
-}
-
-configure<GitPublishExtension> {
-    repoUri.set("https://github.com/kordlib/kord.git")
-    branch.set("gh-pages")
-
-    contents {
-        from(project.projectDir.resolve("dokka"))
-    }
-
-    commitMessage.set("Update Docs")
 }
