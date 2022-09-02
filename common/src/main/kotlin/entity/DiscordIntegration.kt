914f06fa054d4056fc50e674840d1b9360d8912e
Lukellmann <lukellmann@gmail.com>
2022-09-02 17:29:44 +0200

    Generate `IntegrationExpireBehavior`

--- common/src/main/kotlin/entity/DiscordIntegration.kt
+++ common/src/main/kotlin/entity/DiscordIntegration.kt
@@ -1,17 +1,22 @@
+@file:GenerateKordEnum(
+    name = "IntegrationExpireBehavior", valueType = INT,
+    entries = [
+        Entry("RemoveRole", intValue = 0),
+        Entry("Kick", intValue = 1),
+    ],
+)
+
 package dev.kord.common.entity
 
 import dev.kord.common.entity.optional.Optional
 import dev.kord.common.entity.optional.OptionalBoolean
 import dev.kord.common.serialization.DurationInDays
+import dev.kord.ksp.GenerateKordEnum
+import dev.kord.ksp.GenerateKordEnum.Entry
+import dev.kord.ksp.GenerateKordEnum.ValueType.INT
 import kotlinx.datetime.Instant
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlin.DeprecationLevel.ERROR
 
 @Serializable
@@ -57,29 +62,6 @@ public data class IntegrationApplication(
     val summary: String,
     val bot: Optional<DiscordUser> = Optional.Missing(),
 )
-
-@Serializable(with = IntegrationExpireBehavior.Serializer::class)
-public sealed class IntegrationExpireBehavior(public val value: Int) {
-    public class Unknown(value: Int) : IntegrationExpireBehavior(value)
-    public object RemoveRole : IntegrationExpireBehavior(0)
-    public object Kick : IntegrationExpireBehavior(1)
-
-    public companion object Serializer : KSerializer<IntegrationExpireBehavior> {
-        override val descriptor: SerialDescriptor
-            get() = PrimitiveSerialDescriptor("expire_behavior", PrimitiveKind.INT)
-
-        override fun deserialize(decoder: Decoder): IntegrationExpireBehavior = when (val value = decoder.decodeInt()) {
-            0 -> RemoveRole
-            1 -> Kick
-            else -> Unknown(value)
-        }
-
-        override fun serialize(encoder: Encoder, value: IntegrationExpireBehavior) {
-            encoder.encodeInt(value.value)
-        }
-
-    }
-}
 
 @Serializable
 public data class DiscordIntegrationsAccount(
