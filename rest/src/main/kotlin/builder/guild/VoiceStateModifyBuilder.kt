4fb360fa2d58f09b3e58b8dca4a3f8bc07fdd193
Lukellmann <47486203+Lukellmann@users.noreply.github.com>
2022-09-12 08:24:12 +0200

    Update voice state modification (#680)

--- rest/src/main/kotlin/builder/guild/VoiceStateModifyBuilder.kt
+++ rest/src/main/kotlin/builder/guild/VoiceStateModifyBuilder.kt
@@ -1,55 +1,75 @@
 package dev.kord.rest.builder.guild
 
+import dev.kord.common.annotation.KordDsl
 import dev.kord.common.entity.Snowflake
 import dev.kord.common.entity.optional.Optional
 import dev.kord.common.entity.optional.OptionalBoolean
+import dev.kord.common.entity.optional.OptionalSnowflake
 import dev.kord.common.entity.optional.delegate.delegate
 import dev.kord.rest.builder.RequestBuilder
 import dev.kord.rest.json.request.CurrentVoiceStateModifyRequest
 import dev.kord.rest.json.request.VoiceStateModifyRequest
 import kotlinx.datetime.Instant
+import kotlin.DeprecationLevel.WARNING
 
-public class CurrentVoiceStateModifyBuilder(public val channelId: Snowflake) :
-    RequestBuilder<CurrentVoiceStateModifyRequest> {
+@KordDsl
+public class CurrentVoiceStateModifyBuilder() : RequestBuilder<CurrentVoiceStateModifyRequest> {
 
-    private var _requestToSpeakTimestamp: Optional<Instant?> = Optional.Missing()
+    /** @suppress */
+    @Deprecated(
+        "'channelId' is no longer required, use other constructor instead.",
+        ReplaceWith("CurrentVoiceStateModifyBuilder().apply { this@apply.channelId = channelId }"),
+        level = WARNING,
+    )
+    public constructor(channelId: Snowflake) : this() {
+        this.channelId = channelId
+    }
+
+    private var _channelId: OptionalSnowflake = OptionalSnowflake.Missing
+
+    /** The ID of the channel the current user is currently in. */
+    public var channelId: Snowflake? by ::_channelId.delegate()
 
     private var _suppress: OptionalBoolean = OptionalBoolean.Missing
 
+    /** Toggles the current user's suppress state. */
+    public var suppress: Boolean? by ::_suppress.delegate()
+
+    private var _requestToSpeakTimestamp: Optional<Instant?> = Optional.Missing()
+
     /**
-     * Sets the user's request to speak.
+     * Sets the current user's request to speak.
      * The timestamp is used to sort how users appear on the moderators' request list.
      *
      * e.g: A client who requested to speak at 18:00,
      * will appear above a client who requested to speak at 20:00 in the same timezone.
      *
-     * * A date in the past is treated as "now" by Discord.
-     * * A null value removes the request to speak.
+     * - A date in the past is treated as "now" by Discord.
+     * - A null value removes the request to speak.
      */
     public var requestToSpeakTimestamp: Instant? by ::_requestToSpeakTimestamp.delegate()
 
-    /**
-     *  whether this user is muted by the current user.
-     */
-    public var suppress: Boolean? by ::_suppress.delegate()
-
-
-    override fun toRequest(): CurrentVoiceStateModifyRequest {
-        return CurrentVoiceStateModifyRequest(channelId, _suppress, _requestToSpeakTimestamp)
-    }
+    override fun toRequest(): CurrentVoiceStateModifyRequest = CurrentVoiceStateModifyRequest(
+        channelId = _channelId,
+        suppress = _suppress,
+        requestToSpeakTimestamp = _requestToSpeakTimestamp,
+    )
 }
 
 
-public class VoiceStateModifyBuilder(public val channelId: Snowflake) : RequestBuilder<VoiceStateModifyRequest> {
+@KordDsl
+public class VoiceStateModifyBuilder(
+    /** The ID of the channel the user is currently in. */
+    public var channelId: Snowflake,
+) : RequestBuilder<VoiceStateModifyRequest> {
 
     private var _suppress: OptionalBoolean = OptionalBoolean.Missing
 
-    /**
-     *  whether this user is muted by the current user.
-     */
+    /** Toggles the user's suppress state. */
     public var suppress: Boolean? by ::_suppress.delegate()
 
-    override fun toRequest(): VoiceStateModifyRequest {
-        return VoiceStateModifyRequest(channelId, _suppress)
-    }
+    override fun toRequest(): VoiceStateModifyRequest = VoiceStateModifyRequest(
+        channelId = channelId,
+        suppress = _suppress,
+    )
 }
