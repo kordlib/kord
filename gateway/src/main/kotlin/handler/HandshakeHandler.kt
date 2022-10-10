4cc861ed20f76275e8aae85947ddca82e94a4237
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-10-10 13:04:55 -0300

    Don't consume identifyRateLimiter when resuming a gateway connection

--- gateway/src/main/kotlin/handler/HandshakeHandler.kt
+++ gateway/src/main/kotlin/handler/HandshakeHandler.kt
@@ -34,7 +34,11 @@ internal class HandshakeHandler(
     override fun start() {
         on<Hello> {
             reconnectRetry.reset() // connected and read without problems, resetting retry counter
-            identifyRateLimiter.consume {
+            
+            val resumeContext = resumeContext.value
+            if (resumeContext != null) {
+                send(Resume(configuration.token, resumeContext.sessionId, sequence.value ?: 0))
+            } else identifyRateLimiter.consume {
                 send(resumeOrIdentify)
             }
         }
