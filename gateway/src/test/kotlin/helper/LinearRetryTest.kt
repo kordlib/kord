9e39d8707fdde8622d67b3b483aaa2df1c16e810
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-10-23 23:54:59 +0200

    Add test for extreme inputs

--- gateway/src/test/kotlin/helper/LinearRetryTest.kt
+++ gateway/src/test/kotlin/helper/LinearRetryTest.kt
@@ -1,13 +1,11 @@
 package helper
 
 import dev.kord.gateway.retry.LinearRetry
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.TimeSource
-import kotlin.time.measureTime
 
 class LinearRetryTest {
 
@@ -15,15 +13,32 @@ class LinearRetryTest {
     fun testLinearity() = runTest {
         val linearRetry = LinearRetry(1.seconds, 10.seconds, 10)
         var i = 0
-        val elapsed = TimeSource.Monotonic.measureTime {
-            while (linearRetry.hasNext) {
-                linearRetry.retry()
-                i++
-            }
+        val start = currentTime
+
+        while (linearRetry.hasNext) {
+            linearRetry.retry()
+            i++
         }
 
-        println(elapsed)
+        val end = currentTime
+        val elapsed = (end-start).milliseconds
         assert(elapsed >= 55.seconds)
         assert(i == 10)
+    }
+
+    @Test
+    fun testExtreme() = runTest {
+        val linearRetry = LinearRetry(1.seconds, 60.seconds, Integer.MAX_VALUE)
+        var i = 0
+        val start = currentTime
+
+        while (linearRetry.hasNext && i < 1000) {
+            linearRetry.retry()
+            i++
+        }
+
+        val end = currentTime
+        val elapsed = (end-start).milliseconds
+        assert(elapsed <= 1100.seconds)
     }
 }
