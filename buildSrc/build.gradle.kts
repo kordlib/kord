5617d14bcd26a09f437f093f2a0757328ac2d312
Lukellmann <47486203+Lukellmann@users.noreply.github.com>
2022-11-09 12:19:32 +0100

    Update dependencies (#715)
    * Ktor 2.1.2 -> 2.1.3
    * KSP 1.7.20-1.0.7 -> 1.7.20-1.0.8
    * Binary compatibility validator 0.12.0 -> 0.12.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,6 +13,6 @@ dependencies {
     implementation(kotlin("serialization", kotlinVersion))
     implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.7.20")
     implementation("org.jetbrains.kotlinx", "atomicfu-gradle-plugin", "0.18.5")
-    implementation("com.google.devtools.ksp", "symbol-processing-gradle-plugin", "1.7.20-1.0.7")
+    implementation("com.google.devtools.ksp", "symbol-processing-gradle-plugin", "1.7.20-1.0.8")
     implementation(gradleApi())
 }
