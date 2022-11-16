69fbe3e12efb77aa0dfe9980e977e2f386ab306c
Lukellmann <lukellmann@gmail.com>
2022-11-16 13:35:50 +0100

    Don't include Gradle version in libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,9 @@ repositories {
 group = Library.group
 version = Library.version
 
+// To upgrade Gradle, run the following command twice and commit all changes:
+// ./gradlew wrapper --gradle-version <version> --gradle-distribution-sha256-sum <checksum>
+// (use 'Complete (-all) ZIP Checksum' from https://gradle.org/release-checksums for <checksum>)
 tasks.wrapper {
-    gradleVersion = libs.versions.gradle.get()
     distributionType = ALL
-    distributionSha256Sum = libs.versions.gradleChecksum.get()
 }
