3c75c2399da5fa0c2a0f209d63e71765e16eec96
NoComment1105 <nocomment1105@outlook.com>
2022-11-21 00:14:30 +0000

    Lets get this show on the road

--- core/src/main/kotlin/event/channel/ChannelPinsUpdateEvent.kt
+++ core/src/main/kotlin/event/channel/ChannelPinsUpdateEvent.kt
@@ -1,6 +1,7 @@
 package dev.kord.core.event.channel
 
 import dev.kord.common.entity.Snowflake
+import dev.kord.common.exception.RequestException
 import dev.kord.core.Kord
 import dev.kord.core.behavior.GuildBehavior
 import dev.kord.core.behavior.channel.MessageChannelBehavior
@@ -8,12 +9,22 @@ import dev.kord.core.entity.Strategizable
 import dev.kord.core.entity.channel.MessageChannel
 import dev.kord.core.event.Event
 import dev.kord.core.event.channel.data.ChannelPinsUpdateEventData
+import dev.kord.core.exception.EntityNotFoundException
 import dev.kord.core.supplier.EntitySupplier
 import dev.kord.core.supplier.EntitySupplyStrategy
 import dev.kord.core.supplier.getChannelOf
 import dev.kord.core.supplier.getChannelOfOrNull
 import kotlinx.datetime.Instant
 
+/**
+ * The event of a channels Pins being updated.
+ *
+ * @property data The data associated with the event
+ * @property kord The Kord Instance
+ * @property shard The shard for the event
+ * @property customContext Any custom context provided
+ * @property supplier The entity supplier to use for the event
+ */
 public class ChannelPinsUpdateEvent(
     public val data: ChannelPinsUpdateEventData,
     override val kord: Kord,
@@ -22,20 +33,49 @@ public class ChannelPinsUpdateEvent(
     override val supplier: EntitySupplier = kord.defaultSupplier,
 ) : Event, Strategizable {
 
+    /**
+     * The ID of the channel that triggered the event
+     */
     public val channelId: Snowflake get() = data.channelId
 
+    /**
+     * The ID of the guild that triggered the event, or null
+     */
     public val guildId: Snowflake? get() = data.guildId.value
 
+    /**
+     * The timestamp of the last pin, or null.
+     */
     public val lastPinTimestamp: Instant? get() = data.lastPinTimestamp.value
 
+    /**
+     * The [Guild][GuildBehavior] that triggered the event, or null.
+     */
     public val guild: GuildBehavior? get() = guildId?.let { GuildBehavior(it, kord) }
 
+    /**
+     * The channel that triggered the event.
+     */
     public val channel: MessageChannelBehavior get() = MessageChannelBehavior(channelId, kord)
 
+    /**
+     * Requests to get the channel for the event as type [MessageChannel]
+     *
+     * @throws RequestException if something went wrong while retrieving the channel.
+     * @throws EntityNotFoundException if the channel is null.
+     */
     public suspend fun getChannel(): MessageChannel = supplier.getChannelOf(channelId)
 
+    /**
+     * Requests to get the channel for the event as type [MessageChannel] or null
+     *
+     * @throws RequestException if something went wrong while retrieving the channel.
+     */
     public suspend fun getChannelOrNull(): MessageChannel? = supplier.getChannelOfOrNull(channelId)
 
+    /**
+     * Returns a copy of this class with a new [supplier] provided by the [strategy].
+     */
     override fun withStrategy(strategy: EntitySupplyStrategy<*>): ChannelPinsUpdateEvent =
         ChannelPinsUpdateEvent(data, kord, shard, customContext, strategy.supply(kord))
 
--- core/src/main/kotlin/event/channel/TypingStartEvent.kt
+++ core/src/main/kotlin/event/channel/TypingStartEvent.kt
@@ -19,6 +19,15 @@ import dev.kord.core.supplier.getChannelOf
 import dev.kord.core.supplier.getChannelOfOrNull
 import kotlinx.datetime.Instant
 
+/**
+ * The event that typing begins in a channel.
+ *
+ * @property data The data associated with the event
+ * @property kord The Kord Instance
+ * @property shard The shard for the event
+ * @property customContext Any custom context provided
+ * @property supplier The entity supplier to use for the event
+ */
 public class TypingStartEvent(
     public val data: TypingStartEventData,
     override val kord: Kord,
@@ -26,19 +35,39 @@ public class TypingStartEvent(
     override val customContext: Any?,
     override val supplier: EntitySupplier = kord.defaultSupplier,
 ) : Event, Strategizable {
-
+    /**
+     * The ID of the channel that triggered the event.
+     */
     public val channelId: Snowflake get() = data.channelId
 
+    /**
+     * The ID of the user that triggered the event.
+     */
     public val userId: Snowflake get() = data.userId
 
+    /**
+     * The ID of the guild that triggered the event.
+     */
     public val guildId: Snowflake? get() = data.guildId.value
 
+    /**
+     * The [Instant] this event was triggered.
+     */
     public val started: Instant get() = data.timestamp
 
+    /**
+     * The channel that triggered the event.
+     */
     public val channel: MessageChannelBehavior get() = MessageChannelBehavior(channelId, kord)
 
+    /**
+     * The [Guild][GuildBehavior] that triggered the event.
+     */
     public val guild: GuildBehavior? get() = guildId?.let { GuildBehavior(it, kord) }
 
+    /**
+     * The [User][UserBehavior] that triggered the event.
+     */
     public val user: UserBehavior get() = UserBehavior(userId, kord)
 
     /**
@@ -81,6 +110,9 @@ public class TypingStartEvent(
      */
     public suspend fun getGuild(): Guild? = guildId?.let { supplier.getGuildOrNull(it) }
 
+    /**
+     * Returns a copy of this class with a new [supplier] provided by the [strategy].
+     */
     override fun withStrategy(strategy: EntitySupplyStrategy<*>): TypingStartEvent =
         TypingStartEvent(data, kord, shard, customContext, strategy.supply(kord))
 
