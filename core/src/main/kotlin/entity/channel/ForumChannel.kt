70c6452363de53cee021275853899bd3f91fcdaa
NoComment <nocomment1105@outlook.com>
2023-01-24 18:50:14 +0000

    Add missing default layout to core type

--- core/src/main/kotlin/entity/channel/ForumChannel.kt
+++ core/src/main/kotlin/entity/channel/ForumChannel.kt
@@ -1,11 +1,11 @@
 package dev.kord.core.entity.channel
 
+import dev.kord.common.entity.ForumLayoutType
 import dev.kord.core.Kord
 import dev.kord.core.behavior.channel.threads.ThreadParentChannelBehavior
 import dev.kord.core.cache.data.ChannelData
 import dev.kord.core.supplier.EntitySupplier
 import dev.kord.core.supplier.EntitySupplyStrategy
-import dev.kord.rest.builder.channel.ForumChannelModifyBuilder
 
 public class ForumChannel(
     override val data: ChannelData,
@@ -17,6 +17,11 @@ public class ForumChannel(
      * The channel topic, if present.
      */
     public val topic: String? get() = data.topic.value
+
+    /**
+     * The default layout of the forum, if present.
+     */
+    public val defaultForumLayout: ForumLayoutType? get() = data.defaultForumLayout.value
 
     override fun withStrategy(strategy: EntitySupplyStrategy<*>): ForumChannel {
         return ForumChannel(data,kord, strategy.supply(kord))
