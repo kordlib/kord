0ddd74a54c832eca22bcfe8700a14f1f0b3aa1e5
Luis <40919071+oluiss@users.noreply.github.com>
2023-02-16 19:33:15 -0300

    Update Icon.kt

--- core/src/main/kotlin/entity/Icon.kt
+++ core/src/main/kotlin/entity/Icon.kt
@@ -46,11 +46,16 @@ public sealed class Icon(
     public class EmojiIcon(animated: Boolean, emojiId: Snowflake, kord: Kord) :
         Icon(if (animated) Image.Format.GIF else Image.Format.WEBP, animated, DiscordCdn.emoji(emojiId), kord)
 
-    public class DefaultUserAvatar(discriminator: Int, kord: Kord) : Icon(Image.Format.PNG /* Discord Default Avatars only support PNG */, false, DiscordCdn.defaultAvatar(discriminator), kord)
+    public class DefaultUserAvatar(discriminator: Int, kord: Kord) :
+        Icon(Image.Format.PNG /* Discord Default Avatars only support PNG */, false, DiscordCdn.defaultAvatar(discriminator), kord)
 
-    public class UserAvatar(userId: Snowflake, avatarHash: String, kord: Kord) : Icon(if (avatarHash.startsWith("a_")) Image.Format.GIF else Image.Format.WEBP, avatarHash.startsWith("a_"), DiscordCdn.userAvatar(userId, avatarHash), kord)
+    public class UserAvatar(userId: Snowflake, avatarHash: String, kord: Kord) :
+        Icon(if (avatarHash.startsWith("a_")) Image.Format.GIF else Image.Format.WEBP, avatarHash.startsWith("a_"), DiscordCdn.userAvatar(userId, avatarHash), kord)
 
-    public class MemberAvatar(guildId: Snowflake, userId: Snowflake, avatarHash: String, kord: Kord) : Icon(if (avatarHash.startsWith("a_")) Image.Format.GIF else Image.Format.WEBP, avatarHash.startsWith("a_"), DiscordCdn.memberAvatar(guildId, userId, avatarHash), kord)
+    public class MemberAvatar(guildId: Snowflake, userId: Snowflake, avatarHash: String, kord: Kord) :
+        Icon(if (avatarHash.startsWith("a_")) Image.Format.GIF else Image.Format.WEBP, avatarHash.startsWith("a_"), DiscordCdn.memberAvatar(guildId, userId, avatarHash), kord)
 
-    public class RoleIcon(roleId: Snowflake, iconHash: String, kord: Kord) : Icon(if (iconHash.startsWith("a_")) Image.Format.GIF else Image.Format.WEBP, iconHash.startsWith("a_"), DiscordCdn.roleIcon(roleId, iconHash), kord)
+    public class RoleIcon(roleId: Snowflake, iconHash: String, kord: Kord) :
+        Icon(if (iconHash.startsWith("a_")) Image.Format.GIF else Image.Format.WEBP, iconHash.startsWith("a_"), DiscordCdn.roleIcon(roleId, iconHash), kord)
+
 }
