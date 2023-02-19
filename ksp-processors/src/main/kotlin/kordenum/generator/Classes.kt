df77f29bdbbf11ffdb1d99a6a60b849540fa478b
Michael Rittmeister <michael@rittmeister.in>
2023-02-19 09:45:48 +0100

    Remove Classes.kt

--- ksp-processors/src/main/kotlin/kordenum/generator/Classes.kt
+++
@@ -1,7 +0,0 @@
-package dev.kord.ksp.kordenum.generator
-
-import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.MemberName
-import com.squareup.kotlinpoet.asClassName
-import kotlinx.serialization.KSerializer
-
