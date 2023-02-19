491b18c04a62cee452d1849042005adb48d72d00
Michael Rittmeister <michael@rittmeister.in>
2023-02-19 16:28:35 +0100

    Generate GuildMemberFlags

--- common/src/main/kotlin/entity/Member.kt
+++ common/src/main/kotlin/entity/Member.kt
@@ -1,17 +1,25 @@
+@file:GenerateKordEnum(
+    name = "GuildMemberFlag",
+    valueType = GenerateKordEnum.ValueType.INT,
+    isFlags = true,
+    entries = [
+        GenerateKordEnum.Entry(name = "DidRejoin", intValue = 1 shl 0),
+        GenerateKordEnum.Entry(name = "CompletedOnboarding", intValue = 1 shl 1),
+        GenerateKordEnum.Entry(name = "BypassesVerification", intValue = 1 shl 2),
+        GenerateKordEnum.Entry(name = "StartedOnboarding", intValue = 1 shl 3)
+    ]
+)
+
 package dev.kord.common.entity
+
 
 import dev.kord.common.entity.optional.Optional
 import dev.kord.common.entity.optional.OptionalBoolean
 import dev.kord.common.entity.optional.OptionalSnowflake
+import dev.kord.ksp.GenerateKordEnum
 import kotlinx.datetime.Instant
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 public data class DiscordGuildMember(
@@ -113,34 +121,3 @@ public data class DiscordThreadMember(
     val joinTimestamp: Instant,
     val flags: Int
 )
-
-@Serializable(with = GuildMemberFlags.Companion::class)
-public data class GuildMemberFlags(val code: Int) {
-
-    public operator fun contains(flag: GuildMemberFlags): Boolean {
-        return this.code and flag.code == flag.code
-    }
-
-    public companion object : KSerializer<GuildMemberFlags> {
-
-        override val descriptor: SerialDescriptor
-            get() = PrimitiveSerialDescriptor("flags", PrimitiveKind.INT)
-
-        override fun deserialize(decoder: Decoder): GuildMemberFlags {
-            return GuildMemberFlags(decoder.decodeInt())
-        }
-
-        override fun serialize(encoder: Encoder, value: GuildMemberFlags) {
-            encoder.encodeInt(value.code)
-        }
-    }
-
-}
-
-@Serializable
-public enum class GuildMemberFlag(public val code: Int) {
-    DID_REJOIN(1.shl(0)),
-    COMPLETED_ONBOARDING(1.shl(1)),
-    BYPASSES_VERIFICATION(1.shl(2)),
-    STARTED_ONBOARDING(1.shl(3))
-}
