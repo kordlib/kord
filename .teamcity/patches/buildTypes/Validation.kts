921b4fff8b9f61c1e3db6073e7676d625b933305
michael.rittmeister@gmail.com <michael.rittmeister@gmail.com@TeamCity>
2023-03-23 12:21:51 +0000

    TeamCity change in 'Kord' project: VCS roots of 'Validation' build configuration were updated

--- .teamcity/patches/buildTypes/Validation.kts
+++ .teamcity/patches/buildTypes/Validation.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("Validation")
     name = "Validation"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
