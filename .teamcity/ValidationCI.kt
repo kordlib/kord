c4ffbbf3a1c11b82418ac6592f1b92649aeffbc7
Michael Rittmeister <michael@rittmeister.in>
2023-03-23 14:17:18 +0100

    Fail if test count changes significantly

--- .teamcity/ValidationCI.kt
+++ .teamcity/ValidationCI.kt
@@ -1,4 +1,7 @@
+import jetbrains.buildServer.configs.kotlin.FailureConditions
 import jetbrains.buildServer.configs.kotlin.RelativeId
+import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnMetric
+import jetbrains.buildServer.configs.kotlin.failureConditions.failOnMetricChange
 
 val ValidationCI = KordBuild("Validate Code") {
     id = RelativeId("Validation")
@@ -9,6 +12,23 @@ val ValidationCI = KordBuild("Validate Code") {
     steps {
         debuggableGradle("Run checks") {
             tasks = "check"
+        }
+    }
+
+    failureConditions {
+        failOnSignificantDecreaseOf(BuildFailureOnMetric.MetricType.TEST_COUNT)
+        failOnSignificantDecreaseOf(BuildFailureOnMetric.MetricType.TEST_IGNORED_COUNT)
+    }
+}
+
+private fun FailureConditions.failOnSignificantDecreaseOf(metricType: BuildFailureOnMetric.MetricType) {
+    failOnMetricChange {
+        metric = metricType
+        threshold = 20
+        units = BuildFailureOnMetric.MetricUnit.PERCENTS
+        comparison = BuildFailureOnMetric.MetricComparison.LESS
+        compareTo = build {
+            buildRule = lastSuccessful()
         }
     }
 }
