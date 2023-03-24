2ac7eaae5b88312e153d20fff599ce6b1817d2ff
Michael Rittmeister <michael@rittmeister.in>
2023-03-24 20:09:14 +0100

    Fix CI secret

--- .teamcity/Shared.kt
+++ .teamcity/Shared.kt
@@ -13,6 +13,8 @@ fun KordBuild(name: String, configure: BuildType.() -> Unit) = object : BuildTyp
         checkbox(debugParamName, false.toString(), "Debug Mode", "Run build with debug logging enabled")
 
         param("env.GITHUB_BRANCH", "%teamcity.build.branch%")
+
+        password("env.KORD_TEST_TOKEN", "credentialsJSON:cbd8be1d-4808-42ff-90cf-551e9f60e83d")
     }
 
     features {
