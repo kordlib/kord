49d0ec749c9a467330c58b666c2e52b54dde56cc
Michael Rittmeister <michael@rittmeister.in>
2023-03-24 20:07:07 +0100

    Mark environmen variable as secure

--- .teamcity/ValidationCI.kt
+++ .teamcity/ValidationCI.kt
@@ -10,8 +10,8 @@ val ValidationCI = KordBuild("Validate Code") {
     }
 
     steps {
+        password("env.KORD_TEST_TOKEN", "credentialsJSON:cbd8be1d-4808-42ff-90cf-551e9f60e83d")
         debuggableGradle("Run checks") {
-            param("env.KORD_TEST_TOKEN", "credentialsJSON:cbd8be1d-4808-42ff-90cf-551e9f60e83d")
             tasks = "check"
         }
 
