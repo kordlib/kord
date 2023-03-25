8badb69a09b3f11f4435346f3e07e0d6fe0e4a78
Michael Rittmeister <michael@rittmeister.in>
2023-03-25 12:54:23 +0100

    Fix compilation error

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -11,6 +11,7 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(projects.core)
+                compileOnly(projects.kspAnnotations)
             }
         }
 
