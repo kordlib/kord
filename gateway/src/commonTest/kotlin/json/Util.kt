e54a49f9d603fc7c68348c15c35d16c70f71fe30
Michael Rittmeister <michael@rittmeister.in>
2023-03-25 12:55:08 +0100

    Fix gateway tests

--- gateway/src/commonTest/kotlin/json/Util.kt
+++ gateway/src/commonTest/kotlin/json/Util.kt
@@ -3,7 +3,7 @@ package json
 import dev.kord.test.file
 import kotlin.test.assertEquals
 
-suspend fun readFile(prefix: String, name: String) = file("gateway", "json/$prefix/$name.jspon")
+suspend fun readFile(prefix: String, name: String) = file("gateway", "json/$prefix/$name.json")
 
 infix fun <T> T.shouldBe(that: T) {
     assertEquals(that, this)
