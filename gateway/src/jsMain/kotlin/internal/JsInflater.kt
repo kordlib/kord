71d9c378745cf15cba04851b21d38541da93aff5
Michael Rittmeister <michael@rittmeister.in>
2023-03-30 22:21:58 +0200

    Make Inflate class no longer open

--- gateway/src/jsMain/kotlin/internal/JsInflater.kt
+++ gateway/src/jsMain/kotlin/internal/JsInflater.kt
@@ -6,6 +6,6 @@ package dev.kord.gateway.internal
 import js.typedarrays.Uint8Array
 import node.buffer.Buffer
 
-internal open external class Inflate {
-    open fun process(data: Uint8Array): Buffer
+internal external class Inflate {
+    fun process(data: Uint8Array): Buffer
 }
