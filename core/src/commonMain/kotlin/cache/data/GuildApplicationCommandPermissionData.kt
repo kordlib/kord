d7b05c75b912cfa4022ab64c3540b79a78c9aee6
Lukellmann <lukellmann@gmail.com>
2023-03-30 23:47:14 +0200

    Remove unused imports

--- core/src/commonMain/kotlin/cache/data/GuildApplicationCommandPermissionData.kt
+++ core/src/commonMain/kotlin/cache/data/GuildApplicationCommandPermissionData.kt
@@ -3,9 +3,6 @@ package dev.kord.core.cache.data
 import dev.kord.common.entity.ApplicationCommandPermissionType
 import dev.kord.common.entity.DiscordGuildApplicationCommandPermission
 import dev.kord.common.entity.Snowflake
-import kotlin.DeprecationLevel.HIDDEN
-import kotlin.jvm.JvmName
-
 
 public data class GuildApplicationCommandPermissionData(
     val id: Snowflake,
