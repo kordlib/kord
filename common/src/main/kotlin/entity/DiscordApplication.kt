43e5bec73b89a96ab7aa99089ba824d86ae01b86
NoComment <67918617+NoComment1105@users.noreply.github.com>
2023-04-02 15:46:51 +0100

    Add ApplicationFlag.ApplicationAutoModerationRuleCreateBadge (#802)
    see https://github.com/discord/discord-api-docs/pull/6021

--- common/src/main/kotlin/entity/DiscordApplication.kt
+++ common/src/main/kotlin/entity/DiscordApplication.kt
@@ -109,6 +109,9 @@ public data class DiscordPartialApplication(
 
 public enum class ApplicationFlag(public val code: Int) {
 
+    /** Indicates if an app uses the Auto Moderation API. */
+    ApplicationAutoModerationRuleCreateBadge(1 shl 6),
+
     /**
      * Intent required for bots in **100 or more servers** to receive
      * [`PresenceUpdate` events](https://discord.com/developers/docs/topics/gateway#presence-update).
