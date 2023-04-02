066757688f114f77ec885c40e3dc4e6972acd078
Lukellmann <lukellmann@gmail.com>
2023-04-03 01:48:49 +0200

    Clean test-kit annotations

--- test-kit/src/commonMain/kotlin/Annotations.kt
+++ test-kit/src/commonMain/kotlin/Annotations.kt
@@ -3,17 +3,14 @@
 package dev.kord.test
 
 import kotlin.annotation.AnnotationTarget.CLASS
+import kotlin.annotation.AnnotationTarget.FUNCTION
 
-/**
- * Ignores this Test on JS platforms.
- */
-@Target(AnnotationTarget.FUNCTION, CLASS)
+/** Ignores this test on JS platforms. */
+@Target(CLASS, FUNCTION)
 @OptionalExpectation
 expect annotation class IgnoreOnJs()
 
-/**
- * Ignores this test on JVM.
- */
-@Target(AnnotationTarget.FUNCTION, CLASS)
+/** Ignores this test on the JVM. */
+@Target(CLASS, FUNCTION)
 @OptionalExpectation
 expect annotation class IgnoreOnJvm()
