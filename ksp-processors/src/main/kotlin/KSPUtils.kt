b573d62093bec9e8c356ed4dee12e07ada2b509b
Lukellmann <lukellmann@gmail.com>
2023-04-03 03:34:36 +0200

    Clean KSPUtils

--- ksp-processors/src/main/kotlin/KSPUtils.kt
+++ ksp-processors/src/main/kotlin/KSPUtils.kt
@@ -16,20 +16,20 @@ internal fun KSAnnotation.isOfType(qualifiedName: String) =
         && annotationType.resolve().declaration.qualifiedName?.asString() == qualifiedName
 
 internal class AnnotationArguments private constructor(private val map: Map<String, Any?>) {
-    private inline fun <reified V> get(parameter: KProperty1<out Annotation, V>) = map[parameter.name] as V?
+    private inline fun <reified V : Any> get(parameter: KProperty1<out Annotation, V>) = map[parameter.name] as V?
 
-    internal inline fun <reified V>  getSafe(parameter: KProperty1<out Annotation, V>) = get(parameter) ?: error("Missing required parameter: $parameter")
+    internal inline fun <reified V : Any> getSafe(parameter: KProperty1<out Annotation, V>) =
+        get(parameter) ?: error("Missing required parameter: $parameter")
+
     // https://github.com/google/ksp/issues/885
-    internal inline fun <reified V>  getOrDefault(parameter: KProperty1<out Annotation, V>, defaultValue: V) = get(parameter) ?: defaultValue
+    internal inline fun <reified V : Any> getOrDefault(parameter: KProperty1<out Annotation, V>, defaultValue: V) =
+        get(parameter) ?: defaultValue
 
     internal fun getRaw(parameter: KProperty1<out Annotation, Any>) = map[parameter.name]
 
     internal companion object {
         internal val KSAnnotation.annotationArguments: AnnotationArguments
-            get() {
-                return AnnotationArguments(arguments
-                    .associate { it.name!!.getShortName() to it.value })
-            }
+            get() = AnnotationArguments(arguments.associate { it.name!!.getShortName() to it.value })
     }
 }
 
