ab19ddea8931bf1f11476790ace4e1267c8e9e31
Lukellmann <lukellmann@gmail.com>
2023-04-05 17:09:32 +0200

    No documentation for ksp-annotations

--- ksp-annotations/build.gradle.kts
+++ ksp-annotations/build.gradle.kts
@@ -1,7 +1,13 @@
+import org.jetbrains.dokka.gradle.AbstractDokkaLeafTask
+
 plugins {
     `kord-internal-multiplatform-module`
 
-    // workaround for https://youtrack.jetbrains.com/issue/KT-43500 (intended to be unpublished)
+    // workaround for https://youtrack.jetbrains.com/issue/KT-43500 (not intended to be published)
     org.jetbrains.dokka
     `kord-publishing`
+}
+
+tasks.withType<AbstractDokkaLeafTask>().configureEach {
+    enabled = false
 }
