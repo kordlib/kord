f7802269c9dd22d3407534dbb6e3267b606d337e
Michael Rittmeister <michael@rittmeister.in>
2023-04-13 00:46:48 +0200

    Specify publishing secrets differently

--- .teamcity/DeploymentCI.kt
+++ .teamcity/DeploymentCI.kt
@@ -9,13 +9,6 @@ val DeploymentCI = KordBuild("Deployment") {
         vcs()
     }
 
-    params {
-        password("env.NEXUS_USER", "credentialsJSON:1fcb58be-049c-43cc-b002-582f691f12c0")
-        password("env.NEXUS_PASSWORD", "credentialsJSON:f7652101-08d1-4abf-842a-4926fdaa749c")
-        password("system.org.gradle.project.signingKey", "credentialsJSON:aebe42c4-8827-4b93-afaf-7a1bf4f46a51")
-        password("system.org.gradle.project.signingPassword", "credentialsJSON:7de31dac-638d-43b6-a070-9e9d9ed3db22")
-    }
-
     steps {
         debuggableGradle("Run checks") {
             tasks = "check"
--- .teamcity/Shared.kt
+++ .teamcity/Shared.kt
@@ -16,10 +16,12 @@ fun KordBuild(name: String, configure: BuildType.() -> Unit) = object : BuildTyp
 
     params {
         checkbox(debugParamName, false.toString(), "Debug Mode", "Run build with debug logging enabled")
-
         param("env.GITHUB_BRANCH", "%teamcity.build.branch%")
-
         password("env.KORD_TEST_TOKEN", "credentialsJSON:6f4d984c-83d2-4914-8dea-dc0ed996d81d")
+        password("env.NEXUS_USER", "credentialsJSON:1fcb58be-049c-43cc-b002-582f691f12c0")
+        password("env.NEXUS_PASSWORD", "credentialsJSON:f7652101-08d1-4abf-842a-4926fdaa749c")
+        password("system.org.gradle.project.signingKey", "credentialsJSON:aebe42c4-8827-4b93-afaf-7a1bf4f46a51")
+        password("system.org.gradle.project.signingPassword", "credentialsJSON:7de31dac-638d-43b6-a070-9e9d9ed3db22")
     }
 
     features {
