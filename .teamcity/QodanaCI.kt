ca0d5b01c8afc8e25b54ef234cb31cffd652367e
Michael Rittmeister <michael@rittmeister.in>
2023-04-12 22:15:43 +0200

    Add trigger for Qodana CI

--- .teamcity/QodanaCI.kt
+++ .teamcity/QodanaCI.kt
@@ -5,6 +5,10 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.qodana
 val QodanaCI = KordBuild("Qodana") {
     id = RelativeId("qodana")
 
+    triggers {
+        vcs()
+    }
+
     steps {
         qodana {
             cloudToken = "credentialsJSON:acbac377-214a-4239-a3bb-bcf3ae29d83e"
