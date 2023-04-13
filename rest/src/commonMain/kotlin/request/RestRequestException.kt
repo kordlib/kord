d48e117a9cf828a87013b768297653edffd89376
Lukellmann <lukellmann@gmail.com>
2023-04-13 13:14:01 +0200

    Remove val from constructor parameters that are never used as properties

--- rest/src/commonMain/kotlin/request/RestRequestException.kt
+++ rest/src/commonMain/kotlin/request/RestRequestException.kt
@@ -34,7 +34,7 @@ public data class HttpStatus(val code: Int, val message: String)
  * Implementation of the [RestRequestException] for [RestServices][RestService] using Ktor.
  */
 public class KtorRequestException(
-    public val httpResponse: KtorResponse,
+    @Suppress("MemberVisibilityCanBePrivate") public val httpResponse: KtorResponse,
     request: Request<*, *>,
     discordError: DiscordErrorResponse?,
 ) : RestRequestException(request, HttpStatus(httpResponse.status.value, httpResponse.status.description), discordError)
