17b6d53edc5152545d864e3c5265a156421c29f2
Michael Rittmeister <michael@rittmeister.in>
2023-04-13 18:53:17 +0200

    Use different sonatype credentials

--- .teamcity/Shared.kt
+++ .teamcity/Shared.kt
@@ -18,8 +18,8 @@ fun KordBuild(name: String, configure: BuildType.() -> Unit) = object : BuildTyp
         checkbox(debugParamName, false.toString(), "Debug Mode", "Run build with debug logging enabled")
         param("env.GITHUB_BRANCH_NAMEf", "%teamcity.build.branch%")
         password("env.KORD_TEST_TOKEN", "credentialsJSON:6f4d984c-83d2-4914-8dea-dc0ed996d81d")
-        password("env.NEXUS_USER", "credentialsJSON:1fcb58be-049c-43cc-b002-582f691f12c0")
-        password("env.NEXUS_PASSWORD", "credentialsJSON:f7652101-08d1-4abf-842a-4926fdaa749c")
+        password("env.NEXUS_USER", "credentialsJSON:8e4c071b-df9d-4701-b8df-85058c1bf7ef")
+        password("env.NEXUS_PASSWORD", "credentialsJSON:69d63bf7-8064-4062-a01b-96aa50fdc890")
         password("system.org.gradle.project.signingKey", "credentialsJSON:aebe42c4-8827-4b93-afaf-7a1bf4f46a51")
         password("system.org.gradle.project.signingPassword", "credentialsJSON:7de31dac-638d-43b6-a070-9e9d9ed3db22")
     }
