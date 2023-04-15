e8007c1c91193c1a3e2278c0efc7f2a2608f74f5
Michael Rittmeister <michael@rittmeister.in>
2023-04-15 19:02:53 +0200

    Implement Throwable.isTimeout() on native

--- gateway/src/nativeMain/kotlin/DefaultGateway.kt
+++ gateway/src/nativeMain/kotlin/DefaultGateway.kt
@@ -1,9 +1,11 @@
 package dev.kord.gateway
 
+import dev.kord.common.annotation.KordInternal
+import io.ktor.util.network.*
 import kotlin.experimental.ExperimentalNativeApi
 
-// TODO: Timeout
-internal actual fun Throwable.isTimeout() = false
+@KordInternal
+public actual fun Throwable.isTimeout(): Boolean = this is UnresolvedAddressException
 
 @OptIn(ExperimentalNativeApi::class)
 internal actual val os: String get() = Platform.osFamily.name
