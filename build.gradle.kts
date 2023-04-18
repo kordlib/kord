4b41895f49182a3dc852aee3d6d0ef292da0a1e6
Michael Rittmeister <michael@rittmeister.in>
2023-04-18 15:10:26 +0200

    Add missing gradle config from lsat commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,46 @@ repositories {
 
 group = Library.group
 version = Library.version
+
+dependencies {
+    dokkaHtmlMultiModulePlugin(libs.dokka.versioning.plugin)
+}
+
+tasks {
+    val oldDocsRoot = "docs-cache"
+    dokkaHtmlMultiModule {
+        applyVersioningPlugin()
+    }
+
+    val saveDocs = task<Copy>("saveVersionedDocs") {
+        fromDokkaMutliModule()
+        if (Library.isSnapshot) {
+            into("$oldDocsRoot/current")
+        } else {
+            into("$oldDocsRoot/${Library.version}")
+        }
+    }
+
+    task<Copy>("produceDocs") {
+        dependsOn(saveDocs)
+        // copy old versions
+        from(oldDocsRoot) {
+            exclude("current")
+        }
+        // copy up to date version
+        fromDokkaMutliModule {
+            if (Library.isRelease) {
+                into(Library.version)
+            }
+        }
+        into("docs")
+    }
+}
+
+// For some reason from(dokkaHtmlMultiModule) produces some really weird output
+inline fun Copy.fromDokkaMutliModule(crossinline configure: CopySpec.() -> Unit = {}) {
+    from("$buildDir/dokka/htmlMultiModule") {
+        configure()
+    }
+    dependsOn(tasks.dokkaHtmlMultiModule)
+}
