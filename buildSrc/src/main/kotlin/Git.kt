5381371db9b2e4798a2262687e28c97ac18ded72
Lukellmann <lukellmann@gmail.com>
2023-04-28 01:29:57 +0200

    Use exec instead of ProcessBuilder

--- buildSrc/src/main/kotlin/Git.kt
+++ buildSrc/src/main/kotlin/Git.kt
@@ -1,12 +1,13 @@
 import org.gradle.api.Project
+import java.io.ByteArrayOutputStream
 
 internal fun Project.git(vararg command: String): String {
-    val process = ProcessBuilder("git", *command)
-        .redirectErrorStream(true)
-        .directory(rootDir)
-        .start()
-    val output = process.inputStream.reader().use { it.readText() }.trim()
-    val exitStatus = process.waitFor()
-    check(exitStatus == 0) { "git exited with status $exitStatus, output:\n$output" }
-    return output
+    val output = ByteArrayOutputStream()
+    exec {
+        commandLine("git", *command)
+        standardOutput = output
+        errorOutput = output
+        workingDir = rootDir
+    }.rethrowFailure().assertNormalExitValue()
+    return output.toString().trim()
 }
