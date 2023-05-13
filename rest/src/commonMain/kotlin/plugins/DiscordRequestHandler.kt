eaaf0af90be2c33f903ca86ef11e7fe06e321a75
hope <truehope9000@gmail.com>
2023-05-13 17:00:48 +0300

    Discord Ratelimit Plugin

---
+++ rest/src/commonMain/kotlin/plugins/DiscordRequestHandler.kt
@@ -0,0 +1,31 @@
+package dev.kord.rest.plugins
+
+import dev.kord.rest.ratelimit.ExclusionRequestRateLimiter
+import dev.kord.rest.ratelimit.RequestRateLimiter
+import dev.kord.rest.ratelimit.RequestResponse
+import dev.kord.rest.ratelimit.RequestToken
+import dev.kord.rest.request.RequestIdentifier
+import dev.kord.rest.request.from
+import io.ktor.client.plugins.api.*
+import io.ktor.http.*
+import io.ktor.util.*
+import kotlinx.datetime.Clock
+
+public class DiscordRatelimitPluginConfig {
+    public var requestRateLimiter: RequestRateLimiter = ExclusionRequestRateLimiter()
+    public var clock: Clock = Clock.System
+}
+
+public var DiscordRatelimitPlugin: ClientPlugin<DiscordRatelimitPluginConfig> =  createClientPlugin("DiscordRequestHandler", ::DiscordRatelimitPluginConfig) {
+    val requestRateLimiter = pluginConfig.requestRateLimiter
+    val clock = pluginConfig.clock
+    val requestTokenKey = AttributeKey<RequestToken>("requestToken")
+    onRequest { request, _ ->
+        val requestToken = requestRateLimiter.await(request)
+        request.attributes.put(requestTokenKey,requestToken)
+    }
+    onResponse { response ->
+        val requestToken = response.call.attributes[requestTokenKey]
+        requestToken.complete(RequestResponse.from(response, clock))
+    }
+}
