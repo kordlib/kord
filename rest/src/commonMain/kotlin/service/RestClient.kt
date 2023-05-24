b40f6181a1b466118a19e129cdcfbf157b95f8b2
hope <truehope9000@gmail.com>
2023-05-24 13:51:44 +0300

    Remove Ktor Handler from RestClient

--- rest/src/commonMain/kotlin/service/RestClient.kt
+++ rest/src/commonMain/kotlin/service/RestClient.kt
@@ -2,57 +2,32 @@ package dev.kord.rest.service
 
 import dev.kord.common.annotation.KordExperimental
 import dev.kord.common.annotation.KordUnsafe
-import dev.kord.rest.request.KtorRequestHandler
-import dev.kord.rest.route.Route
+import io.ktor.client.*
+
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
-public class RestClient(requestHandler: RequestHandler) : RestService(requestHandler) {
+public class RestClient(client: HttpClient) {
 
     // order like in docs:
 
     // interactions
-    public val interaction: InteractionService = InteractionService(requestHandler)
+    public val interaction: InteractionService = InteractionService(client)
 
     // resources
-    public val auditLog: AuditLogService = AuditLogService(requestHandler)
-    public val autoModeration: AutoModerationService = AutoModerationService(requestHandler)
-    public val channel: ChannelService = ChannelService(requestHandler)
-    public val emoji: EmojiService = EmojiService(requestHandler)
-    public val guild: GuildService = GuildService(requestHandler)
-    public val template: TemplateService = TemplateService(requestHandler)
-    public val invite: InviteService = InviteService(requestHandler)
-    public val stageInstance: StageInstanceService = StageInstanceService(requestHandler)
-    public val sticker: StickerService = StickerService(requestHandler)
-    public val user: UserService = UserService(requestHandler)
-    public val voice: VoiceService = VoiceService(requestHandler)
-    public val webhook: WebhookService = WebhookService(requestHandler)
+    public val auditLog: AuditLogService = AuditLogService(client)
+    public val autoModeration: AutoModerationService = AutoModerationService(client)
+    public val channel: ChannelService = ChannelService(client)
+    public val emoji: EmojiService = EmojiService(client)
+    public val guild: GuildService = GuildService(client)
+    public val template: TemplateService = TemplateService(client)
+    public val invite: InviteService = InviteService(client)
+    public val stageInstance: StageInstanceService = StageInstanceService(client)
+    public val sticker: StickerService = StickerService(client)
+    public val user: UserService = UserService(client)
+    public val voice: VoiceService = VoiceService(client)
+    public val webhook: WebhookService = WebhookService(client)
 
     // topics
-    public val application: ApplicationService = ApplicationService(requestHandler)
-
-    /**
-     * Sends a request to the given [route]. This function exposes a direct call to the Discord api and allows
-     * the user to send a custom [RequestBuilder.body].
-     *
-     * Unless such functionality is specifically needed, users are advised to use the safer [RestService] calls.
-     *
-     * @param route The route to which to send a request.
-     * @param block The configuration for this request.
-     */
-    @KordUnsafe
-    @KordExperimental
-    public suspend inline fun <T> unsafe(route: Route<T>, block: RequestBuilder<T>.() -> Unit): T {
-        contract {
-            callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-        }
-        return call(route) {
-            block()
-        }
-    }
-}
-
-public fun RestClient(token: String): RestClient {
-    val requestHandler = KtorRequestHandler(token)
-    return RestClient(requestHandler)
+    public val application: ApplicationService = ApplicationService(client)
 }
