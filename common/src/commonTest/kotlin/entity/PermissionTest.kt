16cb350cfff357f86971075f1769008e9741761a
lukellmann <lukellmann@gmail.com>
2023-08-14 15:25:01 +0200

    Add tests

---
+++ common/src/commonTest/kotlin/entity/PermissionTest.kt
@@ -0,0 +1,33 @@
+package dev.kord.common.entity
+
+import dev.kord.ksp.Generate.EntityType.INT_FLAGS
+import kotlin.random.Random
+import kotlin.random.nextInt
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
+
+/** Test representative for generated [INT_FLAGS]. */
+class PermissionTest {
+    @Test
+    fun values_returns_expected_flags() {
+        val expected = Permission.entries.map { Permission.fromShift(Random.nextInt(0..99)) }.toSet()
+        assertEquals(expected, actual = Permissions(expected).values)
+        assertEquals(expected = Permission.entries.toSet(), actual = Permissions(Permission.entries).values)
+    }
+
+    @Test
+    fun fromShift_returns_same_object_for_known_entries() {
+        for (perm in Permission.entries) {
+            assertSame(expected = perm, actual = Permission.fromShift(perm.shift))
+        }
+    }
+
+    @Test
+    fun fromShift_throws_IAE() {
+        for (shift in (-100..-1) + Int.MIN_VALUE) {
+            assertFailsWith<IllegalArgumentException> { Permission.fromShift(shift) }
+        }
+    }
+}
