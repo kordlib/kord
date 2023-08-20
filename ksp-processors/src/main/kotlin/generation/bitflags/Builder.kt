e847ad9423f3554cdde2dcd40afebfd9afdc87d3
lukellmann <lukellmann@gmail.com>
2023-08-21 00:49:58 +0200

    Expression body functions

--- ksp-processors/src/main/kotlin/generation/bitflags/Builder.kt
+++ ksp-processors/src/main/kotlin/generation/bitflags/Builder.kt
@@ -11,90 +11,88 @@ import dev.kord.ksp.generation.shared.EMPTY_BIT_SET
 import dev.kord.ksp.generation.shared.GenerationContext
 
 context(BitFlags, GenerationContext)
-internal fun TypeSpec.Builder.addBuilder() {
-    addClass(builderCN) {
-        primaryConstructor {
-            addParameter(valueName, valueCN) {
-                when (valueType) {
-                    INT -> defaultValue("0")
-                    BIT_SET -> defaultValue("%M()", EMPTY_BIT_SET)
-                }
+internal fun TypeSpec.Builder.addBuilder() = addClass(builderCN) {
+    primaryConstructor {
+        addParameter(valueName, valueCN) {
+            when (valueType) {
+                INT -> defaultValue("0")
+                BIT_SET -> defaultValue("%M()", EMPTY_BIT_SET)
             }
         }
-        addProperty(valueName, valueCN, PRIVATE) {
-            mutable(
-                when (valueType) {
-                    INT -> true
-                    BIT_SET -> false
-                }
+    }
+    addProperty(valueName, valueCN, PRIVATE) {
+        mutable(
+            when (valueType) {
+                INT -> true
+                BIT_SET -> false
+            }
+        )
+        initializer(valueName)
+    }
+
+    val builder = builderCN.simpleName
+    addFunction("unaryPlus") {
+        addModifiers(PUBLIC, OPERATOR)
+        receiver(entityCN)
+
+        addStatement(
+            when (valueType) {
+                INT -> "this@$builder.$valueName = this@$builder.$valueName or this.$valueName"
+                BIT_SET -> "this@$builder.$valueName.add(this.$valueName)"
+            }
+        )
+    }
+    addFunction("unaryPlus") {
+        addModifiers(PUBLIC, OPERATOR)
+        receiver(collectionCN)
+
+        addStatement(
+            when (valueType) {
+                INT -> "this@$builder.$valueName = this@$builder.$valueName or this.$valueName"
+                BIT_SET -> "this@$builder.$valueName.add(this.$valueName)"
+            }
+        )
+    }
+
+    addFunction("unaryMinus") {
+        addModifiers(PUBLIC, OPERATOR)
+        receiver(entityCN)
+
+        addStatement(
+            when (valueType) {
+                INT -> "this@$builder.$valueName = this@$builder.$valueName and this.$valueName.inv()"
+                BIT_SET -> "this@$builder.$valueName.remove(this.$valueName)"
+            }
+        )
+    }
+    addFunction("unaryMinus") {
+        addModifiers(PUBLIC, OPERATOR)
+        receiver(collectionCN)
+
+        addStatement(
+            when (valueType) {
+                INT -> "this@$builder.$valueName = this@$builder.$valueName and this.$valueName.inv()"
+                BIT_SET -> "this@$builder.$valueName.remove(this.$valueName)"
+            }
+        )
+    }
+
+    addFunction("build") {
+        returns(collectionCN)
+        addStatement("return %T($valueName)", collectionCN)
+    }
+
+    addFunction("flags") {
+        addKdoc("@suppress")
+        @OptIn(DelicateKotlinPoetApi::class)
+        addAnnotation(
+            Deprecated(
+                "Renamed to 'build'",
+                ReplaceWith("this.build()", imports = emptyArray()),
+                DeprecationLevel.WARNING,
             )
-            initializer(valueName)
-        }
-
-        val builder = builderCN.simpleName
-        addFunction("unaryPlus") {
-            addModifiers(PUBLIC, OPERATOR)
-            receiver(entityCN)
-
-            addStatement(
-                when (valueType) {
-                    INT -> "this@$builder.$valueName = this@$builder.$valueName or this.$valueName"
-                    BIT_SET -> "this@$builder.$valueName.add(this.$valueName)"
-                }
-            )
-        }
-        addFunction("unaryPlus") {
-            addModifiers(PUBLIC, OPERATOR)
-            receiver(collectionCN)
-
-            addStatement(
-                when (valueType) {
-                    INT -> "this@$builder.$valueName = this@$builder.$valueName or this.$valueName"
-                    BIT_SET -> "this@$builder.$valueName.add(this.$valueName)"
-                }
-            )
-        }
-
-        addFunction("unaryMinus") {
-            addModifiers(PUBLIC, OPERATOR)
-            receiver(entityCN)
-
-            addStatement(
-                when (valueType) {
-                    INT -> "this@$builder.$valueName = this@$builder.$valueName and this.$valueName.inv()"
-                    BIT_SET -> "this@$builder.$valueName.remove(this.$valueName)"
-                }
-            )
-        }
-        addFunction("unaryMinus") {
-            addModifiers(PUBLIC, OPERATOR)
-            receiver(collectionCN)
-
-            addStatement(
-                when (valueType) {
-                    INT -> "this@$builder.$valueName = this@$builder.$valueName and this.$valueName.inv()"
-                    BIT_SET -> "this@$builder.$valueName.remove(this.$valueName)"
-                }
-            )
-        }
-
-        addFunction("build") {
-            returns(collectionCN)
-            addStatement("return %T($valueName)", collectionCN)
-        }
-
-        addFunction("flags") {
-            addKdoc("@suppress")
-            @OptIn(DelicateKotlinPoetApi::class)
-            addAnnotation(
-                Deprecated(
-                    "Renamed to 'build'",
-                    ReplaceWith("this.build()", imports = emptyArray()),
-                    DeprecationLevel.WARNING,
-                )
-            )
-            returns(collectionCN)
-            addStatement("return build()")
-        }
+        )
+        returns(collectionCN)
+        addStatement("return build()")
     }
 }
--- ksp-processors/src/main/kotlin/generation/bitflags/Serializer.kt
+++ ksp-processors/src/main/kotlin/generation/bitflags/Serializer.kt
@@ -22,39 +22,37 @@ import kotlinx.serialization.encoding.Encoder
 private val SERIALIZER_METHOD = MemberName("kotlinx.serialization.builtins", "serializer")
 
 context(BitFlags, GenerationContext)
-internal fun TypeSpec.Builder.addSerializer() {
-    addObject("Serializer") {
-        addModifiers(INTERNAL)
-        addSuperinterface(K_SERIALIZER.parameterizedBy(collectionCN))
+internal fun TypeSpec.Builder.addSerializer() = addObject("Serializer") {
+    addModifiers(INTERNAL)
+    addSuperinterface(K_SERIALIZER.parameterizedBy(collectionCN))
 
-        addProperty<SerialDescriptor>("descriptor", OVERRIDE) {
-            initializer(
-                "%M(%S, %T)",
-                PRIMITIVE_SERIAL_DESCRIPTOR,
-                collectionCN.canonicalName,
-                valueType.toPrimitiveKind(),
-            )
-        }
+    addProperty<SerialDescriptor>("descriptor", OVERRIDE) {
+        initializer(
+            "%M(%S, %T)",
+            PRIMITIVE_SERIAL_DESCRIPTOR,
+            collectionCN.canonicalName,
+            valueType.toPrimitiveKind(),
+        )
+    }
 
-        addProperty("delegate", K_SERIALIZER.parameterizedBy(valueCN), PRIVATE) {
-            when (valueType) {
-                INT -> initializer("%T.%M()", valueCN, SERIALIZER_METHOD)
-                BIT_SET -> initializer("%T.serializer()", valueCN)
-            }
+    addProperty("delegate", K_SERIALIZER.parameterizedBy(valueCN), PRIVATE) {
+        when (valueType) {
+            INT -> initializer("%T.%M()", valueCN, SERIALIZER_METHOD)
+            BIT_SET -> initializer("%T.serializer()", valueCN)
         }
+    }
 
-        addFunction("serialize") {
-            addModifiers(OVERRIDE)
-            addParameter<Encoder>("encoder")
-            addParameter("value", collectionCN)
-            addStatement("encoder.encodeSerializableValue(delegate, value.$valueName)")
-        }
+    addFunction("serialize") {
+        addModifiers(OVERRIDE)
+        addParameter<Encoder>("encoder")
+        addParameter("value", collectionCN)
+        addStatement("encoder.encodeSerializableValue(delegate, value.$valueName)")
+    }
 
-        addFunction("deserialize") {
-            addModifiers(OVERRIDE)
-            returns(collectionCN)
-            addParameter<Decoder>("decoder")
-            addStatement("return %T(decoder.decodeSerializableValue(delegate))", collectionCN)
-        }
+    addFunction("deserialize") {
+        addModifiers(OVERRIDE)
+        returns(collectionCN)
+        addParameter<Decoder>("decoder")
+        addStatement("return %T(decoder.decodeSerializableValue(delegate))", collectionCN)
     }
 }
