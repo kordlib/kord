fdf16a28fabb0e2f3b55a23182ec06e18028e749
lukellmann <lukellmann@gmail.com>
2023-08-22 00:46:43 +0200

    Use correct MemberName for EXACTLY_ONCE

--- ksp-processors/src/main/kotlin/generation/shared/Names.kt
+++ ksp-processors/src/main/kotlin/generation/shared/Names.kt
@@ -2,12 +2,13 @@ package dev.kord.ksp.generation.shared
 
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.MemberName
+import com.squareup.kotlinpoet.MemberName.Companion.member
 import com.squareup.kotlinpoet.asClassName
 import kotlinx.serialization.KSerializer
 
 internal val OPT_IN = ClassName("kotlin", "OptIn")
 internal val CONTRACT = MemberName("kotlin.contracts", "contract")
-internal val EXACTLY_ONCE = MemberName("kotlin.contracts.InvocationKind", "EXACTLY_ONCE")
+internal val EXACTLY_ONCE = ClassName("kotlin.contracts", "InvocationKind").member("EXACTLY_ONCE")
 
 internal val PRIMITIVE_SERIAL_DESCRIPTOR = MemberName("kotlinx.serialization.descriptors", "PrimitiveSerialDescriptor")
 internal val K_SERIALIZER = KSerializer::class.asClassName()
