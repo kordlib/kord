d158f4c2c066e97beac3c45ae6b7c31279c3eea4
lukellmann <lukellmann@gmail.com>
2023-08-22 00:33:49 +0200

    Better file names

--- ksp-processors/src/main/kotlin/generation/shared/OptIns.kt
+++ ksp-processors/src/main/kotlin/generation/shared/OptIns.kt
@@ -5,15 +5,6 @@ import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.joinToCode
 import dev.kord.ksp.addAnnotation
-import dev.kord.ksp.generation.GenerationEntity
-import kotlin.DeprecationLevel.*
-
-internal val GenerationEntity.Entry.nameWithSuppressedDeprecation
-    get() = when (deprecated?.level) {
-        null -> name
-        WARNING -> """@Suppress("DEPRECATION")·$name"""
-        ERROR, HIDDEN -> """@Suppress("DEPRECATION_ERROR")·$name"""
-    }
 
 context(GenerationContext)
 internal fun Annotatable.Builder<*>.addEntryOptIns() {
