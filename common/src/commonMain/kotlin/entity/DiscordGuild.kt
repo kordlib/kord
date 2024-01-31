f1f9d3985f655f388a4c458309f8ecd89a523159
viztea <44017640+viztea@users.noreply.github.com>
2024-01-31 09:07:14 -0800

    fix(common): remove serial name annotation for `DiscordVoiceState#member`

--- common/src/commonMain/kotlin/entity/DiscordGuild.kt
+++ common/src/commonMain/kotlin/entity/DiscordGuild.kt
@@ -423,7 +423,7 @@ public data class DiscordVoiceState(
     @SerialName("guild_id") val guildId: OptionalSnowflake = OptionalSnowflake.Missing,
     @SerialName("channel_id") val channelId: Snowflake?,
     @SerialName("user_id") val userId: Snowflake,
-    @SerialName("guild_member") val member: Optional<DiscordGuildMember> = Optional.Missing(),
+    val member: Optional<DiscordGuildMember> = Optional.Missing(),
     @SerialName("session_id") val sessionId: String,
     val deaf: Boolean,
     val mute: Boolean,
