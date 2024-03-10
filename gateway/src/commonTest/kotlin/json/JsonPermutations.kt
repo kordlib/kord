53717db49eae112e1221168bf0f5a9790cc5fc03
lukellmann <lukellmann@gmail.com>
2024-03-10 07:21:52 +0100

    Rename

--- gateway/src/commonTest/kotlin/json/JsonPermutations.kt
+++ gateway/src/commonTest/kotlin/json/JsonPermutations.kt
@@ -1,18 +1,18 @@
 package dev.kord.gateway.json
 
 fun jsonObjectPermutations(vararg keyValuePairs: Pair<String, String>): List<String> {
-    fun <T> recurse(pairs: List<T>): List<List<T>> =
-        if (pairs.isEmpty()) {
+    fun <T> permutations(list: List<T>): List<List<T>> =
+        if (list.isEmpty()) {
             listOf(emptyList())
         } else {
-            val head = pairs.first()
-            val tail = pairs.subList(1, pairs.size)
-            recurse(tail).flatMap { perm ->
+            val head = list.first()
+            val tail = list.subList(1, list.size)
+            permutations(tail).flatMap { perm ->
                 val len = perm.size
                 (0..len).map { i -> perm.subList(0, i) + head + perm.subList(i, len) }
             }
         }
-    return recurse(keyValuePairs.toList()).map { pairs ->
+    return permutations(keyValuePairs.toList()).map { pairs ->
         pairs.joinToString(separator = ",", prefix = "{", postfix = "}") { (key, value) -> "\"$key\":$value" }
     }
 }
