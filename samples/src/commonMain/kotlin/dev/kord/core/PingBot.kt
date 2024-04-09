a883c08931b8d73d121d1d4fe5ba50f915a287c5
Michael Rittmeister <michael@rittmeister.in>
2024-04-09 20:49:58 +0200

    Fix unsafe api usage

--- samples/src/commonMain/kotlin/dev/kord/core/PingBot.kt
+++ samples/src/commonMain/kotlin/dev/kord/core/PingBot.kt
@@ -1,10 +1,10 @@
 package dev.kord.core
 
-import dev.kord.core.behavior.reply
+import dev.kord.core.behavior.channel.PollParentChannelBehavior
+import dev.kord.core.behavior.channel.createPoll
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
-import dev.kord.rest.builder.message.create.poll
 import kotlin.time.Duration.Companion.minutes
 
 suspend fun main(args: Array<String>) {
@@ -14,8 +14,7 @@ suspend fun main(args: Array<String>) {
         if (message.author?.isBot == true) return@on
         if (message.content == "!ping") message.channel.createMessage("pong")
         if (message.content == "!poll") {
-            message.reply {
-                poll {
+            (message.channel as PollParentChannelBehavior).createPoll {
                     question("Does this work?")
 
                     answer("Yes")
@@ -25,7 +24,6 @@ suspend fun main(args: Array<String>) {
                 }
             }
         }
-    }
 
     kord.login {
         presence { playing("!ping to pong") }
