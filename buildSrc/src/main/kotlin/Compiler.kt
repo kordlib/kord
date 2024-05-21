05028c6e47c53759fd2be67a90d97ebaf73530ec
Luca Kellermann <lukellmann@gmail.com>
2024-05-21 15:55:38 +0200

    Update dependencies
    * Gradle 8.6 -> 8.7
    * Kotlin 1.9.22 -> 1.9.24
    * Ktor 2.3.9 -> 2.3.11
    * kotlinx.coroutines 1.8.0 -> 1.8.1
    * kotlinx-datetime 0.5.0 -> 0.6.0
    * kotlin-logging 6.0.3 -> 6.0.9
    * SLF4J 2.0.12 -> 2.0.13
    * kotlin-node 20.11.5-pre.706 -> 20.11.30-pre.751
    * Stately 2.0.6 -> 2.0.7
    * KSP 1.9.22-1.0.17 -> 1.9.24-1.0.20
    * MockK 1.13.10 -> 1.13.11
    * BSON 5.0.1 -> 5.1.0
    * AtomicFU 0.23.2 -> 0.24.0
    * Replace gradle/wrapper-validation-action with gradle/actions/wrapper-validation

--- buildSrc/src/main/kotlin/Compiler.kt
+++ buildSrc/src/main/kotlin/Compiler.kt
@@ -1,9 +1,4 @@
-import kotlinx.atomicfu.plugin.gradle.AtomicFUPluginExtension
-import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.assign
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.gradle.dsl.KotlinCommonCompilerOptions
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
@@ -35,13 +30,5 @@ fun KotlinSourceSet.applyKordOptIns() {
         // allow `ExperimentalCoroutinesApi` for `TestScope.currentTime`
         if ("Test" in name) optIn(OptIns.coroutines)
         kordOptIns.forEach(::optIn)
-    }
-}
-
-fun Project.configureAtomicFU() {
-    // https://github.com/Kotlin/kotlinx-atomicfu/issues/210
-    configure<AtomicFUPluginExtension> {
-        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
-        dependenciesVersion = libs.findVersion("kotlinx-atomicfu").get().requiredVersion
     }
 }
