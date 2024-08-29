b9770ec183de39a440bb60ee5591aaff808f1596
Luca Kellermann <lukellmann@gmail.com>
2024-08-29 14:40:53 +0200

    Fix :samples for Kotlin to 2.1.0-dev-5441

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -11,6 +11,11 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(projects.core)
+
+                // these are required for whatever reason for Kotlin 2.1.0-dev-5441
+                implementation(projects.common)
+                implementation(projects.rest)
+                implementation(projects.gateway)
             }
         }
         jvmMain {
