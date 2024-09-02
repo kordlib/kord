2a653157bc79ddacabd790febfa256f84bde4100
Luca Kellermann <lukellmann@gmail.com>
2024-09-02 14:21:06 +0200

    Update Kotlin to 2.0.20 (#975)
    Context receivers will be replaced by [1] and are deprecated for now,
    so the CONTEXT_RECEIVERS_DEPRECATED warning must be suppressed in the
    ksp-processors project.
    
    The changes in the KLib ABI dumps are due to [2], constructors of sealed
    classes aren't public ABI.
    
    [1] https://github.com/Kotlin/KEEP/issues/367
    [2] https://github.com/JetBrains/kotlin/commit/56ba701b3d05738d70a4f906859bbb066c9531a0

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -32,8 +32,9 @@ kotlin {
             dependencies {
                 api(libs.ktor.client.js)
 
-                // workaround for https://youtrack.jetbrains.com/issue/KT-43500
-                // (intended to be compileOnly in commonMain only)
+                // workaround for https://youtrack.jetbrains.com/issue/KT-43500 /
+                // https://youtrack.jetbrains.com/issue/KT-64109#focus=Comments-27-10064206.0-0 /
+                // https://youtrack.jetbrains.com/issue/KT-61096 (intended to be compileOnly in commonMain only)
                 implementation(projects.kspAnnotations)
             }
         }
