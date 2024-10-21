e99c5c583229d20e69632aae39cee47da637abef
Luca Kellermann <lukellmann@gmail.com>
2024-10-21 21:54:10 +0200

    Bump DeprecationLevels after 0.15.0 (#987)
    WARNING -> ERROR
    ERROR -> HIDDEN
    HIDDEN -> remove

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -15,9 +15,6 @@ kotlin {
                 api(libs.kord.cache.map)
 
                 implementation(libs.kotlin.logging)
-
-                // TODO remove when kordLogger is removed
-                implementation(libs.kotlin.logging.old)
             }
         }
         jvmMain {
