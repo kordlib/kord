2b669519939a3fd8db7f8fc0363f87c386efdabb
Michael Rittmeister <michael@rittmeister.in>
2024-11-07 00:51:17 +0100

    Use a better implementation for UpdatableByteArrayInputStream
    Co-authored-by: viztea <44017640+viztea@users.noreply.github.com>

--- gateway/src/jvmMain/kotlin/ZstdDecompressor.kt
+++ gateway/src/jvmMain/kotlin/ZstdDecompressor.kt
@@ -3,7 +3,6 @@ package dev.kord.gateway
 import com.github.luben.zstd.ZstdInputStream
 import io.ktor.websocket.*
 import java.io.ByteArrayInputStream
-import java.io.InputStream
 
 internal actual fun ZstdDecompressor() = object : Decompressor {
 
@@ -11,7 +10,7 @@ internal actual fun ZstdDecompressor() = object : Decompressor {
     private val zstdStream = ZstdInputStream(input).apply { continuous = true }
 
     override fun Frame.decompress(): String {
-        input.updateDelegate(data)
+        input.update(data)
         return zstdStream.readBytes().decodeToString()
     }
 
@@ -20,14 +19,10 @@ internal actual fun ZstdDecompressor() = object : Decompressor {
     }
 }
 
-private class UpdatableByteArrayInputStream : InputStream() {
-    private var delegate: ByteArrayInputStream? = null
-
-    private val d: InputStream get() = delegate ?: error("No data available")
-
-    override fun read(): Int = d.read()
-
-    fun updateDelegate(bytes: ByteArray) {
-        delegate = ByteArrayInputStream(bytes)
+private class UpdatableByteArrayInputStream : ByteArrayInputStream(ByteArray(0)) {
+    fun update(newBuf: ByteArray) {
+        this.pos = 0
+        this.buf = newBuf
+        this.count = newBuf.size
     }
 }
