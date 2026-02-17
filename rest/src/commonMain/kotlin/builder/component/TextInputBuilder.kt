5574eb63af7b09edff168aa1fecf1739f29757e5
Michael Rittmeister <michael@rittmeister.in>
2026-02-17 03:26:45 +0100

    Feat/components v2 (#1009)
    * feat(common,core,rest): components v2
    
    * fix(core): ComponentInteraction factory
    
    * chore: api dump
    
    * Improve builder hierarchy
    
    * Improve builder hierarchy
    
    * Document and improve core entities
    
    * Update common/src/commonMain/kotlin/entity/DiscordMessage.kt
    
    Co-authored-by: Luca Kellermann <lukellmann@gmail.com>
    
    * Fix some requested changes
    
    * Apply suggestions from code review
    
    Co-authored-by: Peter Gulko <ztrap.developer@gmail.com>
    
    * Fix documentation
    
    * Api dump
    
    * add contract to MediaGalleryBuilder#item (#1017)
    
    * Updates to components v2 (#1028)
    
    * Add updated components v2 stuff
    
    * API dump
    
    * forgot builders
    
    * Allow components v2 stuff to be done on modals (#1031)
    
    * Allow components v2 stuff to be done on modals
    
    * Fix labels not working in modals. Credit @Taubsie Current issue is action rows now no longer have a `components` field, but I can't quite figure out how yet.
    
    * * Fix serializer being broken @Taubsie
    * Just go back to having `components` in `ComponentData`
    
    * * Fix more stuff @Taubsie
    * Add text input to label builder, was missed before
    * Remove label option from text inputs as Label exists now
    
    * Fix modal replies @Taubsie
    
    * API Dump
    
    * Fix file upload component not providing files
    
    * Add convenience fields to ModalSubmitInteraction.kt to avoid messy casts on the users end
    
    * Update textInputs field on submit interaction to work with components v2. Original field marked as deprected.
    
    Updated deprecation notice for removal
    
    * Fixed an Exception when using Text Displays
    
    * Correct deprecation notice, compv2 will be in 0.18.0 so raising to error in 0.18.0 is wrong.
    
    * Add RadioGroup, CheckboxGroup and Checkbox components
    
    * Do review actions
    
    ---------
    
    Co-authored-by: viztea <hi@vzt.gay>
    Co-authored-by: Luca Kellermann <lukellmann@gmail.com>
    Co-authored-by: Peter Gulko <ztrap.developer@gmail.com>
    Co-authored-by: nillpoe <nillpoe@pm.me>
    Co-authored-by: NoComment <67918617+NoComment1105@users.noreply.github.com>

--- rest/src/commonMain/kotlin/builder/component/TextInputBuilder.kt
+++ rest/src/commonMain/kotlin/builder/component/TextInputBuilder.kt
@@ -20,7 +20,8 @@ import dev.kord.common.entity.optional.delegate.delegate
 public class TextInputBuilder(
     public var style: TextInputStyle,
     public var customId: String,
-    public var label: String,
+    @Deprecated("Deprecated in favor of `label` and `description` on the **Label** component.", level = DeprecationLevel.WARNING)
+    public var label: String? = null,
 ) : ActionRowComponentBuilder() {
     /**
      * The range of lengths that can be accepted. Accepts any range between [0,4000].
@@ -49,11 +50,12 @@ public class TextInputBuilder(
     public var required: Boolean? by ::_required.delegate()
 
     override fun build(): DiscordTextInputComponent {
+        @Suppress("DEPRECATION")
         return DiscordTextInputComponent(
             type = ComponentType.TextInput,
             style = Optional(style),
             customId = Optional(customId),
-            label = Optional(label),
+            label = if (label == null) Optional.Missing() else Optional(label),
             minLength = allowedLength?.let { OptionalInt.Value(it.start) } ?: OptionalInt.Missing,
             maxLength = allowedLength?.let { OptionalInt.Value(it.endInclusive) } ?: OptionalInt.Missing,
             placeholder = _placeholder,
