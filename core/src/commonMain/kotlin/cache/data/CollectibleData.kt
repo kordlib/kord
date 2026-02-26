932b37949c7148b1af1109ae22df579a79d3571e
Taubsie <50213229+Taubsie@users.noreply.github.com>
2026-02-26 23:46:39 +0100

    Fixed a SerializationException (#1046)

--- core/src/commonMain/kotlin/cache/data/CollectibleData.kt
+++ core/src/commonMain/kotlin/cache/data/CollectibleData.kt
@@ -5,7 +5,7 @@ import dev.kord.common.entity.DiscordNameplate
 import dev.kord.common.entity.optional.Optional
 
 public data class CollectibleData(
-    val nameplate: Optional<DiscordNameplate> = Optional.Missing()
+    val nameplate: Optional<DiscordNameplate?> = Optional.Missing()
 ) {
     public companion object {
         public fun from(entity: DiscordCollectible): CollectibleData = with(entity) {
