1825f2adf367b7b55d660807a3703c20904aec98
nx <66590929+fi-xz@users.noreply.github.com>
2026-03-25 18:31:42 +0900

    Fix typo of StageInstance events
    Change "stageInstace" to "stageInstance"
    While this change break binary compatibilities I think the change is needed
    Should be OK since this event is added very recently

--- core/src/commonMain/kotlin/event/guild/StageInstanceDeleteEvent.kt
+++ core/src/commonMain/kotlin/event/guild/StageInstanceDeleteEvent.kt
@@ -5,12 +5,12 @@ import dev.kord.core.entity.StageInstance
 import dev.kord.core.event.Event
 
 public class StageInstanceDeleteEvent(
-    public val stageInstace: StageInstance,
+    public val stageInstance: StageInstance,
     override val shard: Int,
     override val customContext: Any?,
     override val kord: Kord
 ) : Event {
     override fun toString(): String {
-        return "StageInstanceDeleteEvent(stageInstace=$stageInstace, shard=$shard)"
+        return "StageInstanceDeleteEvent(stageInstance=$stageInstance, shard=$shard)"
     }
 }
