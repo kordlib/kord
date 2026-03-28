2d81808f4431bd795cf02a9410ddd3a50f1b2a7e
Maya <maychennn@protonmail.com>
2026-03-28 03:28:45 -0400

    Common interface for threads and del. threads

--- core/src/commonMain/kotlin/entity/channel/thread/TextChannelThread.kt
+++ core/src/commonMain/kotlin/entity/channel/thread/TextChannelThread.kt
@@ -3,9 +3,13 @@ package dev.kord.core.entity.channel.thread
 import dev.kord.common.entity.ChannelType
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import dev.kord.core.behavior.GuildBehavior
+import dev.kord.core.behavior.channel.threads.ThreadParentChannelBehavior
 import dev.kord.core.cache.data.ChannelData
+import dev.kord.core.entity.Guild
 import dev.kord.core.entity.channel.ForumChannel
 import dev.kord.core.entity.channel.TextChannel
+import dev.kord.core.entity.channel.ThreadParentChannel
 import dev.kord.core.supplier.EntitySupplier
 import dev.kord.core.supplier.EntitySupplyStrategy
 
@@ -15,13 +19,21 @@ import dev.kord.core.supplier.EntitySupplyStrategy
 public class TextChannelThread(
     override val data: ChannelData,
     override val kord: Kord,
-    override val supplier: EntitySupplier = kord.defaultSupplier
-) : ThreadChannel {
+    override val supplier: EntitySupplier = kord.defaultSupplier,
+) : ThreadChannel, MaybeThreadChannel {
+
+    public override val id: Snowflake = data.id
 
     /**
      * Whether this thread is private.
      */
     public val isPrivate: Boolean get() = data.type == ChannelType.PrivateThread
+
+    public override val parentId: Snowflake = super<ThreadChannel>.parentId
+
+    public override val parent: ThreadParentChannelBehavior = super<ThreadChannel>.parent
+
+    public override val type: ChannelType = super<ThreadChannel>.type
 
     /**
      * Whether non-moderators can add other non-moderators to a thread.
@@ -32,6 +44,16 @@ public class TextChannelThread(
 
     override val guildId: Snowflake
         get() = data.guildId.value!!
+
+    override val guild: GuildBehavior = super<ThreadChannel>.guild
+
+    override suspend fun getGuild(): Guild = super<ThreadChannel>.getGuild()
+
+    override suspend fun getGuildOrNull(): Guild? = super<ThreadChannel>.getGuildOrNull()
+
+    override suspend fun getParent(): ThreadParentChannel = super<ThreadChannel>.getParent()
+
+    override suspend fun getParentOrNull(): ThreadParentChannel? = super<ThreadChannel>.getParentOrNull()
 
     override suspend fun asChannel(): TextChannelThread = this
 
---
+++ core/src/commonMain/kotlin/event/channel/thread/ThreadsChangeEvent.kt
@@ -0,0 +1,4 @@
+package dev.kord.core.event.channel.thread
+
+interface ThreadsChangeEvent {
+}
