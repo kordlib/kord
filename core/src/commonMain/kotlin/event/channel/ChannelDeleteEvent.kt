9fa250950e98285689b018103566c29be0166553
Maya <maychennn@protonmail.com>
2026-03-28 02:16:18 -0400

    Common interface for Channels changes

--- core/src/commonMain/kotlin/event/channel/ChannelDeleteEvent.kt
+++ core/src/commonMain/kotlin/event/channel/ChannelDeleteEvent.kt
@@ -1,14 +1,8 @@
 package dev.kord.core.event.channel
 
-import dev.kord.core.Kord
 import dev.kord.core.entity.channel.*
-import dev.kord.core.event.Event
 
-public interface ChannelDeleteEvent : Event {
-    public val channel: Channel
-    override val kord: Kord
-        get() = channel.kord
-}
+public interface ChannelDeleteEvent : ChannelsChangeEvent
 
 public class CategoryDeleteEvent(
     override val channel: Category,
---
+++ core/src/commonMain/kotlin/event/guild/GuildEvent.kt
@@ -0,0 +1,4 @@
+package dev.kord.core.event.guild
+
+interface GuildEvent {
+}
