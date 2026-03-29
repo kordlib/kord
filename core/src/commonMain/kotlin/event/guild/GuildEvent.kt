122c43def60212c88b515458c933fcfa36eb8817
Maya <maychennn@protonmail.com>
2026-03-29 17:02:35 -0400

    Common interface for message changes
    I wasn't sure what to do with MessageBulkDeleteEvent so I made two
    interfaces to attempt both my ideas

--- core/src/commonMain/kotlin/event/guild/GuildEvent.kt
+++
@@ -1,4 +0,0 @@
-package dev.kord.core.event.guild
-
-interface GuildEvent {
-}
--- core/src/commonTest/kotlin/compilation/patternMatching/Events.kt
+++ core/src/commonTest/kotlin/compilation/patternMatching/Events.kt
@@ -6,6 +6,7 @@ import dev.kord.core.event.channel.ChannelDeleteEvent
 import dev.kord.core.event.channel.ChannelUpdateEvent
 import dev.kord.core.event.channel.thread.ThreadChannelCreateEvent
 import dev.kord.core.event.channel.thread.ThreadChannelDeleteEvent
+import dev.kord.core.event.message.*
 
 fun eventMatching(event: Event): Int {
     return when (event) {
@@ -14,6 +15,22 @@ fun eventMatching(event: Event): Int {
         is ChannelUpdateEvent -> 3
         is ThreadChannelDeleteEvent -> 4
         is ThreadChannelCreateEvent -> 5
+        is MessageCreateEvent -> 6
+        is MessageDeleteEvent -> 7
+        is MessageBulkDeleteEvent -> 8
+        is MessageUpdateEvent -> 9
+        else -> 0
+    }
+}
+
+fun messagesChangeEventMatching(event: MessagesChangeEvent): Int {
+    return when (event) {
+        is MessageChangeEvent -> when (event) {
+            is MessageCreateEvent -> 1
+            is MessageDeleteEvent -> 2
+            is MessageUpdateEvent -> 3
+        }
+        is MessageBulkDeleteEvent -> 3
         else -> 0
     }
 }
